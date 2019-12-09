c29235a5db020915e011fae2953c1ef3cff22c2a
Naguib Schreiber <nagsster@hotmail.com>
2019-12-09 15:01:21 +0100

    console

--- PB-language-statements/style.js
+++ PB-language-statements/style.js
@@ -1,3 +1,38 @@
-console.log('Hello' + ' World';
+console.log('Hello' + ' World');
 //console.log('This' + ' is' + ' my' + ' first' + ' js' + ' file');
 //SyntaxError: missing ) after argument list
+
+
+let first = 'testing';
+let second = 'coding';
+let third = 'push';
+
+console.log('first ' + 'second ' + 'third');
+
+
+let x = "hallo";
+function foo () {
+// It start a completely new memory
+//happening in the background: make a box for s;
+// ====== HOITING
+
+console.log(x);  //undefined
+var s = "test";  // define s as “test”
+console.log(x);  // test
+
+}
+foo();
+
+// Empty variable
+
+let something; // = 'undefined' //not on purpose  // on accident
+let number = null; //on purpose
+//let something = 5; // no new box for something possible
+something = 5; // overwrite on purpose !
+console.log(number);
+console.log(typeof(number));
+let anything =4;
+console.log(anything)
+
+let somethingEqualsAnything = anything;
+console.log(somethingEqualsAnything)
---
+++ PB-operators-arithmetic-pt1/style.js
@@ -0,0 +1,33 @@
+let x = 4;
+console.log(x);
+let y = 20;
+console.log(y);
+
+console.log(x-y);
+console.log(y-x);
+console.log(x*y);
+console.log(x/y);
+
+let z = 10;
+console.log(z);
+
+console.log(x*y);
+let resultOne = (x*y/z);
+console.log(resultOne); //8
+
+let a = 15;
+let b = 9;
+console.log(a/b)
+let c = 20;
+let resultTwo = (a+b*c);
+console.log(resultTwo); //195
+
+console.log(a+1);
+console.log(b-1);
+
+let d = a-b;
+console.log(d+c);
+
+//reminder
+
+console.log(resultOne % resultTwo);
