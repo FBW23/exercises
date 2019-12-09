43f4aee7c9c832aef80ee6e1403ea50d40a03e62
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2019-12-09 15:12:58 +0100

    new

---
+++ PB-operators-arithmetic-pt1/math.js
@@ -0,0 +1,29 @@
+let x=8;
+let y=5;
+
+
+console.log(x);
+console.log(y);
+
+console.log(x+y);
+console.log(x-y);
+console.log(y-x);
+console.log(y*x);
+console.log(x/y);
+let z=10;
+let resultOne=((y*x)/z);
+let a=15;
+let b=9;
+console.log(a/b);
+let c=20;
+let resultTwo=((a+b)*c);
+console.log(a++);
+console.log(b--);
+let=(b-a);
+let =(resultOne/resultTwo);
+
+
+
+
+
+
