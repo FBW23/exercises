24164f3fea1d77e8cc609dde09fd30cbb74851e1
Julien Montagnon <jumontagnon@gmail.com>
2019-12-09 15:15:48 +0100

    operators

--- PB-language-statements/index.js
+++ PB-language-statements/index.js
@@ -1,4 +1,4 @@
-console.log ('hello ' + 'world')
+console.log('hello ' + 'world');
 console.log('this ' + 'is ' + 'my ' + 'first ' + 'js ' + 'file'); //I promise, it's really my first
 
 /*1. no it doesn't work
---
+++ PB-operators-arithmetic-pt1/operators.js
@@ -0,0 +1,41 @@
+let xxx = 23;
+let yyy = 80;
+
+console.log(xxx);
+console.log(yyy);
+
+console.log(xxx + yyy);
+
+console.log(xxx-yyy);
+console.log(yyy-xxx);
+
+console.log(xxx*yyy);
+
+console.log(xxx/yyy);
+
+let zzz=10;
+console.log(xxx*yyy/zzz);
+let resultOne=(xxx*yyy/zzz);
+console.log(resultOne);
+
+let a=15;
+let b=9;
+console.log(a/b);
+
+let c=20;
+
+let resultTwo= a+b *c;
+
+console.log(resultTwo);
+
+a++;
+b--;
+
+let d=a-b;
+console.log(d);
+
+console.log(d+c);
+
+console.log(resultOne);
+
+console.log(resultOne  % resultTwo);
