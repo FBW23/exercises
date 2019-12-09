69461853c1e1d779bb9f468139fde464da1e7434
Julia <juliarpim@gmail.com>
2019-12-09 21:32:33 +0100

    javascript

---
+++ PB-operators-arithmetic-pt2/math.js
@@ -0,0 +1,44 @@
+let a = 6450;
+let b = 15;
+
+console.log(a*b);
+
+let silvia = 500;
+let weeks = 48;
+console.log(silvia*weeks);
+
+let square = 4.75;
+let times = 4
+console.log(square*times);
+
+let triangul = 5;
+let triangHeight = 6;
+let divider = 2;
+
+console.log(triangHeight*triangul/divider);
+
+let cubVol = 9;
+console.log(cubVol*3);
+
+let tipsOne = 22.35;
+let tipsTwo = 26.67;
+let tipsThree = 35.92;
+
+console.log(tipsOne*10/100);
+console.log(tipsTwo*15/100);
+console.log(tipsThree*20/100);
+
+let average =  8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4;
+console.log(average/10);
+
+let percentage = 17/30;
+console.log(percentage*100);
+
+
+
+
+
+
+
+
+
