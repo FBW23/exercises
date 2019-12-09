ec0d77a22e54d88f3ebc23fe5f4ffc36e1cc2449
Julia <juliarpim@gmail.com>
2019-12-09 15:27:36 +0100

    changes

--- PB-language-statements/js.js
+++ PB-language-statements/js.js
@@ -1,5 +1,24 @@
 console.log('hello  '+'World');
 // console.log('This '+'is '+'my '+ 'first '+ 'js');
-console.log 'hello ' + 'World';
+console.log ('hello ' + 'World');
+
+var first = 2; //number
+var second = 'hello world';//String
+var third = true; //Boolean
+
+let fourth = 12;
+let fifth = 'hello world';
+let sixth = false; 
+
+
+console.log(first);
+var first = 12;
+console.log(first);
+
+console.log(fourth);
+let fourth = 4;
+console.log(fourth);
+
+
 
 
