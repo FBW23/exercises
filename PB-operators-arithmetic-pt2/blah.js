01f2bd704b48d3e64f847d4ef6752678c03691ad
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2019-12-10 11:15:35 +0100

    nope.js

---
+++ PB-operators-arithmetic-pt2/blah.js
@@ -0,0 +1,80 @@
+// In one night, a movie theater sells tickets for 6450 dollars. Each ticket costs 15 dollars. How many tickets were sold?
+let a=6450;
+let b=15;
+
+console.log(a/b)
+// Sylvia's income is 500 dollars per week. How much does Sylvia makes every year?
+let c=500;
+let d=52;
+
+console.log(c*d)
+// Q3. Calculate the percentage of 17/30. Expected output: number%
+let e=17;
+let f=30;
+
+console.log (e/f)
+
+// Q4. Calculate the perimeter of a square. Assume each side is 4.75cm.
+let g=4.75;
+let h=4
+
+console.log (g*h);
+// Q5. Calculate the perimeter of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+
+let i=5;
+let j=6;
+let k=7;
+console.log (i+j+k);
+// Q6. Calculate the area of a square. Each side is 5cm.
+
+let l=5;
+console.log(l*l);
+
+// Q7. Calculate the area of a triangle. Assume the length of the sides are 5cm, 6cm, 7cm.
+
+console.log(i*i+j*j)
+console.log (k*k)
+let height=(i*i+j*j)
+let base=(61)
+console.log(base*base)
+
+// Q8. Calculate the volume of a cube. Length of each side is 9cm.
+
+let side=9;
+console.log(side*side*side)
+
+// Q9. Calculate the three bills including tips: €22.35 + 10% tip €26.67 + 15% tip €35.92 + 20% tip
+
+let bill=22.35;
+let bill2=26.67;
+let bill3=35.92;
+let tip=bill*0.10;
+let tip2=bill2*0.15;
+let tip3=bill3*0.20;
+console.log (bill+tip+bill2+tip2+bill3+tip3)
+
+// Q10. The number of hours Noemy worked over the last two weeks are 8, 6, 5, 9, 8, 2, 1, 8.5, 7, 4 What is Noemy's average hours worked per day?
+
+let work=8+6+5+9+8+2+1+8.5+7+4;
+let average=10;
+console.log (work/average)
+
+// Q11. A math student scored 75, 70, 85, 90, 100 on the first five tests he took . After he took his sixth math test, the average is now 85. What did he score on the sixth test? Expected output: Score in the sixth test: --.
+
+
+let score=75+70+85+90+100
+let medium=85
+let x=6
+let z=medium*x-score
+console.log(z)
+
+// Q12. For James to get a 1st class degree, he needs to get an average of 80% in all 9 of his assessments. He has taken 8 assessments and his average is 78%. What is the minimum percentage he must get in his last assessment to ensure he gets a first class? Expected output: James needs a minimum of --% to get an 80% average.
+
+let degree=80
+let av=78*8
+let test=9
+//let degree=(av+result)/test
+// degree*test=av+result
+//degree*test-av=result
+let result=degree*test-av;
+console.log(result)
