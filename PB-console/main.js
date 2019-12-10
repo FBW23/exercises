04c7f5df0c44c90428721b32eec3f212df154772
DCI-REC106 <vagobala@gmail.com>
2019-12-10 14:09:38 +0100

    dskfjdks

---
+++ PB-console/main.js
@@ -0,0 +1,45 @@
+console.log(' Console log a line of text eg. "this is an exercise in console logging" (do not forget quotation marks). ');
+
+console.log('this is an exercise in console logging');
+
+
+
+console.log('2. Declare a variable and give your variable the following value: This is the value. Console log your variable. ');
+
+
+let value='This is my value';
+console.log(value);
+
+
+
+
+console.log("3. Declare a variable called firstName and assign your first name to that variable. Do the same with your last name. Console log both variables. NOTE: When console logging two variables, separate the variables with a comma (,) in the console log e.g. console.log(variable1, variable2);");
+
+
+let firstName ='nono';
+let lastName='haha';
+
+console.log(firstName +',' +lastName);
+
+
+
+
+console.log("4. Make a profile of someones information. Store all the information in variables eg. name, city, job etc.five variables total Console log the variables. ");
+
+let name='Speedy Gonzales';
+let city='Tijuana';
+let job='annoying cats';
+let food='cheese';
+let hat='Sombrero';
+
+console.log(`name: ${name} ,  hometown: ${city}, job: ${job}, favourite food: ${food} , favourite hat type: ${hat}`);
+
+
+
+
+
+console.log('5. Print a sentence using the profile variables above, adding text where necessary for sentence flow, i.e.') 
+
+
+
+console.log(name + ' is a 50 year old mouse from ' + city + ' that loves '+ job + ' and eating ' + food + ' whilst wearing a ' +hat);
