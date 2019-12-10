85fa37e3bff0cf0649905675c6128973b67adbca
Julien Montagnon <jumontagnon@gmail.com>
2019-12-10 12:45:52 +0100

    operators2

---
+++ PB-operators-arithmetic-pt2/operators2.js
@@ -0,0 +1,83 @@
+let ticketSold=6450;
+let ticketPrice=15;
+
+let Q1=ticketSold/ticketPrice;
+console.log(Q1+' tickets');
+//Q1=430 tickets
+
+let incomePerWeek=500;
+let numberOfWeeks= 12*4;
+
+let Q2=incomePerWeek*numberOfWeeks;
+console.log(Q2+'$');
+//Q2=24000$
+
+let number= 17/30;
+let total=100;
+let Q3= number*total;
+console.log(Q3+' %');
+//Q3= 57%
+
+let side=4.75;
+let Q4= 4*side;
+console.log(Q4+' cm');
+//Q4=19cm
+
+let side1=5;
+let side2=6;
+let side3=7;
+let Q5= side1+side2+side3;
+console.log(Q5+' cm');
+//Q5=18cm
+
+let Q6=side1*side1;
+console.log(Q6+' cm²');
+//Q6=25cm²
+
+// let Q7=;
+// console.log(Q7);
+//Q7=
+
+let lenght=9;
+let Q8=lenght*lenght*lenght;
+console.log(Q8+' cm³');
+//Q8=729cm³
+
+let price1=22.35;
+let tip1=price1*10/100;
+let bill1=price1+tip1;
+
+let price2=26.67;
+let tip2=price2*15/100;
+let bill2=price2+tip2;
+
+let price3=35.92;
+let tip3=price3*20/100;
+let bill3=price3+tip3;
+
+console.log(bill1+'€', bill2+'€', bill3+'€');
+//Q9= 24.585€ 30.6705€ 43.104€
+
+let D1=8;
+let D2=6;
+let D3=5;
+let D4=9;
+let D5=8;
+let D6=2;
+let D7=1;
+let D8=8.5;
+let D9=7;
+let D10=4;
+
+let Q10=(D1+D2+D3+D4+D5+D6+D7+D8+D9+D10)/10;
+console.log(Q10 + ' hours');
+//Q10=5,85 hours
+
+let numberOfTests=6;
+let averageTotal=85;
+let total5FirstTests=420;
+let Q11=(numberOfTests*averageTotal)-total5FirstTests;
+
+console.log('Score in the sixth test:'+Q11);
+
+//Q12= just burned my brain
