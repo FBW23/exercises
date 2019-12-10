55a6ea55fc19ec15dc50ed23ab559493bd6470df
Constantin <grizman040@gmail.com>
2019-12-10 17:05:34 +0100

    ternary

---
+++ PB-ternary-exercises/tenerary.js
@@ -0,0 +1,55 @@
+let isDog = 'pat,pat';
+console.count(isDog ? 'pat,pat' : 'find me a dog to pat!');
+
+
+let speedCheck = 50;
+let mySpeed = 49;
+console.log((speedCheck>mySpeed)?'you are going too fast!':'You are driving below the speed limit, Oma');
+console.log((speedCheck<mySpeed)?'you are going too fast!':'You are driving below the speed limit, Oma');
+
+
+
+let age;
+console.log(age>=16? 'server butter beer': 'serve beer');
+let ages = 20;
+console.log(ages>=16? 'server butter beer': 'serve beer');
+
+
+
+let isStudent = true;
+console.log(isStudent? 'Ticket costs €5,00' : 'Ticket costs €12,00');
+
+let okMarie = 'cake';
+console.log(okMarie ==='cake'? 'Let them eat cake' : 'Oh, bother');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
