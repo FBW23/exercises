015ef06a927992bcd03f2dde95eda4428f18097d
Bendis Hoxha <bendishoxha1@gmail.com>
2019-12-10 11:11:22 +0100

    done

---
+++ PB-operators-arithmetic-pt2/var.js
@@ -0,0 +1,125 @@
+//Q1.
+let ticketTheater = 6450;
+console.log(6450);
+
+//Q2.
+let ticket = 15;
+console.log(15);
+
+console.log(6450/15);
+// The ticket cost 430
+
+
+// Q3.
+let salary = 5000;
+console.log(5000);
+
+console.log(5000*52);
+// one year salary 260000
+
+console.log(17/30)*100;
+// 56%
+
+
+// Q4. 
+let x = 4.75;
+
+console.log(x*4,75);
+// 19.75
+
+// Q5.
+let a = 5;
+let b = 6;
+let c = 7;
+
+console.log(a+b+c);
+//18
+
+// Q6.
+let d = 5;
+let squareArea=Math.pow(d,2);
+console.log(squareArea);
+//25
+
+// Q7.
+let f=5;
+let i = 6;
+let j=7;
+let halfPerimeter=(5+6+7)/2;
+let triangleArea=Math.sqrt(halfPerimeter*(halfPerimeter-f)*(halfPerimeter-i)*(halfPerimeter-j));
+console.log(triangleArea);
+//14.6
+
+// Q8.
+let z = 9;
+
+console.log(z*z*z);
+//729
+
+// Q9.
+let bill1 = 22.35;
+let bill2 = 26.67;
+let bill3 = 35.92;
+let tip1 = 10;
+let tip2 = 15;
+let tip3 = 20;
+let sum1, sum2, sum3;
+tip1=(bill1*10)/100;
+tip2=(bill2*15)/100;
+tip3=(bill3*20)/100;
+sum1=bill1+tip1;
+sum2=bill2+tip2;
+sum3=bill3+tip3;
+
+console.log(sum1);
+//first bill 24.59 euro
+console.log(sum2);
+//second bill 30.67 euro
+console.log(sum3);
+//third bill 43.1 euro
+
+// Q10. 
+let mon = 8;
+let tue = 6;
+let wed = 5;
+let thur = 9;
+let fri = 8;
+let mon1 = 2;
+let tue1 = 1;
+let wed1 = 8.5;
+let thur1 = 7;
+let fri1 = 4;
+
+console.log((mon+tue+wed+thur+fri+mon1+tue1+wed1+thur1+fri1)/10);
+//5.85 hours
+
+
+// Q11.
+let firstWeek = 75+70+85+90+100;
+let avarage = 85;
+let numberOfTests=6;
+let finaltest;
+
+
+finaltest= (numberOfTests*avarage)-firstWeek+'%';
+console.log('Score in sixth test' + finaltest);
+// Score in sixth test90%
+
+// Q12.
+let averageAssessments = 9;
+let getAverage=80;
+let passedTests=8;
+let passedAverage=78;
+let totalaverage=(getAverage*averageAssessments);
+let totalCurrently=(passedAverage*passedTests);
+
+console.log(totalaverage-totalCurrently);
+
+//96%
+
+
+
+
+
+
+
