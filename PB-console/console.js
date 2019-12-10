c3138d71318c782b7492b730b4e6428c6b5dcfb6
Julien Montagnon <jumontagnon@gmail.com>
2019-12-10 14:16:00 +0100

    console done

---
+++ PB-console/console.js
@@ -0,0 +1,19 @@
+let line1 = 'this is an exercise in console logging';
+console.log(line1);
+
+let variable1 = 'This is the value.'
+console.log(variable1);
+
+let firstName = 'Julien';
+let lastName = 'Montagnon';
+console.log(firstName, lastName);
+
+console.log('Profile');
+let firstName1 = 'Denis';
+let lastName2 = 'deGrossesse';
+let city = 'Dunkerque';
+let job = 'Postman';
+let age = '23';
+console.log(`First name: ${firstName1}, Last Name: ${lastName2}, Age: ${age}, City: ${city}, Job: ${job}`);
+
+console.log(`${firstName1} ${lastName2} is a ${age} years old guy living in ${city}. ${firstName1} really enjoy his job as a ${job} in his beautiful city of ${city}.`);
---
+++ PB-operators-comparison/julien.js
@@ -0,0 +1,15 @@
+let x=20;
+let y=30;
+console.log('Check whether x and y are equal.')
+console.log(x===y); //false
+
+console.log('Check whether x and y are *not* equal.')
+console.log(x!==y);//true
+
+console.log(x>y); //false
+
+console.log(y>x); //true
+
+console.log(x<=y); //true
+
+
