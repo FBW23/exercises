a9946f76a14f85a8e5192ac24b96d296eee6119a
Constantin <grizman040@gmail.com>
2019-12-10 14:27:54 +0100

    console

---
+++ PB-console/console.js
@@ -0,0 +1,24 @@
+console.log(`this is an exercise in console logging`);
+
+let firstOne = "This is the value.";
+console.log(firstOne);
+
+let firstName = 'Constantin';
+let secondName = 'Grizman';
+console.log(`${firstName}, ${secondName}`);
+
+let 
+name = 'Bubu',
+job='Designer',
+city='Berlin',
+hobby='Music';
+console.log(`Profile: ${name}/${job}/${city}/${hobby}` );
+
+
+
+let person ='John Smith ',
+age=' is a 43 ',
+time='year old ',
+profession='teacher ',
+life='who lives ';
+console.log(`${person} ${age} ${time} ${profession} ${life} ${city}`);
