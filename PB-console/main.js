0619294a1e3241b42edbe559882e48038cd3b5f2
DCI-REC106 <vagobala@gmail.com>
2019-12-10 14:13:42 +0100

    dskfjdks

--- PB-console/main.js
+++ PB-console/main.js
@@ -1,12 +1,13 @@
 console.log(' Console log a line of text eg. "this is an exercise in console logging" (do not forget quotation marks). ');
-
+console.log('-----------------------------');
 console.log('this is an exercise in console logging');
+console.log('=========================================================================');
 
 
 
 console.log('2. Declare a variable and give your variable the following value: This is the value. Console log your variable. ');
 
-
+console.log('----------------------');
 let value='This is my value';
 console.log(value);
 
@@ -14,18 +15,18 @@ console.log(value);
 
 
 console.log("3. Declare a variable called firstName and assign your first name to that variable. Do the same with your last name. Console log both variables. NOTE: When console logging two variables, separate the variables with a comma (,) in the console log e.g. console.log(variable1, variable2);");
-
+console.log('-------------------------');
 
 let firstName ='nono';
 let lastName='haha';
 
 console.log(firstName +',' +lastName);
 
-
+console.log('=========================================================================');
 
 
 console.log("4. Make a profile of someones information. Store all the information in variables eg. name, city, job etc.five variables total Console log the variables. ");
-
+console.log('-------------------------');
 let name='Speedy Gonzales';
 let city='Tijuana';
 let job='annoying cats';
@@ -35,11 +36,13 @@ let hat='Sombrero';
 console.log(`name: ${name} ,  hometown: ${city}, job: ${job}, favourite food: ${food} , favourite hat type: ${hat}`);
 
 
-
+console.log('=========================================================================');
 
 
 console.log('5. Print a sentence using the profile variables above, adding text where necessary for sentence flow, i.e.') 
-
+console.log('-----------------------');
 
 
 console.log(name + ' is a 50 year old mouse from ' + city + ' that loves '+ job + ' and eating ' + food + ' whilst wearing a ' +hat);
+
+console.log('=========================================================================');
