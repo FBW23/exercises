79e040537904986b5a93fa02bad8157680fec377
Bendis Hoxha <bendishoxha1@gmail.com>
2019-12-11 11:04:10 +0100

    done

---
+++ PB-data-types-pt1/var.js
@@ -0,0 +1,60 @@
+// 1.Declare a variable and assign a string as a value. Make sure the string is at least 6 characters long. Print the variable to the console.
+let altruist = 'Adam Sam';
+console.log(altruist);
+
+// 2.Access the 2nd and 6th elements of the string. Print the elements to the console. **Make sure you have selected the right index numbers** 
+console.log(altruist[1]);
+//d
+console.log(altruist[5]);
+//D
+
+
+// 3.3 Declare a variable called name and assign a string as a value. 
+
+let name= 'Altruistmus';
+console.log(name);
+
+
+// 4. Declare a variable called age and assign a number as a value. 
+
+let age=40;
+console.log(age);
+
+
+// 5. Declare a variable called year and assign a number as a value. 
+
+let year=1981;
+console.log(year);
+
+
+// 6. Print the following: "*name* is *age* in *year*", using your declared variables. 
+
+console.log(`${name} is ${age} and born in ${year}`);
+
+
+// 7. Declare a variable called isMarried and assign a boolean value. 
+
+let isMarried=true;
+console.log(isMarried);
+
+
+// 8. Print the following "*name* is married: true/false", using your declared variables.
+
+console.log(`${name} is married \:${isMarried}`)
+
+// 9. Declare a variable called programmingLanguage and assign the string "JavaScript". Declare a variable called isFun and give it a value of true.  Print "JavaScript is fun: true" to the console, using your declared variables. 
+
+let programmingLanguage= 'JavaScript';
+let isFun=true;
+console.log(`${programmingLanguage} is fun \:${isFun}`);
+
+
+
+// 10. Print the J and S of JavaScript from the variable programmingLanguage to the console. 
+
+console.log(programmingLanguage.substring(0,1))
+console.log(programmingLanguage.substring(4,5))
+
+
+// 11. Print the length of the string "JavaScript" to the console.
+console.log(programmingLanguage.length);
