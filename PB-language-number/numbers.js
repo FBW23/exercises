635b686bb313d9ab22f1828cb5fc86b0dd4b4d35
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2019-12-11 15:27:59 +0100

    finished exercise?

---
+++ PB-language-number/numbers.js
@@ -0,0 +1,32 @@
+console.log('');
+console.log('1. Create two variables. One variable should contain a string value and the other should contain a number. Concatenate the string and the number.');
+console.log('');
+let string = 'empest';
+let number = 7;
+console.log(number + string);
+console.log('');
+console.log('in this case the \"+\" works as a unifier and has no addiction function.');
+console.log('');
+
+console.log('');
+console.log('2a. Create a variable with the value of "1005". Convert it to a number.');
+console.log('');
+let stringIntDigit = "1005";
+const newIntNumber = parseInt(stringIntDigit);
+console.log(newIntNumber);
+console.log('');
+console.log('2b. Create a variable with the value of "10.05". Convert it to a number.');
+console.log('');
+let stringFloatDigit = "10.05";
+const newFloatNumber = parseFloat(stringFloatDigit);
+console.log(newFloatNumber);
+
+console.log('');
+console.log('3. Check if the following numbers are even numbers. Use a ternary and if the number is even print 30 is even, else print that it is odd.');
+console.log('');
+let isEven = 30;
+let isEven2 = 939;
+let isEven3 = 40.9;
+console.log(isEven%2==0 ? 'si' : 'no');
+console.log(isEven2%2==0 ? 'si' : 'no');
+console.log(isEven3%2==0 ? 'si' : 'no');
