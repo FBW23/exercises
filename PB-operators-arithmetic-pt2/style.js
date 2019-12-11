1b14154b6a2431e639c4db2325f52548abb80326
Naguib Schreiber <nagsster@hotmail.com>
2019-12-11 14:17:50 +0100

    new

---
+++ PB-operators-arithmetic-pt2/style.js
@@ -0,0 +1,26 @@
+let movie = 6450;
+let ticket = 15;
+console.log(movie/ticket) //430
+
+let Silvia = 500;
+let weekInYear = 52;
+console.log(500*weekInYear) //26000
+
+let a = 17;
+let b = 30;
+let c = '%';
+console.log(17/30*100+c);
+
+let square = 4;
+let side = 4.75;
+let size = 'cm2'
+console.log(square*side+size)
+
+
+let earth = 'Earth';
+let art = 'art';
+console.log(earth.includes(art)); //true: Earth - art = Eh
+console.log(earth.includes('sky')); //false case 
+console.log(earth)
+
+let chickenDay = "Wednesday"
---
+++ PB-strings-basic-methods/strings.js
@@ -0,0 +1,48 @@
+const park = "I can walk in the park all day!"
+console.log(park.substring(18, 22));
+
+const helloWorld = "Hello World"
+const upperCase = helloWorld.toUpperCase();
+console.log(upperCase);
+
+const helloEarth = "Hello Earthling"
+const lowercase = helloEarth.toLowerCase();
+console.log(lowercase);
+
+const js = "JavaScript"
+console.log(js.substring(3, 6));
+
+const shoes = "nice shoes"
+console.log(shoes.includes('n'));
+
+const fruit = "Bananas"
+const b = fruit[0].toUpperCase();
+const s = fruit[6].toUpperCase();
+console.log(`${b}${fruit}${s}`);
+
+const char1 = "Scritch"
+const char = char1.substring(4); 
+console.log(`${char}${char1}${char}`);
+
+const varian = "Oi, oi, oi!"
+console.log(varian.toUpperCase().includes('oi'));
+
+const bw = "BoogieWoogie"
+const middlebw = bw.substring(1,11);
+console.log(middlebw);
+const lastbw = bw.substr(11);
+const firstbw = bw.substring(0,1);
+console.log(`${lastbw}${middlebw}${firstbw}`);
+
+let me ="Nags",
+from = "Berlin",
+school = "DCI";
+console.log(`my name is ${me}. I am from ${from} and I am a Student of ${school}`);
+
+const quick = "the quick brown fox"
+const quick2 = quick.substring(1);
+console.log(quick[0].toUpperCase()+quick2);
+
+
+
+
