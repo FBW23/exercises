ccda7d60d13ec2a9326708cfce133497ae49fe18
Constantin <grizman040@gmail.com>
2019-12-11 11:04:32 +0100

    data-types

--- PB-data-types-pt1/data.js
+++ PB-data-types-pt1/data.js
@@ -1,9 +1,9 @@
-const name = 'constantin';
+let name = 'constantin';
 console.log(name);
-const second = name[1];
+let second = name[1];
 
 console.log(second);
-const sixth = name[5];
+let sixth = name[5];
 console.log(sixth);
 
 let age =  35;
@@ -12,6 +12,9 @@ console.log(`${name} is ${age} in ${year} `);
 const isMarried = true;
 console.log(`${name} is married \:${isMarried}`);
 
-let programmingLanguage = 'JavaScript';
-let isFun = true;
+const programmingLanguage = 'JavaScript';
+const isFun = true;
 console.log(`${programmingLanguage} is fun \:${isFun}`);
+console.log(programmingLanguage.substring(0,1));
+console.log(programmingLanguage.substring(4,5));
+console.log(programmingLanguage.length);
