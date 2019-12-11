61535bb22dc5921553e5b2ae514c3fce458d438b
jamil61 <jamisul6192@gmail.com>
2019-12-11 10:24:35 +0100

    JS

--- PB-ternary-exercises/dog.js
+++ PB-ternary-exercises/dog.js
@@ -16,11 +16,12 @@ console.log();
 let isDog;
 console.log(isDog? 'pat pat': 'find me a dog to pat');
 
-let speedCheck;
-console.log(speedCheck > 50 ? 'youre going too fast!': 'Youre driving below the speed limit, Oma');
+let speedCheck = 50; 
+let currentSpeed = 60;
+console.log(speedCheck < currentSpeed ? 'youre going too fast!': 'Youre driving below the speed limit, Oma');
 
-let age;
-console.log(age<=16? "serve butter beer": "serve beer");
+let age=16;
+console.log(age>=16? "serve butter beer": "serve beer");
 
 let isStudent;
 console.log(isStudent? "Ticket costs €5,00" : "Ticket costs €12,00");
@@ -28,3 +29,5 @@ console.log(isStudent? "Ticket costs €5,00" : "Ticket costs €12,00");
 let okMarie;
 console.log(okMarie? 'cake' : "Oh, bother");
 
+let message = "hello";
+console.log(message[length]);
