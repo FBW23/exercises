68b6790ffc1c516abbeb0b57b59be92cbcf12de0
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2019-12-17 11:27:44 +0100

    .

--- PB-language-math/langmath.js
+++ PB-language-math/langmath.js
@@ -10,7 +10,38 @@ console.log("");
 console.log("Create a program that prints a random **_integer_** from 1 - 6.");
 console.log("");
 let randomNumber = Math.random();
-console.log("random number between 0 and 6 ->", Math.ceil(randomNumber*6));
+console.log("random number between 0 and 6 ->", Math.ceil(randomNumber * 6));
 console.log();
-let randomNumber2 = Math.random() + 1;
-console.log("random number created with round ->", Math.round(randomNumber*6))
+console.log("6 SIDES DICE ROLL")
+let min = 1;
+let max = 6;
+console.log("Result is: ", Math.floor(Math.random() * (max - min + 1)) + min);
+console.log();
+// NUMBER PITFALLS
+
+console.log("typeof NaN is ->", typeof (NaN));
+console.log("is 23 not a number?", isNaN(23)); // boolean to determine if it is a number or not
+console.log("is a string not a number?", isNaN('bau')); // true - not a number
+console.log("is Infinity a number?", isNaN(1 / 0));
+console.log("one divided by zero gives:", 1 / 0); // infinity
+console.log("typeof Infinity ->", typeof (Infinity)); // number
+// e = 2.7;
+// pi = 3.14;
+console.log("shows the constants saved in the JS library ->", Math.E, Math.PI, Math.exp(5)); // 
+console.log("calculation with these constants ->", Math.E * Math.PI); // calculation with these constants.
+// DEALING WITH ROUNDING ERRORS
+console.log();
+console.log(Math.E / Math.PI);
+console.log(Math.round(Math.PI)); // cuts off all the digits after the point
+console.log("PI ->", Math.PI);
+console.log(Math.PI.toFixed(1)); // leaves one digit after the comma
+console.log(Math.PI.toFixed(2)); // money calculation - leaves two digits after the comma
+console.log(Number.prototype.toFixed(1));
+console.log(Math.PI.toFixed(20)); // more digits available!
+// toFixed will put out a string if we don't clculate afterwards
+console.log();
+console.log(typeof (-Infinity));
+console.log(Infinity - Infinity); // calculate NaN
+console.log(Infinity + Infinity); // Infinity
+console.log(-Infinity + Infinity); // NaN
+// calculation with infinity possible (but strange)
