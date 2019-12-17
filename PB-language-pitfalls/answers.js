f524d604893497c64872bd4d76c0f4d178d7c9f7
jamil61 <jamisul6192@gmail.com>
2019-12-17 11:33:50 +0100

    NaN

--- PB-language-pitfalls/answers.js
+++ PB-language-pitfalls/answers.js
@@ -3,7 +3,7 @@ console.log("NaN stands for 1-Not 2-A 3-Number");
 console.log(isNaN(3));
 
 console.log(typeof(NaN));
-
+console.log("the type of NaN is a number");
 console.log((0.1 * 0.2).toFixed(2));
 
 console.log(Infinity/0);
