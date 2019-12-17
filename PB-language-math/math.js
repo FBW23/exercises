3ef1bebb63e4726a20a5ae023d5d4cd428e6b264
Bendis Hoxha <bendishoxha1@gmail.com>
2019-12-17 10:13:06 +0100

    Done

---
+++ PB-language-math/math.js
@@ -0,0 +1,27 @@
+// 1 Minimum and Maximum
+
+console.log(Math.min(-1,0,1,2,3,4));
+// -1
+
+console.log(Math.max(-1,0,1,2,3,4));
+// 4
+
+// 2. Rounding
+// a.
+console.log(Math.ceil(3321.32321));
+console.log(Math.ceil(326.76));
+console.log(Math.ceil(76788.7));
+console.log(Math.ceil(-9.78));
+console.log(Math.ceil(43.342));
+
+// b.
+console.log(Math.floor(3321.32321));
+console.log(Math.floor(326.76));
+console.log(Math.floor(76788.7));
+console.log(Math.floor(-9,78));
+console.log(Math.floor(28.329));
+
+
+// 3.
+let randomNumber=Math.random();
+console.log(Math.ceil(randomNumber*6));
