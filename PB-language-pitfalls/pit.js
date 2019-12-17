f57fc5d983c87903a79d62f28c52d20300de613c
Naguib Schreiber <nagsster@hotmail.com>
2019-12-17 11:45:20 +0100

    pit

---
+++ PB-language-pitfalls/pit.js
@@ -0,0 +1,11 @@
+// NaN = Not a Number
+console.log(isNaN ("hello"));
+console.log(isNaN (3));
+// it is a property of the global object. The value of NaN is the same as the value of
+
+let b = 0.1*0.2;
+console.log(b.toFixed(2));
+
+console.log(Infinity/0);
+console.log(Infinity/Infinity);
+console.log(1/0);
