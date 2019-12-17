25976ebfdbbd32324bac1e707d1ef7a67a54e9a0
wagowsky <vagobala@gmail.com>
2019-12-17 10:04:55 +0100

    nospaces

--- PB-language-math/main.js
+++ PB-language-math/main.js
@@ -14,11 +14,11 @@ console.log(Math.max(-1,0,1,2,3,4));
 console.log('Round up the following number: 3321.32321')
 console.log(Math.ceil(3321.32321));
 console.log('Round up the following number:  326.76')
-console.log(Math.ceil( 326.76));
+console.log(Math.ceil(326.76));
 console.log('Round up the following number: 76788.7')
 console.log(Math.ceil(76788.7));
 console.log('Round up the following number: -9.78')
-console.log(Math.ceil( -9.78));
+console.log(Math.ceil(-9.78));
 console.log('Round up the following number: 43.342')
 console.log(Math.ceil(43.342));
 // ### b. Round down
@@ -27,11 +27,11 @@ console.log(Math.ceil(43.342));
 console.log('Round down the following number: 3321.32321')
 console.log(Math.floor(3321.32321));
 console.log('Round down the following number:  326.76')
-console.log(Math.floor( 326.76));
+console.log(Math.floor(326.76));
 console.log('Round down the following number: 76788.7')
 console.log(Math.floor(76788.7));
 console.log('Round down the following number: -9.78')
-console.log(Math.floor( -9.78));
+console.log(Math.floor(-9.78));
 console.log('Round down the following number: 43.342')
 console.log(Math.floor(43.342));
 // ## 3. Dice Roll!
