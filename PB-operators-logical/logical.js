d904d0a9047f446715fae8682a9b3e5a15f69432
Naguib Schreiber <nagsster@hotmail.com>
2019-12-17 14:15:00 +0100

    logic

---
+++ PB-operators-logical/logical.js
@@ -0,0 +1,27 @@
+let a = true;
+let b = false;
+let result = (a) && (b);
+console.log(result);
+let result2 = a || b;
+console.log(result2);
+let result3 = !(a&&b);
+console.log(result3);
+
+
+let x = 3;
+let y = 8;
+let z = 6;
+let result4 = (x > z) && (x > y);
+console.log(result4);
+let result5 = (x !== y);
+console.log(result5);
+let result6 = (z < y) || (z > x);
+console.log(result6);
+let result7 = (x === z) || (x !== y);
+console.log(result7); 
+let result8 = (x >=10) && (y <= 10);
+console.log(result8);
+let result9 = (x*z < 100) || (x*y > 100);
+console.log(result9);
+
+// well is getting to understand the movement of each variable.
