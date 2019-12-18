c3382419c45aea96726996277e5de5c982e3f881
jamil61 <jamisul6192@gmail.com>
2019-12-18 13:25:51 +0100

    please give only integers

--- PB-conditionals-pt1/cond.js
+++ PB-conditionals-pt1/cond.js
@@ -16,14 +16,18 @@ if ((50 >= c && c <= 99) || (50 >= d && d <= 99) || (50 >= e && e <= 99)) {
 }
 //3.
 let f = 25,
-    g = 20,
+    g = 39.4,
     h = 26;
-if (f > g && f > h) {
-    console.log(f);
-} else if (g > h) {
-    console.log(g);
+if (Number.isInteger(f) && Number.isInteger(g) && Number.isInteger(h)) {
+    if (f > g && f > h) {
+        console.log(f);
+    } else if (g > h) {
+        console.log(g);
+    } else {
+        console.log(h);
+    }
 } else {
-    console.log(h);
+    console.log('please give only integers');
 }
 
 //4.
@@ -40,53 +44,59 @@ let i = 40,
 let sum = i + j;
 if (sum >= 50 && sum <= 80) {
     console.log(65);
-} else if(sum>80){
+} else if (sum > 80) {
     console.log(80);
-}
-else{
+} else {
     console.log('below the range');
 }
-
 //6.
 let k = 12,
     l = 4;
 let Sum = k + l;
 let minus = k - l;
-
 if (Sum === 8 || minus === 8) {
     console.log(true);
 }
-
 //7.
-
 let m = 12,
-    n = 15;
+    n = 15.4;
 let SUm = m + n;
-if (m === 15 || n === 15 || SUm === 15) {
-    console.log(true);
+if (Number.isInteger(m) && Number.isInteger(n)) {
+    if (m === 15 || n === 15 || SUm === 15) {
+        console.log(true);
+    }
+} else {
+    console.log('please give only integers');
 }
 //8.
-
-let int1 = 70,
-    int2;
-
-if (int1 % 7 || int1 % 11 || int2 % 7 || int2 % 11) {
-    console.log(true);
+let int1 = 70.1,
+    int2 = 2.3;
+if (Number.isInteger(int1) && Number.isInteger(int2)) {
+    if (int1 % 7 || int1 % 11 || int2 % 7 || int2 % 11) {
+        console.log(true);
+    }
+} else {
+    console.log('please give an integer');
 }
-
 //9.
 let int3 = 5,
-    int4 = 5;
+    int4 = 5.4;
 let sum2 = int3 + int4;
-if (int3 === int4) {
-    console.log(3 * sum2);
+if (Number.isInteger(int3) && Number.isInteger(int4)) {
+    if (int3 === int4) {
+        console.log(3 * sum2);
+    } else if (Number.isInteger(int3) && Number.isInteger(int4) && Number.isInteger(sum2) && !(int3 === int4)) {
+        console.log(sum2);
+    }
 } else {
-    console.log(sum2);
+    console.log('please give an integer');
 }
 //10.
-let number = 20;
-if (number - 19 > 0) {
+let number = 20.6;
+if (Number.isInteger(number) && number - 19 > 0) {
     console.log((number - 19) * 2);
+} else {
+    console.log('please give an integer')
 }
 //11.
 let firstName = 'jamil',
