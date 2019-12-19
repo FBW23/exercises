2a36944cb2b32ed04b10f6506f37d01423c768ba
jamil61 <jamisul6192@gmail.com>
2019-12-19 10:32:10 +0100

    more explination

--- PB-program-blockscope/scope.js
+++ PB-program-blockscope/scope.js
@@ -1,5 +1,4 @@
-const determiner = -3;
-
+const determiner = NaN;
 if (determiner < 0) {
     var x;
     x = ' is smaller than 0';
@@ -12,12 +11,13 @@ let updater;
 if (determiner >= 0) {
     updater = 'greater or equal to 0';
     var message = 'Positive Integer';
+    console.log(message);
 } else {
     updater = 'less than 0';
 }
 console.log(updater);
 console.log(message);
-console.log(message + 'will print because the var is globally reachable no matter where it lives , if you give a value to determiner you will start getting string values for message');
+console.log(message + 'will print because the var is globally reachable no matter where it lives , if you give a value to determiner you will start getting string values for message , message will have an existence and a value only if the determiner is having a positive value; and it is undefined when determiner is negative');
 
 console.log('where you do not need more than one line of code and for not complicated statements and outputs , it is ok to use the ternary operator');
 console.log('example : let isCorrect= student ? "5euros fee" : "12euros fee"');
