1679a831f10415c8eb97c38a50456414900085c8
Naguib Schreiber <nagsster@hotmail.com>
2019-12-19 11:07:28 +0100

    block

---
+++ PB-program-blockscope/block.js
@@ -0,0 +1,25 @@
+let determiner = 5;
+
+if (determiner < 0) {
+    let x = "Less than 0";
+    console.log("Less than 0");
+} else {
+    let x = "Greater or equal to 0"
+    console.log("Greater or equal to 0");
+}
+
+if (determiner >= 0) {
+    var updater = "Greater or equal to 0";
+    let message = "Positive Integer";
+    console.log(message);
+
+} else {
+    update = "Less than 0"
+}
+
+// answer is Positive Integer
+//console.log(message); is indefinite because its outside the block
+
+
+let bmw = "15k" ? "expensive" : "cheaper";
+console.log(bmw);
