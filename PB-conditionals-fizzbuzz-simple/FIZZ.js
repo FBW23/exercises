027b4c94ec736c08e79665469c94725d9ac81fe0
jamil61 <jamisul6192@gmail.com>
2019-12-20 08:30:18 +0100

    LCM

---
+++ PB-conditionals-fizzbuzz-simple/FIZZ.js
@@ -0,0 +1,11 @@
+let x;
+if (x % 3) {
+    console.log('Fizz');
+} else if (x % 5) {
+    console.log('Buzz')
+} else if (x % 15) {
+    console.log('FizzBuzz')
+} else {
+    console.log(x);
+}
+//for short-handing the operation notice that it is enough for a number to be divided by the least common multiple of tow chosen numbers to be able to divide by any of them , in this case it is 15 ; 
