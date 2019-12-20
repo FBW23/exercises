bbf67d586882c26890f345ab976c5ae2585bfb58
jamil61 <jamisul6192@gmail.com>
2019-12-20 08:35:51 +0100

    proved

--- PB-conditionals-fizzbuzz-simple/FIZZ.js
+++ PB-conditionals-fizzbuzz-simple/FIZZ.js
@@ -9,3 +9,4 @@ if (x % 3) {
     console.log(x);
 }
 //for short-handing the operation notice that it is enough for a number to be divided by the least common multiple of tow chosen numbers to be able to divide by any of them , in this case it is 15 ; 
+console.log(225/15);
