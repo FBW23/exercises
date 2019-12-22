695261c03acd6733bd8470c853ca0aaae87a73ff
jamil61 <jamisul6192@gmail.com>
2019-12-22 09:15:18 +0100

    recursion order

--- PB-conditionals-fizzbuzz-simple/FIZZ.js
+++ PB-conditionals-fizzbuzz-simple/FIZZ.js
@@ -1,12 +1,10 @@
-let x;
-if (x % 3) {
-    console.log('Fizz');
-} else if (x % 5) {
-    console.log('Buzz')
-} else if (x % 15) {
-    console.log('FizzBuzz')
-} else {
-    console.log(x);
+let x=63;
+if(x%15===0){
+    console.log("FizzBuzz");
+}else if(x%3===0&&x%5){
+    console.log('buzz')
+}else{
+    console.log('fizz')
 }
 //for short-handing the operation notice that it is enough for a number to be divided by the least common multiple of tow chosen numbers to be able to divide by any of them , in this case it is 15 ; 
 console.log(225/15);
