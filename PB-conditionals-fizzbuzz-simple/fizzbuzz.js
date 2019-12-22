279f518cf1457dbd784bdab730086c5bd06c56da
Constantin <grizman040@gmail.com>
2019-12-22 02:21:44 +0100

    FuzzBuzz Button HTML&CSS&JS

--- PB-conditionals-fizzbuzz-simple/fizzbuzz.js
+++ PB-conditionals-fizzbuzz-simple/fizzbuzz.js
@@ -1,14 +1,21 @@
-console.log("NO Function ---Reload The Pae To GENERATE NUMBER");
 
-let fizzBizz = Math.floor(Math.random() * 50) + 1;
-if ((fizzBizz % 3 === 0) && (fizzBizz % 5 === 0)) {
-    console.log("FizzBuzz");
-} else if (fizzBizz % 5 === 0) {
-    console.log("Buzz");
+function fuzzBuzz() {
+    var fuzz = document.getElementById("result");
+    var number = Math.floor(Math.random() * 50) + 1;
+    var three = "Fuzz";
+    var five = "Buzz";
+    if (number % 3 === 0 && number % 5 === 0) {
+        fuzz.innerHTML = (number+" "+three+five);
+        // document.getElementById("buzzMe").style.backgroundColor = green;
+    } else if (number % 3 === 0) {
 
-} else if (fizzBizz % 3 === 0) {
-    console.log("Fizz");
-
-} else {
-    console.log(fizzBizz);
+        fuzz.innerHTML = (number+" "+three);
+        // document.getElementById("buzzMe").style.backgroundColor = red;
+    } else if (number % 5 === 0) {
+        fuzz.innerHTML = (number+" "+five);
+    } else {
+        fuzz.innerHTML = number;
+        // document.getElementById("buzzMe").style.backgroundColor = blue;
+ 
+    }
 }
