3530c34f941f8c9ac33f5ea40ee2fcc9c3ca0eca
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2020-01-06 14:51:21 +0100

    erased the === 0

--- PB-conditionals-fizzbuzz-simple/fizzbuzz.js
+++ PB-conditionals-fizzbuzz-simple/fizzbuzz.js
@@ -1,13 +1,24 @@
 console.log();
-let number = 45;
+let number = 15;
 
-if (((number % 3) === 0) && ((number % 5) === 0)) {
+if ((number % 3 === 0) && (number % 5 === 0)) {
     console.log('FizzBuzz')
-} else if ((number % 3) === 0) {
+} else if (number % 3 === 0) {
     console.log('Fizz');
-} else if ((number % 5) === 0) {
+} else if (number % 5 === 0) {
     console.log('Buzz')
 } else {
     console.log(number);
 }
 
+// BONUS
+
+if (!(number % 3) && !(number % 5)) {
+    console.log('FizzBuzz')
+} else if (!(number % 3)) {
+    console.log('Fizz');
+} else if (!(number % 5)) {
+    console.log('Buzz');
+} else {
+    console.log(number);
+}
