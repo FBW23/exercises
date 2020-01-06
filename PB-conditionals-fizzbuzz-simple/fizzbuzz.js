8b24745659ca6944017078ed35c05cffbe4ef2f8
Julien Montagnon <jumontagnon@gmail.com>
2020-01-06 15:32:40 +0100

    bla

--- PB-conditionals-fizzbuzz-simple/fizzbuzz.js
+++ PB-conditionals-fizzbuzz-simple/fizzbuzz.js
@@ -1,9 +1,9 @@
-let myNumber = 4;
-if (((myNumber % 3) == 0) && ((myNumber % 5) == 0)) {
+let myNumber = 15;
+if ((myNumber % 3 === 0) && (myNumber % 5 === 0)) {  // or (myNumber % 15 === 0)
     console.log('FizzBuzz');
-} else if ((myNumber % 3) == 0) {
+} else if (!(myNumber % 3)) {
     console.log('Fizz');
-} else if ((myNumber % 5) == 0) {
+} else if (!(myNumber % 5)) {
     console.log('Buzz');
 } else {
     console.log(myNumber);
