30aedc87b7add9bf06cbb8050fd05cce29443c60
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2020-01-06 14:21:48 +0100

    fizzbuzz corrected

--- PB-conditionals-fizzbuzz-simple/fizzbuzz.js
+++ PB-conditionals-fizzbuzz-simple/fizzbuzz.js
@@ -1,12 +1,13 @@
 console.log();
 let number = 15;
 
-if ((number % 3) === 0) {
+if (((number % 3) && (number % 5)) === 0) {
+    console.log('FizzBuzz')
+} else if ((number % 3) === 0) {
     console.log('Fizz');
 } else if ((number % 5) === 0) {
     console.log('Buzz')
-} else if (((number % 3) && (number % 5)) === 0) {
-    console.log('FizzBuzz')
 } else {
     console.log(number);
 }
+
