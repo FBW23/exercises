2792e07b0c28e509cc9ae0e200e1372b39384813
Constantin <grizman040@gmail.com>
2020-01-07 11:39:24 +0100

    do while

---
+++ PB-program-repetition/loop.js
@@ -0,0 +1,26 @@
+console.log('-------------1-----------');
+let i = 0;
+while (i < 15) {
+    i++;
+}
+console.log(i);
+console.log('-------------2-----------');
+let krokodile = 1;
+let cat = 0;
+while (cat < 20) {
+    cat++;
+    krokodile = krokodile + cat;
+    console.log(krokodile);
+}
+console.log(krokodile);
+console.log('--------------3----------');
+
+let myNumber = 'The number is ';
+let b = 0;
+do {
+    b = b + 1;
+    myNumber += b;
+    console.log(myNumber);
+} while (b < 20);
+console.log(b);
+
