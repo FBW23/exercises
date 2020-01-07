ff5e99d281254a884e020b137becd22a89ed1c02
Julien Montagnon <jumontagnon@gmail.com>
2020-01-07 11:03:04 +0100

    repetition

---
+++ PB-program-repetition/repetition.js
@@ -0,0 +1,35 @@
+// ## 1. Run Along
+// Create a `while` loop that runs as long as the variable `i` is less than 15. Print `i`.
+
+let i = 0;
+
+while (i < 15) {
+    i++;
+}
+console.log(i);
+
+// ## 2. Add it up 
+// Create a function that adds numbers from 1-20 together using a `while` loop. 
+
+
+let sum = 0;
+
+while (i < 20) {
+    i++;
+    sum = sum + i;
+}
+console.log(sum);
+
+
+
+
+// ## 3. Do this while i...
+// Use a `do`, `while` loop to print `The number is [i]` while i is less than 20. 
+
+let result = 'The number is ';
+
+do {
+    i++;
+    result = result + i;
+} while (i < 20);
+console.log(result);
