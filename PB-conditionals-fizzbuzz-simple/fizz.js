68c3e577bbe938933f13471d3c83c027a7112227
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-01-07 11:29:41 +0100

    done

--- PB-conditionals-fizzbuzz-simple/fizz.js
+++ PB-conditionals-fizzbuzz-simple/fizz.js
@@ -1,9 +1,10 @@
-let MyNumber=12;
-if (MyNumber % 3 === 0) {
-    console.log('Fizz');
-} else  if  (MyNumber  % 5 === 0) {
-    console.log('buzz');
-} else if ((MyNumber  % 3 ===  0) && (MyNumber %5 === 0)) {
+let MyNumber = 25;
+if (MyNumber % 15 === 0) {
     console.log('FizzBuzz');
-}
-else {console.log(MyNumber);}
+} else if (MyNumber % 3 === 0) {
+    console.log('Fizz');
+} else if (MyNumber % 5 === 0) {
+        console.log ('Buzz');
+    } else {
+        console.log(MyNumber);
+    }
