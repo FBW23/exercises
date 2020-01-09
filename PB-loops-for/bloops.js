0109816e09f7f8d5d1cda08659d9b41042d58a9f
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-01-09 11:39:52 +0100

    just the 2nd

---
+++ PB-loops-for/bloops.js
@@ -0,0 +1,21 @@
+
+let i=1;
+let sum=0;
+
+// while(i<=20){
+//     sum=sum+i;
+//     console.log(i);
+//     console.log(sum)
+//     i++;
+// }
+// console.log(sum);
+
+for (let i=0;i<=5;i++){
+    console.log('There is '+i + ' bottle(s) on the wall'); 
+}
+
+
+
+// There are i bottles of beer on the wall. Write a program that will output, "There is one bottle of beer on the wall." "There are two bottles of beer on the wall" up until there are five bottles.
+
+
