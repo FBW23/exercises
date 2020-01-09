3747cd2aeacfa31273a47267bd340d1360340a98
Bianca Richa <biancaricha@gmail.com>
2020-01-09 11:23:08 +0100

    fix errors

---
+++ PB-conditionals-fizzbuzz-simple/fizzbuzz.js
@@ -0,0 +1,11 @@
+let num = 30;
+
+if((num % 3 === 0) && (num % 5 === 0)) {
+    console.log("FizzBuzz");
+} else if (num % 3 === 0) { 
+    console.log("Fizz");
+} else if (num % 5 === 0) {
+    console.log("Buzz");
+} else {
+    console.log(num); 
+}
