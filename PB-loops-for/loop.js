c9b7cd40a6814680eb9c2e1c56eef82abe805069
Naguib Schreiber <nagsster@hotmail.com>
2020-01-09 11:53:01 +0100

    loopy

---
+++ PB-loops-for/loop.js
@@ -0,0 +1,26 @@
+let i = "bottle of the wall";
+let sum = 0;
+
+while (i < 5) {
+    sum = sum + 1;
+    console.log(i);
+    i++;
+
+}
+for (i = 1; i <= 5; i++) {
+    if (i === 1) {
+        console.log("There is One Bottle of Beer on the Wall");
+
+    } else if (i === 2) {
+        console.log("There is Two Bottle of Beer on the Wall");
+    } else if (i === 3) {
+        console.log("There is Three Bottle of Beer on the Wall");
+    } else if (i === 4) {
+        console.log("There is Four Bottle of Beer on the Wall");
+    } else if (i === 5) {
+        console.log("There is Five Bottle of Beer on the Wall");
+    }
+    // console.log("There is " + i + " Bottle of Beer on the Wall");
+}
+
+console.log("...........................................");
