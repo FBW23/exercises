7422f7f440c69c35500ef6fcc03dbc4f0b3a30ac
Bianca Richa <biancaricha@gmail.com>
2020-01-09 11:40:12 +0100

    bottle loops

---
+++ PB-loops-for/loops.js
@@ -0,0 +1,21 @@
+// 1. **Addition.** 
+// Write a program to add up the numbers 1 to 20.
+
+let sum;
+for (let i = 1; i <= 20; i++) {
+    console.log(i)
+}
+
+// 2. **There are i bottles of beer on the wall.** 
+// Write a program that will output, "There is one bottle of beer on the wall." 
+//"There are two bottles of beer on the wall" up until there are five bottles. 
+
+
+for (let j = 1; j <= 5; j++) {
+    console.log("There is " + j + " bottles of beer on the wall");
+}
+
+// 3. **The odd/even reporter.**
+// Write a program that will iterate from 0 to 20. 
+// For each iteration, it will check if the current number is even or odd, 
+// and report that to the screen (e.g. "2 is even").
