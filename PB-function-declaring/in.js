a648cd5dfa653e1dbff2d03e2342a840a986278f
jamil61 <jamisul6192@gmail.com>
2020-01-14 11:07:59 +0100

    Arrow

---
+++ PB-function-declaring/in.js
@@ -0,0 +1,20 @@
+function multi(num1, num2) {
+    console.log(num1 * num2)
+};
+//-------------------------------
+const MULTI = function (num1, num2) {
+    console.log(num1 * num2);
+};
+//----------------------------------------
+const ARROW = (num1, num2) => console.log(num1 * num2);
+
+///---------------------------------------------
+function modu(num1, num2) {
+    console.log(num1 % num2);
+}
+
+const MODU = function (num1, num2) {
+    console.log(num1 % num2);
+};
+
+const ARROW2 = (num1, num2) => console.log(num1 % num2);
