6d2f5261e5796c9252c819ca3874607db54d71c4
jamil61 <jamisul6192@gmail.com>
2020-01-14 14:05:07 +0100

    scope

--- PB-function-scope/index.js
+++ PB-function-scope/index.js
@@ -1,29 +1,30 @@
 const EXP = function (num1, num2) {
     if (Object.prototype.toString.call(num1) === '[object Number]' && Object.prototype.toString.call(num2) === '[object Number]') {
         for (var i = 1; i <= num2; i++) {
-            console.log(Math.pow(num1, i))
+            console.log(Math.pow(num1, i));
         }
     } else {
-        console.log('please use only numbers')
+        console.log('please use only numbers');
     }
-
 }
-console.log(Object.prototype.toString.call(9))
-EXP(2, 2);
+console.log(Object.prototype.toString.call(9));
+EXP(2, 5);
 
 
 //----------------------
-let myFavoriteFruit='orange';
-const printFavoriteFruit=function(){
-    myFavoriteFruit='apple';
-    console.log("My favorite fruit is: "+myFavoriteFruit);
-}
+let myFavoriteFruit = 'orange';
+const printFavoriteFruit = function () {
+    myFavoriteFruit = 'apple';
+    console.log("My favorite fruit is: " + myFavoriteFruit);
+};
 
 printFavoriteFruit();
 
 //---------------------------------
 
-const exponent=(num1,num2)=>{let result=Math.pow(num1,num2)};
-console.log(exponent(2,4));
+const exponent = (num1, num2) => {
+    let result = Math.pow(num1, num2);
+};
+console.log(exponent(2, 4));
 // console.log(result);
 console.log('trying to access the variable \"result\" will detect a break in the code cause you may never access a locally scoped variable from a global scale \" like in the case of being out side the function\"')
