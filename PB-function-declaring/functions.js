664c1ed6e6baa938fb63e81d64dc45725e34b3cc
Bendis Hoxha <bendishoxha1@gmail.com>
2020-01-14 11:36:21 +0100

    Done

--- PB-function-declaring/functions.js
+++ PB-function-declaring/functions.js
@@ -3,10 +3,10 @@
 // #### 1. Multiply - Function Declaration
 // * Create a function that multiples a number by another number.
 
-function Multiply(x, y) {
+function multiply(x, y) {
     console.log(x * y);
 };
-
+multiply(5, 6);
 
 // #### 2. Multiply - Function Declarations as Values
 // * Rework the syntax of the above function so that the function declaration is stored as a value.
@@ -15,6 +15,8 @@ const MULTIPLY = function (x, y) {
     console.log(x * y);
 };
 
+MULTIPLY(5, 7);
+
 // #### 3. Multiply - Arrow Function
 // * Rework the syntax of the function declaration so that is uses the arrow function shorthand.
 
@@ -22,17 +24,24 @@ const ARROW = (x, y) => {
     console.log(x * y);
 };
 
-
+ARROW(7, 9);
 
 // #### 4. Declarations
 // * Create functions (using all three declarations) to check the remainder of division given two numbers. 
-function Alt(a, b) {
+function alt(a, b) {
     console.log(a % b);
 };
+
+alt(9, 3);
 
 const ALT = function (a, b) {
     console.log(a % b);
 };
+
+ALT(2, 8);
+
 const ARROW0 = (a, b) => {
     console.log(a % b);
 };
+
+ARROW0(6, 9);
