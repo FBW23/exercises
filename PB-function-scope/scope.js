e6d0e23ec8277666f597680662c4bc279e811744
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2020-01-14 15:23:01 +0100

    cmmtt

---
+++ PB-function-scope/scope.js
@@ -0,0 +1,44 @@
+console.log();
+console.log('Exercise One');
+console.log();
+
+let multiply = 0;
+
+function twoNumbers(firstNum, secondNum) {
+    if (((typeof firstNum) === 'number') && ((typeof secondNum) === 'number')) {
+        console.log(firstNum + ' and ' + secondNum + ' are both numbers.');
+    } else {
+        console.log('not a number');
+    }
+    for (let i = 1; i <= secondNum; i++) {
+        console.log(Math.pow(firstNum, i));
+    }
+}
+
+twoNumbers(3, 5);
+
+console.log();
+console.log('Exercise Two');
+console.log();
+
+let frucht = 'oranges';
+
+function printFavoriteFruit() {
+    frucht = 'pear';
+    console.log('My favorite fruit is:', frucht);
+}
+
+printFavoriteFruit();
+
+console.log();
+console.log('Exercise Three');
+console.log();
+
+function exponent(numberOne, numberTwo) {
+    for (i = 0; i < numberTwo; i++) {
+        let result = numberOne * numberOne;
+        console.log(result);
+    }
+}
+// console.log(result); this is not possible because the result is defined as a local variable
+exponent(5, 5);
