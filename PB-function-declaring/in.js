90fda770e1db2ed813b0dc55131745972f98e854
jamil61 <jamisul6192@gmail.com>
2020-01-14 11:39:52 +0100

    Called

--- PB-function-declaring/in.js
+++ PB-function-declaring/in.js
@@ -1,20 +1,25 @@
 function multi(num1, num2) {
     console.log(num1 * num2)
-};
+}
+
+multi(8,9);
 //-------------------------------
 const MULTI = function (num1, num2) {
     console.log(num1 * num2);
 };
+MULTI(8,9);
 //----------------------------------------
 const ARROW = (num1, num2) => console.log(num1 * num2);
-
+ARROW(8,9);
 ///---------------------------------------------
 function modu(num1, num2) {
     console.log(num1 % num2);
 }
-
+modu(8,9);
 const MODU = function (num1, num2) {
     console.log(num1 % num2);
 };
+MODU(8,9);
 
 const ARROW2 = (num1, num2) => console.log(num1 % num2);
+ARROW2(9,8);
