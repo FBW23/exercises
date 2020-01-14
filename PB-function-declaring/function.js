48b424292ad58c341427336c8f58b57cfaf4f923
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2020-01-14 11:35:31 +0100

    committing the final exercise

--- PB-function-declaring/function.js
+++ PB-function-declaring/function.js
@@ -1,12 +1,12 @@
 function multiplyByNumber(number, number2) {
     console.log('Create a function that multiples a number by another number >', number * number2);
 }
-multiplyByNumber(5, 5);
+multiplyByNumber(5, 65);
 
 const myFunction = function multiplyByNumber(number, number2) {
     console.log('Rework the syntax of the above function so that the function declaration is stored as a value >', number * number2);
 };
-myFunction(5, 5);
+myFunction(45, 45);
 
 const myArrowFunction = (number, number2) => {
     console.log('Rework the syntax of the function declaration so that is uses the arrow function shorthand >', number * number2);
@@ -15,27 +15,27 @@ myArrowFunction(5, 5);
 
 function remainder(number, number2) {
     if (number % number2 === 0) {
-        console.log('it is divisible by 5.');
+        console.log(number + ' is divisible by ' + number2);
     } else {
-        console.log('it is not divisible by 5.')
+        console.log(number + ' is not divisible by ' + number2);
     }
 }
 remainder(24, 34);
 
 const remainder2 = function remainder(number, number2) {
     if (number % number2 === 0) {
-        console.log('it is divisible by 5.');
+        console.log(number + ' is divisible by ' + number2);
     } else {
-        console.log('it is not divisible by 5.');
+        console.log(number + ' is not divisible by ' + number2);
     }
 };
 remainder2(300, 50);
 
 const remainder3 = (number, number2) => {
     if (number % number2 === 0) {
-        console.log('it is divisible by 5.');
+        console.log(number + ' is divisible by ' + number2);
     } else {
-        console.log('it is not divisible by 5.');
+        console.log(number + ' is not divisible by ' + number2);
     }
 };
 remainder3(70, 5);
