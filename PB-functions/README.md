931b7dc7086ef67803c6047715095825ab17a162
Bendis Hoxha <bendishoxha1@gmail.com>
2020-01-15 15:29:30 +0100

    done

--- PB-functions/README.md
+++ PB-functions/README.md
@@ -57,7 +57,7 @@ i.e.
  dogAge(4) ➞ "Your doggo is 28 years old in dog years!"
 
 **8. A Lifetime Supply...**
-Create a function to calculate how much you'll actually be getting for a lifetime supply of your favourite snack. The function should accept two arguments: age and amount per day. The function should calculate the amount that will be consumed for the rest of your life given a constant max age. **Bonus** Accept floating point values for amount per day and round the result. 
+Create a function to calculate how much you'll actually be getting for a lifetime supply of your favorite snack. The function should accept two arguments: age and amount per day. The function should calculate the amount that will be consumed for the rest of your life given a constant max age. **Bonus** Accept floating point values for amount per day and round the result. 
 
 i.e. 
 
--- PB-functions/return.js
+++ PB-functions/return.js
@@ -1,5 +1,4 @@
-// **1. Add Up.**
-
+console.log(`..............................Task 1...............`)
 const addUP = (a) => {
     let sum = 0;
     for (let i = 1; i <= a; i++) {
@@ -12,40 +11,109 @@ console.log(addUP(4));
 console.log(addUP(13));
 console.log(addUP(600));
 
-// **2. Cubed.**
-
+console.log(`..............................Task 2...............`)
 const sumOfCubes = (s, d, f) => {
 
-    if (s && d && f) {
-        return (Math.pow(s, 3) + Math.pow(d, 3) + Math.pow(f, 3));
-    } else if (s && d) {
-        return (Math.pow(s, 3) + Math.pow(d, 3));
-    } else if (s) {
-        return (Math.pow(s, 3));
-    } else {
-        return 0;
+    let sum1 = 0;
+
+    if (s) {
+        sum1 += Math.pow(s, 3);
     }
+    if (d) {
+        sum1 += Math.pow(d, 3);
+    }
+    if (f) {
+        sum1 += Math.pow(f, 3);
+    }
+    return sum1;
+    // if (s && d && f) {
+    //     return (Math.pow(s, 3) + Math.pow(d, 3) + Math.pow(f, 3));
+    // } else if (s && d) {
+    //     return (Math.pow(s, 3) + Math.pow(d, 3));
+    // } else if (s) {
+    //     return (Math.pow(s, 3));
+    // } else {
+    //     return 0;
+    // }
 }
 console.log(sumOfCubes(1, 5, 9));
 console.log(sumOfCubes(2));
 console.log(sumOfCubes());
 
 
-// **3. String Check.**
-
-
-
-// **4. Less Than or Equal to Zero?**
-
-const lessThanOrEqualToZero = (num) => {
-    if (num <= 0) {
+console.log(`..............................Task 3...............`)
+const stringCheck = (check1, check2) => {
+    if (check2.startsWith(check1) === true) {
         return true;
     } else {
         return false;
     }
 }
+console.log(stringCheck("bu", "button")); // true
+console.log(stringCheck("tri", "triplet")); // true
+console.log(stringCheck("beau", "pastry")); // false
 
-console.log(lessThanOrEqualToZero(3));
-console.log(lessThanOrEqualToZero(0));
-console.log(lessThanOrEqualToZero(-4));
-console.log(lessThanOrEqualToZero(10));
+// second solution short one
+const cubed3 = (n, m) => n.startsWith(m) ? true : false;
+console.log(stringCheck("beau", "pastry")); // false
+
+console.log(`..............................Task 4...............`)
+const lessThanOrEqualToZero = (num) => num <= 0;
+//  {
+//     if (num <= 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+console.log(lessThanOrEqualToZero(3)); // false
+console.log(lessThanOrEqualToZero(0)); // true
+console.log(lessThanOrEqualToZero(-4)); // true
+console.log(lessThanOrEqualToZero(10)); // false
+
+console.log(`..............................Task 5...............`)
+
+function countOccurrences(string1, letter) {
+    let sumCount = 0;
+    for (let position = 0; position < string1.length; position++) {
+        if (string1.charAt(position) === letter) {
+            sumCount += 1;
+        }
+    }
+    return sumCount;
+}
+
+console.log(countOccurrences("this is a string", "i")); //3
+
+
+console.log(`..............................Task 6...............`)
+const calculateBaseToExponent = (basenumber, exponentnumber) => {
+    return Math.pow(basenumber, exponentnumber);
+};
+
+
+console.log(calculateBaseToExponent(5, 5)); // 3125
+console.log(calculateBaseToExponent(10, 10)); // 10000000000
+console.log(calculateBaseToExponent(3, 3)); // 27
+
+
+console.log(`..............................Task 7..............................`)
+
+const dogAge = (years) => {
+    return (years * 7);
+}
+
+console.log(dogAge(4));
+
+
+console.log(`..............................Task 8............................`)
+
+const calcSupply = (age, amount) => ((80 - age) * 365 * (amount));
+
+console.log(calcSupply(25, 2));
+console.log(calcSupply(40, 3));
+
+console.log(`..............................Task 9............................`)
+
+
