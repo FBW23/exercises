75b35946685c6a3081cc170b45b2b65394714144
Bianca Richa <biancaricha@gmail.com>
2020-01-15 13:45:21 +0100

    string check 2

--- PB-functions/function.js
+++ PB-functions/function.js
@@ -60,14 +60,25 @@ console.log(cubes);
 
  
 const wordFunction = function(c, d) {
-    let word = c.charAt(0); // "b"
-    let string = d.charAt(0);
-    if (string === word) {
+
+    if (d.includes(c)) {
         return true
     } else {
         return false;
     }
 }
 
-const wordFunction2 = wordFunction("bianca", "richa"); //print false
+const wordFunction2 = wordFunction("bi", "richa"); //print false
 console.log(wordFunction2);
+
+//================================================================================
+
+// 4. Less Than or Equal to Zero? Create a function that takes a number as its only argument 
+//and returns true if it's less than or equal to zero, otherwise return false.
+
+
+// Examples:
+// lessThanOrEqualToZero(3) ➞ false
+// lessThanOrEqualToZero(0) ➞ true
+// lessThanOrEqualToZero(-4) ➞ true
+// lessThanOrEqualToZero(10) ➞ false
