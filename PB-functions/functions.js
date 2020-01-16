e807e87fbfe45672c0ec0dc8c05a1937dfeae05e
Nicolo Tommasi <nicolo.tommasi@gmail.com>
2020-01-16 09:30:59 +0100

    cmmtt

--- PB-functions/functions.js
+++ PB-functions/functions.js
@@ -81,12 +81,84 @@ console.log();
 console.log('**4. Less Than or Equal to Zero?**');
 console.log();
 
-const lessThanOrEqualToZero = (a) => a <= 0 ? true : false;
+const lessThanOrEqualToZero = (a) => a <= 0;
 
-let result4 = lessThanOrEqualToZero(-3);
-console.log(result4);
+let result4 = lessThanOrEqualToZero(3);
+console.log(result4); // expected true
 
 console.log();
 console.log('**5. Count Occurrences.**');
 console.log();
 
+const countOccurrences = (string, letter) => {
+    let letterPosition = 0;
+    let matchingSpot = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string.indexOf(letter, firstMatch) > -1) {
+            letterPosition++;
+            matchingSpot = string.indexOf(letter, matchingSpot) + 1;
+        }
+    } return letterPosition;
+}
+let result5 = countOccurrences('precipitelissimevolmente', 'e');
+console.log(countOccurrences);
+
+
+console.log();
+console.log('**6. X To The Power of X.**');
+console.log();
+
+const calculateBaseToExponent = (a) => {
+    return Math.pow(a, a);
+};
+
+console.log(calculateBaseToExponent(5));
+console.log(calculateBaseToExponent(10));
+console.log(calculateBaseToExponent(3));
+
+console.log();
+console.log('**7. Dog Years.**');
+console.log();
+
+const dogAge = (a) => {
+    return a * 7;
+}
+
+let finalAge = dogAge(5);
+console.log(`Your dog is ${finalAge} years old in dog years!`);
+
+console.log();
+console.log('**8. A Lifetime Supply...**');
+console.log();
+
+const calcSupply = (presentAge, numberOfSnacks) => {
+    let amountSoFar = presentAge * numberOfSnacks * 365;
+    let lifetimeAmount = 80 * 365 * numberOfSnacks;
+    return lifetimeAmount - amountSoFar;
+}
+
+let result8 = calcSupply(37, 2);
+console.log('You will need ' + result8 + ' chocolate bars (3 a day) to last you til the age of 80.');
+
+console.log();
+console.log('**9. Where\'s Waldo?**');
+console.log();
+
+const isWaldoHere = (str) => {
+    let strLower = str.toLowerCase();
+    if (strLower.includes('waldo')) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+ isWaldoHere("is there wal here ?");
+ console.log('is there wal here? ->', isWaldoHere("is there wal here ?"));
+ console.log("I found you Waldo! ->", isWaldoHere("I found you Waldo!"));
+ console.log("is wally here? ->", isWaldoHere("is wally here?"));
+ console.log("waldo is here ->", isWaldoHere("waldo is here"));
+
+ console.log();
+ console.log('**10. Pie.**');
+ console.log();
