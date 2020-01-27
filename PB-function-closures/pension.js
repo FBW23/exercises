8c99cb40426d2757c9090a0d293415d859c41e08
o0pmitev <pmitev89@gmail.com>
2020-01-27 13:45:18 +0100

    second commit

--- PB-function-closures/pension.js
+++ PB-function-closures/pension.js
@@ -1,3 +1,22 @@
+// add
+function add(a) {
+    return function(b) {
+        return a + b;
+    }
+    
+}
+
+// Multiplier
+const multiplier = function(a) {
+    return function(b) {
+        return a * b;
+    }; // closing the return from  line 26
+}; // Closing the const statement thats why we need ;
+
+console.log(multiplier(2)(2));
+console.log(multiplier(3)(4));
+
+// Pension
 (function(age,retAge,mWage,percentage) {
     
     let result;
