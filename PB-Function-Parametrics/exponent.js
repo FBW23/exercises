27e93b6d1ce36d4a7786643c6e46c2d0c66b1df6
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-01-27 14:55:20 +0100

    cmmtt

--- PB-Function-Parametrics/exponent.js
+++ PB-Function-Parametrics/exponent.js
@@ -2,7 +2,7 @@
 //
 // Example: Calling the function with 2 and 4 will return 8 (2 to the power of 4), If you call it with just 2, it should return 4 (2 to the power of 2).
 
-const exponent = (num, exp) => {
+const exponent = (num, exp = 2) => {
   let result = 1;
 
   for (let i = 0; i < exp; i++) {
--- PB-Function-Parametrics/twofer.js
+++ PB-Function-Parametrics/twofer.js
@@ -1,8 +1,13 @@
 // Complete the function below so that it returns "Two for me and one for you" when no arguments are passed
 
-const twofer = who => {
+// const twofer = who => {
+//   return `Two for me and one for ${who}`;
+// };
+
+const twofer = (who = 'you') => {
   return `Two for me and one for ${who}`;
 };
+
 
 console.log(twofer("Fran")); // -> "Two for me and one for Fran"
 console.log(twofer()); // -> "Two for me and one for you"
