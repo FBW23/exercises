b8efa6e79d63edf6e2d6f282aa38c2fdc48da201
nagster23 <nagsster.s@gmail.com>
2020-01-27 15:41:11 +0100

    sum

--- PB-Function-Parametrics/sum.js
+++ PB-Function-Parametrics/sum.js
@@ -1,6 +1,14 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+
+
+const sum = (...args) => {
+    let sum1 = 0;
+    for (let i = 0; i < args.length; i++) {
+        sum1 += args[i];
+    }
+    return sum1
+};
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
