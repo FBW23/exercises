797142269edcaf68e36955910b7201fba15d0564
wagowsky <vagobala@gmail.com>
2020-01-27 15:07:12 +0100

    expo

--- PB-Function-Parametrics/sum.js
+++ PB-Function-Parametrics/sum.js
@@ -1,6 +1,13 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+        
+    }
+    return total;
+};
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
