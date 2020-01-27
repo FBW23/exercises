93a0bf6b5979b1962e7719403d7c2ade23624513
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-01-27 18:26:53 +0100

    done

--- PB-Function-Parametrics/sum.js
+++ PB-Function-Parametrics/sum.js
@@ -1,7 +1,16 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = (...args) => args+args{
-     return  };
+const sum = (...args) =>{
+    let total=0;
+for(let i = 0; i < args.length;i++) {
+     total += args[i]; // just adding the total of arguments
+     
+}; 
+return total // total of arguments divided  by the number of arguments
+};
+
+
+// total of arguments add  by the number of arguments
 
 console.log(sum(1)); // -> 1
 console.log(sum(1, 15)); // -> 16
