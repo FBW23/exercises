71671ee52c87d9fba8b59a580f467233bf044438
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-01-27 15:08:57 +0100

    cmmtt

--- PB-Function-Parametrics/exponent.js
+++ PB-Function-Parametrics/exponent.js
@@ -3,12 +3,19 @@
 // Example: Calling the function with 2 and 4 will return 8 (2 to the power of 4), If you call it with just 2, it should return 4 (2 to the power of 2).
 
 const exponent = (num, exp = 2) => {
-  let result = 1;
+  let result = 1; // starts from one
 
-  for (let i = 0; i < exp; i++) {
-    result *= num;
+  for (let i = 0; i < exp; i++) { // i < exp (second argument = exp = 3)
+    // * for the first case *
+    // first loop result = 3 (num) * 1 (result) = 3
+    // second loop result = 3 (num) * 3 (result) = 9
+    // third loop result = 3 (num) * 9 (result) = 27
+
+    // * for the second case * exp = 2 set to default
+    // first loop result = 3 (num) * 1 (result) = 3
+    // second loop result = 3 (num) * 3 (result) = 9
+    result = num * result;
   }
-
   return result;
 };
 
