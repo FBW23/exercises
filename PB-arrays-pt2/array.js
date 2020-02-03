71190c1882fd9fd0a3f7332e387d3e493c03fc47
DCI-REC107 <Julia pim>
2020-02-03 22:48:20 +0100

    exercicies

--- PB-arrays-pt2/array.js
+++ PB-arrays-pt2/array.js
@@ -20,16 +20,16 @@ const longestWord = (string) => {
     console.log(myArrayLength);
     let longestWord = myArray[0];
     console.log(myArray);
-    for (let i = 0; i < myArrayLength;i++) {
+    for (let i = 0; i < myArrayLength; i++) {
         if (myArray[i].length <= longestWord.length) {
         } else {
-           longestWord=myArray[i];
+            longestWord = myArray[i];
         }
     }
-return longestWord;
+    return longestWord;
 };
 
-console.log(longestWord ('this is a web development course'));
+console.log(longestWord('this is a web development course'));
 
 // i.e. longestWord("this is a web development course") ➞  "development"
 
@@ -40,10 +40,10 @@ const reverseNumber = (myArray2) => {
     let myArray3 = myString.split('');
     let myReverse = myArray3.reverse();
     console.log(myReverse);
-    let myString2=myReverse.join('');
+    let myString2 = myReverse.join('');
     console.log(myString2);
-    
-    
+
+
 
 
 
@@ -56,11 +56,26 @@ reverseNumber(34532);
 
 // **4. AEIOU: Vowels.**
 // Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, "a, e, i, o, u") in the string. 
+const findVowels = (str) => {
+    let vowelsCount = 0;
+    let myArrayTostring = str.toString();
+    console.log(myArrayTostring);
+    for (let i = 0; i <= myArrayTostring.length - 1; i++) {
+        if (myArrayTostring[i] === "a" || myArrayTostring[i] === "e" || myArrayTostring[i] === "i" || myArrayTostring[i] === "o" || myArrayTostring[i] === "u") {
+            vowelsCount += 1;
+        }
+    }
+    return vowelsCount;
 
-// i.e. findVowels("this is a string") ➞ 4
+}
+
+
+
+console.log(findVowels(["this is a string"]));
 
 // **5. Missing Number.**
 // Create a function that takes an array of all integers between 1 and 10 (excluding one) and returns the missing integer.
+
 
 // Examples: 
 // * missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
@@ -69,6 +84,16 @@ reverseNumber(34532);
 
 // **6. Cubed.**
 // Create a function that takes in an array of numbers and returns the sum of its cubes. 
+const sumOfCubes = (numbers) => {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += Math.pow(numbers[i], 3);
+
+    } return sum;
+
+}
+
+console.log(sumOfCubes([1, 5, 9]));
 
 // Examples: 
 // * sumOfCubes([1, 5, 9]) ➞ 855 // Since 1^3 + 5^3 + 9^3 = 1 + 125 + 729 = 855
@@ -77,6 +102,25 @@ reverseNumber(34532);
 
 // **7. Dictionary.**
 // Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
+// const dictionary = (str, array) => {
+//     let letters = (dic) => dic === str;
+//     let contains = dictionary.filter(letters);
+//     console.log(contains);
+// }
+
+// dictionary("bu", ["button", "breakfast", "border"]);
+
+
+
+// const dictionary = (str, array) =>{
+//     if (str != array){
+//         let empty = [];
+//         return empty;
+//     } else if (){
+
+//     }
+// }
+
 
 // Notes:
 // * If none of the words match, return an empty array.
@@ -90,6 +134,18 @@ reverseNumber(34532);
 // **8. Even Number Generator.**
 // Create a function that finds all even numbers from 1 to the given number.
 
+const evenNums = (array) => {
+    let empty = [];
+    for (i = 1; i < array; i++) {
+        if ((array[i] % 2) === 0) {
+            console.log(array[i]);
+        } else if ((array[i] % 2 === 0) {
+            return empty;
+     }
+    }
+}
+
+ evenNums([8]);
 // Examples:
 // * evenNums(8) ➞ [2, 4, 6, 8]
 // * evenNums(4) ➞ [2, 4]
