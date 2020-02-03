67ebde68dd680c13a7d59917248d9175e6bb6450
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-02-03 09:59:11 +0100

    new

--- PB-datastructure-array/array.js
+++ PB-datastructure-array/array.js
@@ -2,6 +2,6 @@ let myArray=[1,2,6,4,5];
 console.log(myArray)
 let thirdElement=myArray[2];
 console.log(thirdElement);
- myArray[4]=[7];
- let fourthElement=myArray[4];
- console.log(fourthElement);
+ myArray[3]=['fourth element'];
+ 
+  console.log(myArray);
