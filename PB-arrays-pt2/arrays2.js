c4dee6c44f10a4fa0b786f136ad5bdafac326153
hbendis <bendishoxha1@gmail.com>
2020-02-04 12:54:29 +0100

    done

--- PB-arrays-pt2/arrays2.js
+++ PB-arrays-pt2/arrays2.js
@@ -44,24 +44,66 @@ const vowelCount = s => s.replace(/[^aeiou]/gi, '').length;
 console.log(vowelCount("this is a string"));
 console.log(`:::::::::::::::::::::::::::Task 5:::::::::::::::::`)
 
-function missingNums(arr) {
-    let n = arr.length + 1,
+function missingNums(x) {
+    let n = x.length + 1,
         sum = 0,
         expectedSum = n * (n + 1) / 2;
 
-    for (let i = 0, len = arr.length; i < len; i++) {
-        sum += arr[i];
+    for (let i = 0, len = x.length; i < len; i++) {
+        sum += x[i];
     }
 
     return expectedSum - sum;
 }
+
 console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]));
 console.log(missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]));
 console.log(missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]));
 console.log(`:::::::::::::::::::::::::::Task 6:::::::::::::::::`)
 
+function sumOfCubes(ofCube) {
+    let sum = 0;
+    let i = ofCube.length; {
+        while (i--)
+            sum += ofCube[i] ** 3;
+        return sum;
+    }
+}
+console.log(sumOfCubes([1, 5, 9])); //855
+console.log(sumOfCubes([2])); //8
+console.log(sumOfCubes([])); //0
+
 console.log(`:::::::::::::::::::::::::::Task 7:::::::::::::::::`)
-
+const dictionary = (a, b) => {
+    let c = [];
+    for (let i = 0; i < b.length; i++) {
+        if (b[i].startsWith(a)) {
+            c.push(b[i]);
+        }
+    }
+    return c;
+};
+console.log(dictionary("bu", ["button", "breakfast", "border"]));
+console.log(dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]));
+console.log(dictionary("beau", ["pastry", "delicious", "name", "boring"]));
 console.log(`:::::::::::::::::::::::::::Task 8:::::::::::::::::`)
+const evenNums = (m) => {
+    let q = [];
+    for (let i = 1; i <= m; i++) {
+        if (i % 2 === 0) {
+            q.push(i);
+        }
+    }
+    return q;
+};
+console.log(evenNums(8));
+console.log(evenNums(4));
+console.log(evenNums(2));
 
-console.log(`:::::::::::::::::::::::::::Task 9:::::::::::::::::`)
+
+console.log(`:::::::::::::::::::::::::::Bonus:::::::::::::::::`)
+const alphaOrder = function (text) {
+    return text.split('').sort().join('');
+};
+console.log(alphaOrder("webdev"));
+console.log(alphaOrder("WebdeV"));
