6d00a3dd6723cc733f961083aa0ce7f14ef371b3
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-02-04 10:00:34 +0100

    cmmtt

--- PB-arrays-pt2/arr2.js
+++ PB-arrays-pt2/arr2.js
@@ -1,3 +1,6 @@
+console.log('**1. The Greater Numbers.**');
+// Create a function which accepts two arguments: the first argument being an array of numbers, and the second argument being a number. The function should return the elements of the array which are greater than the second argument. 
+
 const findGreatest = (array, number) => {
     if (array[0] > number) {
         console.log(array[0]);
@@ -16,7 +19,7 @@ findGreatest([10, 20, 30], 12);
 
 findGreatest([0, 10, 3], 4);
 
-// **2. For the longest word.**
+console.log('**2. For the longest word.**');
 // Create a function to find the longest word in a given string. 
 
 // i.e. longestWord("this is a web development course") ➞  "development"
@@ -48,7 +51,7 @@ const longestWord = (str) => {
 
 longestWord("this is a web development course");
 
-// **3. Reverse.**
+console.log('**3. Reverse.**');
 // Create a function to reverse a number. 
 
 const reverse = (num) => {
@@ -59,7 +62,7 @@ const reverse = (num) => {
 reverse(34532);
 // ➞ 23543
 
-// **4. AEIOU: Vowels.**
+console.log('**4. AEIOU: Vowels.**');
 // Create a function that takes a string in its parameters and counts the number of vowels (i.e. in English, "a, e, i, o, u") in the string. 
 
 // const findVowels = str => {
@@ -89,34 +92,30 @@ const findVowels = str => {
 console.log(findVowels("this is a string"));
 // ➞ 4
 
-// **5. Missing Number.**
+console.log('**5. Missing Number.**');
 // Create a function that takes an array of all integers between 1 and 10 (excluding one) and returns the missing integer.
 
-// const missingNums = arr => {
-//     // console.log(beasts.indexOf('giraffe'));
-//     // expected output: -1
-//     let completeSequence = [1,2,3,4,5,6,7,8,9,10];
-//     let missingObject = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] !== completeSequence[i]) {
-//             missingObject = completeSequence[i];
-//             console.log('missing', missingObject);
-//             return
-//         }
-//     }
-// };
+const missingNums = arr => {
+    let missingObject = [];
+    for (let i = 1; i <= 10; i++) {
+        if (arr.indexOf(i) === -1) {
+            missingObject = i;
+            return missingObject;
+        }
+    }
+};
 
 // KEINE AHNUNG!!!
 
 // // Examples: 
-// console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]));
+console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]));
 // // ➞ 5
-// missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]);
+console.log(missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]));
 // // ➞ 10
-// missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]);
+console.log(missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]));
 // // ➞ 7 
 
-// **6. Cubed.**
+console.log('**6. Cubed.**');
 // Create a function that takes in an array of numbers and returns the sum of its cubes. 
 
 const sumOfCubes = (arr) => {
@@ -136,7 +135,7 @@ console.log(sumOfCubes([2]));
 console.log(sumOfCubes([]));
 // ➞ 0
 
-// **7. Dictionary.**
+console.log('**7. Dictionary.**');
 // Create a function that takes an initial string and an array of words, and returns a filtered array of the words that start with the same letters as the initial string.
 
 // Notes:
@@ -147,16 +146,47 @@ const dictionary = (item, arr) => {
     let matchingWord = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].startsWith(item)) {
-            console.log(arr[i]);
+            matchingWord.push(arr[i]); 
         } 
     }
     return matchingWord
 };
 
 // Examples:
-dictionary("bu", ["button", "breakfast", "border"]);
+console.log(dictionary("bu", ["button", "breakfast", "border"]));
 // ➞ ["button"]
-dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]);
+console.log(dictionary("tri", ["triplet", "tries", "trip", "piano", "tree"]));
 // ➞ ["triplet", "tries", trip"]
-dictionary("beau", ["pastry", "delicious", "name", "boring"]);
+console.log(dictionary("beau", ["pastry", "delicious", "name", "boring"]));
 // ➞ []
+
+console.log('**8. Even Number Generator.**');
+// Create a function that finds all even numbers from 1 to the given number.
+
+const evenNums = item => {
+    allEvenNums = [];
+    for (let i = 2; i <= item; i++) {
+        if (i % 2 === 0) {
+            // console.log('item ->', i);
+            allEvenNums.push(i);
+        }
+    }
+    return allEvenNums;
+};
+
+// Examples:
+console.log(evenNums(8));
+// ➞ [2, 4, 6, 8]
+console.log(evenNums(4));
+// ➞ [2, 4]
+console.log(evenNums(2));
+// ➞ [2]
+// **Notes:** 
+// * If there are no even numbers, return an empty array. 
+// * Do not include 0. 
+
+// **Bonus: Alphabetical Order.**
+// Create a function to sort a string into alphabetical order. **NB:** assume numbers, symbols and punctuation are not included in the string.
+
+// alphaOrder("webdev");
+// ➞ "bdeevw" 
