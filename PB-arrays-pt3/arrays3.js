3b291757c1a53cca070371a265bdeb1ab0b2303e
hbendis <bendishoxha1@gmail.com>
2020-02-06 10:06:39 +0100

    done

--- PB-arrays-pt3/arrays3.js
+++ PB-arrays-pt3/arrays3.js
@@ -34,12 +34,32 @@ function unique(myArray) {
     }
   }
 }
-
 console.log(unique([3, 3, 3, 7, 3, 3])); //➞ 7
 console.log(unique([0, 0, 0.77, 0, 0])); //➞ 0.77
 console.log(unique([0, 1, 1, 1, 1, 1, 1, 1])); //➞ 0
 console.log(`:::::::::::::::::::::Task 5:::::::::::::::::::::`)
+
+function wordRank(s) {
+  let as = s.split(' ').map(s => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
+  return s.split(' ')[as.indexOf(Math.max(...as))];
+}
+console.log(wordRank("The quick brown fox.")); // ➞ "brown"
+console.log(wordRank("Nancy is very pretty.")); // ➞ "pretty"
+console.log(wordRank("Check back tomorrow, man!")); //➞ "tomorrow"
+console.log(wordRank("Today is Wednesday.")); // ➞ "Wednesday
 console.log(`:::::::::::::::::::::Task 6:::::::::::::::::::::`)
+
+const hackerSpeak = str => {
+  str = str.replace(/[aA]/g, "4");
+  str = str.replace(/[eE]/g, "3");
+  str = str.replace(/[iI]/g, "1");
+  str = str.replace(/[oO]/g, "0");
+  str = str.replace(/[sS]/g, "5");
+  return str;
+};
+console.log(hackerSpeak("javascript is cool")); // ➞ "j4v45cr1pt 15 c00l"
+console.log(hackerSpeak("programming is fun")); //➞ "pr0gr4mm1ng 15 fun"
+console.log(hackerSpeak("become a coder")); //➞ "b3c0m3 4 c0d3r"
 console.log(`:::::::::::::::::::::Task Bonus 1:::::::::::::::::::::`)
 console.log(`:::::::::::::::::::::Task Bonus 2:::::::::::::::::::::`)
 console.log(`:::::::::::::::::::::Task Bonus 3:::::::::::::::::::::`)
