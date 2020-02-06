497f107b326f1184c8e07242efbf4312ce28b573
Bianca Richa <biancaricha@gmail.com>
2020-02-06 13:23:17 +0100

    object

--- PB-datastructure-objectscope/object.js
+++ PB-datastructure-objectscope/object.js
@@ -27,13 +27,14 @@ const person = {
     getCurrentStatus: function () {
         console.log(`${this.firstName} ${this.lastName}, ${this.age}, is a ${this.job} from ${this.city} and she is ${ this.status}.`);
     },
-    getLength: function (){
-       return Object.keys(this).length;
+    getLength: function () {
+        return Object.keys(this).length;
     }
 };
 person.getCurrentStatus();
 console.log(person.getLength());
 
+console.log(Object.keys(person).length);
 
 
 // 2. Create a person object. Include the person's first and last name, age, job, city etc. 
@@ -43,3 +44,37 @@ console.log(person.getLength());
 
 // Bonus
 // Write a method to get the length of the person object.
+
+//
+
+const object = {
+    a: 1,
+    b: 2,
+    c: 3
+};
+ for(const property in object) {
+     console.log(`${property}: ${object[property]}`)
+ }
+
+ let sum = 0;
+ //for...in loop
+ for(const key in object) {
+     sum += object[key]; //sum up the values
+ }
+ console.log(sum);
+
+ //how many keys?
+ let count = 0;
+ for(const something in object) {
+     count++;
+ }
+ console.log(count);
+
+ const array = Object.keys(object);
+ console.log(array);
+
+ let string = "";
+ for(const index in array) { //always go over the full object /array from beginning to the end
+string += array[index];
+ }
+ console.log(string); //expected output
