14420e4cdc0494a40c8c9bac808201f8e591f368
grizman040 <grizman040@gmail.com>
2020-02-06 15:28:44 +0100

    da

---
+++ PB-objects-pt1/obj.js
@@ -0,0 +1,37 @@
+const person = {
+    hobby: 'fishing',
+    bornIn: 'kilimandjaro',
+    listenMusik: 'trance'
+}
+for (const property in person) {
+    console.log(`${property}- ${person[property]}`);
+}
+console.log(`----------------ex2---------------`);
+
+const a = (object) => {
+    let sum = [];
+    for (const key in object) {
+        sum.push(object[key]);
+    }
+    return sum;
+};
+console.log(a({
+    choice1: "tea",
+    choice2: "coffee",
+    choice3: "milk"
+}));
+
+console.log(`------------------------------ex3----------------`);
+
+let personalitet = {
+    firstName: "Michael",
+    lastName: "Smith",
+    job: "driver",
+    age: 20,
+    city: "Paris",
+
+    biography() {
+        return `${this.firstName} ${this.lastName} is a ${this.age} year old ${this.job} in ${this.city}`
+    }
+};
+console.log(personalitet.biography());
