94efc9db88fd5383b9afef7ea3a528e7708416e7
DCI-PC20 <juliarpim@gmail.com>
2020-02-11 15:42:10 +0100

    oi

--- PB-datastructure-destructuring/destructuring.js
+++ PB-datastructure-destructuring/destructuring.js
@@ -4,7 +4,9 @@
 // #### 1. Array Destructuring
 // * Declare the variables `fruit`, `vegetable`, `food`. Assign the following values to the variables respectively: "banana", "cucumber", "bread", "cakes", "pizza" (assign the "bread", "cakes" and "pizza" to `food`). Use array destructuring assignment to complete this task.
 
-let fruit, vegetable, food;
+let fruit; 
+let vegetable; 
+let food;
 [fruit, vegetable, ...food] = ['banana', 'cucumber', 'bread', 'breads', 'cakes', 'pizza'];
 console.log(fruit);
 console.log(vegetable);
@@ -12,12 +14,21 @@ console.log(food);
 
 // #### 2.Object Destructuring
 // * Create an object which contains key pair values of names and the respective halloween costume e.g. `fran: "witch"`.   Assign each key pair value to corresponding variable. Print out the object values. 
-let bianca, joao, balaz;
-({ bianca, joao, balaz } = { bianca: 'Bee', joao: 'Freddy Mercury', balaz: 'Elvis' });
+let bianca, joao, balaz, nicolo, julien, bendis, nagster, constantine, martina, plamen;
+({ bianca, joao, balaz, nicolo, julien, bendis, nagster, constantine, martina, plamen } = { bianca: 'Bee', joao: 'Freddy Mercury', balaz: 'Elvis', nicolo: 'Popcorn', julien: 'Itamar', bendis: 'Pikachu', nagster: 'Gangster', constantine: 'Zombie', martina: 'Cat Woman', plamen: 'Plum'});
 
 console.log(bianca);
 console.log(joao);
 console.log(balaz);
+console.log(nicolo);
+console.log(julien);
+console.log(bendis);
+console.log(nagster);
+console.log(constantine);
+console.log(martina);
+console.log(plamen);
+
+
 
 // #### 3. Parameters: Object Destructuring 
 // * Create an object with band/musician details e.g. name of the band/musician, greatest hit, nationality, genre etc. 
