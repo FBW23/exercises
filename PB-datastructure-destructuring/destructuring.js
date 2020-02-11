5d91b18c585ba6ceb0b729906c22e5e29b4b24c5
hbendis <bendishoxha1@gmail.com>
2020-02-11 15:37:02 +0100

    done 3

--- PB-datastructure-destructuring/destructuring.js
+++ PB-datastructure-destructuring/destructuring.js
@@ -6,7 +6,34 @@ console.log(vegetable);
 console.log(...food);
 
 console.log(`:::::::::::::::::::::::::::::::Task 2:::::::::::::::::::::::::::::::;`)
-let []
+let adam, frau;
+({
+    adam,
+    frau,
+
+} = {
+    adam: 'wolf',
+    frau: 'witch',
+
+});
+console.log(adam);
+console.log(frau);
 
 
 console.log(`:::::::::::::::::::::::::::::::Task 3:::::::::::::::::::::::::::::::;`)
+
+const object = {
+    name: "The Beatles",
+    nationality: "UK",
+    genre: 'Rock, Pop',
+    greatesthit: 'Hey Jude'
+};
+const musician = ({
+    name,
+    nationality,
+    genre,
+    greatesthit
+}) => {
+    return name + ' from ' + nationality + ' were famous in ' + genre + ' and greatest album ' + greatesthit
+};
+console.log(musician(object));
