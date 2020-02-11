26c482143d147983b9153a78f07503a5eedde162
o0pmitev <pmitev89@gmail.com>
2020-02-11 10:36:50 +0100

    Finished PB-datastructure-spread BONUS

--- PB-datastructure-spread/index.js
+++ PB-datastructure-spread/index.js
@@ -39,32 +39,41 @@ const findLargest = arr => {
   return Math.max(...arr);
 };
 
-console.log(findLargest([1,2,3,4,5]));
+console.log(findLargest([1, 2, 3, 4, 5]));
 
 // 4. Find the Smallest
 // Create a function to find the smallest number in an array.
 
 const findSmallest = arr => {
- return Math.min(...arr);
+  return Math.min(...arr);
 };
 
-console.log(findSmallest([1,2,3,4,5]));
+console.log(findSmallest([1, 2, 3, 4, 5]));
 
 
 // 5. Clone and Merge
 ///Given two objects:
-const person = {name: "John"};
-const job = {role: "Teacher"};
+const person = {
+  name: "John"
+};
+const job = {
+  role: "Teacher"
+};
 
 //5.1 Clone the person object.
 
-const clonePerson = {...person};
+const clonePerson = {
+  ...person
+};
 console.log(clonePerson);
 
 //5.2 Merge these two objects into one object: 
 //"employee". Use the spread operator to do so.
 
-const employee = {...person, ...job};
+const employee = {
+  ...person,
+  ...job
+};
 console.log(employee);
 
 //  Then change the values of the properties in the employee object.
@@ -72,3 +81,23 @@ employee.name = "Plamen";
 employee.role = "Web Developer";
 
 console.log(employee);
+
+// Bonus: 6. Is the average a whole number?
+// Create a function that takes 4 integers as parameters and 
+//returns true or false depending on whether 
+//the average of all the arguments is a whole number or not.
+
+const isWhole = (...arr) => {
+  function sum(a, x, y, z, ) {
+    return (a + x + y + z) / 4;
+  };
+  if (sum(...arr) % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(isWhole(1, 2, 3, 4));
+console.log(isWhole(9, 2, 2, 5));
+console.log(isWhole(2, 2, 2, 2));
