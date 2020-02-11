3ac97b281e0bf7adccd3247fccc82b4c6e5063a0
o0pmitev <pmitev89@gmail.com>
2020-02-11 10:28:11 +0100

    Finished PB-datastructure-spread

---
+++ PB-datastructure-spread/index.js
@@ -0,0 +1,74 @@
+// 1. Combining Arrays
+//Create two arrays. e.g. euroCountries and 
+//asianCountries. Add asianCountries items to the 
+//end of euroCountries array.
+//Once again create two arrays. Save all elements 
+//of both arrays to another variable.
+
+console.log("~~~~~~~~~~~~~~~1. Combining Arrays~~~~~~~~~~~~~~~~");
+
+const euroCountries = ["Bulgaria", "Spain", "Greece", "Germany"];
+const asianCountries = ["Japan", "China"];
+
+euroCountries.push(...asianCountries);
+console.log(euroCountries);
+
+const food = ["Paella", "Rise with Chicken", "Bob"];
+const drinks = ["Wine", "Beer", "Water"];
+
+// const newVariable = [...food, ...drinks];
+
+const newVariable = ([...food, ...drinks]).toString();
+
+console.log(newVariable);
+
+// 2. Copying Arrays
+// Copy an array using the spread operator. 
+// Store the copied array in another variable.
+
+console.log("~~~~~~~~~~~~~~~2. Copying Arrays~~~~~~~~~~~~~~~~");
+
+const myArray = [1, 2, 3, 4];
+const cloneMyArray = [...myArray];
+console.log(cloneMyArray);
+
+//3. Find the Largest...
+//Create a function to find the largest number in an array.
+
+const findLargest = arr => {
+  return Math.max(...arr);
+};
+
+console.log(findLargest([1,2,3,4,5]));
+
+// 4. Find the Smallest
+// Create a function to find the smallest number in an array.
+
+const findSmallest = arr => {
+ return Math.min(...arr);
+};
+
+console.log(findSmallest([1,2,3,4,5]));
+
+
+// 5. Clone and Merge
+///Given two objects:
+const person = {name: "John"};
+const job = {role: "Teacher"};
+
+//5.1 Clone the person object.
+
+const clonePerson = {...person};
+console.log(clonePerson);
+
+//5.2 Merge these two objects into one object: 
+//"employee". Use the spread operator to do so.
+
+const employee = {...person, ...job};
+console.log(employee);
+
+//  Then change the values of the properties in the employee object.
+employee.name = "Plamen";
+employee.role = "Web Developer";
+
+console.log(employee);
