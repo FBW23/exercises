908d2bff418aa1b4f23748ed4dc55c91fb83d41d
DCI-PC20 <juliarpim@gmail.com>
2020-02-11 14:38:35 +0100

    oi

---
+++ PB-datastructure-destructuring/destructuring.js
@@ -0,0 +1,36 @@
+// # Destructuring
+
+// **Instructions**
+// #### 1. Array Destructuring
+// * Declare the variables `fruit`, `vegetable`, `food`. Assign the following values to the variables respectively: "banana", "cucumber", "bread", "cakes", "pizza" (assign the "bread", "cakes" and "pizza" to `food`). Use array destructuring assignment to complete this task.
+
+let fruit, vegetable, food;
+[fruit, vegetable, ...food] = ['banana', 'cucumber', 'bread', 'breads', 'cakes', 'pizza'];
+console.log(fruit);
+console.log(vegetable);
+console.log(food);
+
+// #### 2.Object Destructuring
+// * Create an object which contains key pair values of names and the respective halloween costume e.g. `fran: "witch"`.   Assign each key pair value to corresponding variable. Print out the object values. 
+let bianca, joao, balaz;
+({bianca, joao, balaz} = {bianca: 'Bee', joao: 'Freddy Mercury', balaz: 'Elvis'});
+
+console.log(bianca);
+console.log(joao);
+console.log(balaz);
+
+// #### 3. Parameters: Object Destructuring 
+// * Create an object with band/musician details e.g. name of the band/musician, greatest hit, nationality, genre etc. 
+// * Then, create a function to display all of the band/musician's data. 
+const myMusic ={
+    name: 'Elis Regina',
+    greatestHit: 'Águas de Março',
+    nationality: 'Brazilian',
+    genre: 'MPB, Samba, Rock, Bossa nova',
+    
+
+}
+// * Example
+// ```javascript
+// `Regina Spektor is a American-Russian singer/songwriter and pianist. The musician sings indie-pop and their greatest hit is "Us"`.
+// ```
