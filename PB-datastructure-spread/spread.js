0761d4ea0d1c4388429605cdb67f8ef652b22f86
hbendis <bendishoxha1@gmail.com>
2020-02-11 13:02:05 +0100

    done

--- PB-datastructure-spread/spread.js
+++ PB-datastructure-spread/spread.js
@@ -1,31 +1,46 @@
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 1::::::::::::::::::::::::::::::::::::::::`)
 
-const euroCountries = ['Germany', 'Italy', 'Spain', 'Montenegro', 'Greece'];
+const euroCountries = ['Germany', 'Italy', 'Spain'];
 //console.log(euroCountries);
 
-const asianCountries = ['Malaysia', 'China', 'Japan', 'Singapore', 'Indonesia'];
+const asianCountries = ['Malaysia', 'China', 'Japan'];
 //console.log(asianCountries);
 
 
-euroCountries.push(asianCountries.join(', '));
+euroCountries.push(asianCountries.join);
 console.log(euroCountries);
 
-const newArray = euroCountries.concat(asianCountries);
+const middleEastCountries = ['Syria', 'Israel', 'Jordan'];
+const balkanCountries = ['Albania', 'Serbia', 'Greece'];
+const newArray = middleEastCountries.concat(balkanCountries);
 console.log(newArray);
 
 
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 2::::::::::::::::::::::::::::::::::::::::`)
-
+const copyBalkanCountries = [...balkanCountries];
+console.log(copyBalkanCountries);
 
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 3::::::::::::::::::::::::::::::::::::::::`)
 
-
-
+const largestNumber = number => Math.max(...number)
+console.log(largestNumber([2, 57, 89, 323]));
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 4::::::::::::::::::::::::::::::::::::::::`)
 
-
+const smallestNumber = number => Math.min(...number)
+console.log(smallestNumber([2, 57, 89, 323]));
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 5::::::::::::::::::::::::::::::::::::::::`)
 
+const person = {name: "John"}
+const job = {role: "Teacher"}
 
+const clonePerson ={...person};
+console.log(clonePerson);
+
+const employee = {...person, ...job}
+console.log(employee);
+
+employee.name = 'Carl';
+employee.role = 'Student';
+console.log(employee);
 
 console.log(`::::::::::::::::::::::::::::::::::::::::::::Task 6::::::::::::::::::::::::::::::::::::::::`)
