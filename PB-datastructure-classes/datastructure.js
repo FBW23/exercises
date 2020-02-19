a819bd86ef6e0be887623800a2e73c87580ae766
hbendis <bendishoxha1@gmail.com>
2020-02-19 10:31:26 +0100

    done3

---
+++ PB-datastructure-classes/datastructure.js
@@ -0,0 +1,90 @@
+console.log(`:::::::::::::::::::::::: Task:1 :::::::::::::::::::::::::::::::`)
+//Create a class called `Person` which accepts the name of a person as a string, and his/her age as a number. The `Person` class should have a method called describe which returns a string with the following syntax: "name, age years old". So, for example, if John is 19 years old, then the function describe of his object will return "John, 19 years old".
+
+
+//Rewrite the following code to use the "class" syntax. 
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    describe() {
+        return this.getInfo();
+    }
+    // Method
+    getInfo() {
+        return `${this.name}, ${this.age} years old`;
+
+    }
+}
+
+const amar = new Person("Amar", 5);
+console.log(amar.describe());
+console.log(`:::::::::::::::::::::::: Task:2 :::::::::::::::::::::::::::::::`)
+//Write a JavaScript program to get the volume of a cylinder with four decimal places using a class. 
+class Cylinder {
+    constructor(height, radius) {
+        this.height = height;
+        this.radius = radius;
+    }
+    getVolume() {
+        return (Math.PI * this.radius * 2 * this.height).toFixed(4);
+    }
+}
+
+const cylinder = new Cylinder(2, 8);
+console.log(cylinder.getVolume());
+console.log(`:::::::::::::::::::::::: Task:3 :::::::::::::::::::::::::::::::`)
+
+class Clock {
+    constructor({
+        template
+    }) {
+        this.template = template;
+    }
+
+    render() {
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
+    }
+
+    stop() {
+        clearInterval(this.timer);
+    };
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 10);
+    }
+}
+
+let clock = new Clock({
+    template: 'h:m:s'
+});
+clock.start();
+setTimeout(() => clock.stop(), 10);
+
+console.log(`:::::::::::::::::::::::: Task:4 :::::::::::::::::::::::::::::::`)
+
+class Properties {
+constructor(brand, channel, volume) {
+ this.brand = brand;
+ this.channel = channel;
+this.volume = volume;
+ }
---
+++ PB-datastructure-logicalthinking/logical.js
@@ -0,0 +1,9 @@
+console.log(`:::::::::::::::::::::::::::::Task 1::::::::::::::::::::::::::`)
+const numbers = [4, 2, 5, 3, 9, 7, 1, 8, 2, 6];
+numbers.sort();
+console.log(numbers);
+
+console.log(`:::::::::::::::::::::::::::::Task 2::::::::::::::::::::::::::`)
+const names = ["Fran", "Leon", "Ale", "Jenna", "Bleda", "Hadi"];
+names.sort();
+console.log(names);
