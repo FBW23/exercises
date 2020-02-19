3c31ac341bbf857558dd57554a8588cba4c6a1c6
nagster23 <54030149+nagster23@users.noreply.github.com>
2020-02-19 20:55:47 +0800

    gtn thr

--- PB-datastructure-classes/classy.js
+++ PB-datastructure-classes/classy.js
@@ -6,20 +6,20 @@
 // then the function describe of his object will return "John, 19 years old".
 
 class Person {
-    constructor(name,age) {
-        this.name=name;
-        this.age=age;
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
     //getter
-    get describe (){
+    get describe() {
         return this.personInfo();
     }
     //Method
-    personInfo (){
+    personInfo() {
         return `${this.name}, ${this.age} years old`
     }
 };
-const superHero = new Person ('John', 19);
+const superHero = new Person('John', 19);
 console.log(superHero.describe);
 
 console.log("...........................................");
@@ -31,15 +31,15 @@ console.log("...........................................");
 
 
 class Cylinder {
-    constructor(diameter,height) {
-        this.diameter=diameter;
-        this.height=height;
+    constructor(diameter, height) {
+        this.diameter = diameter;
+        this.height = height;
     }
-   volume (){
-      return (Math.PI**this.diameter*this.height).toFixed(4);
-  }
+    volume() {
+        return (Math.PI ** this.diameter * this.height).toFixed(4);
+    }
 }
-const cylinderVolume= new Cylinder (2,4);
+const cylinderVolume = new Cylinder(2, 4);
 console.log(cylinderVolume.volume());
 
 console.log(".................................................");
@@ -47,41 +47,68 @@ console.log(".................................................");
 // 3. Tick Tock
 // Rewrite the following code to use the "class" syntax.
 
-  class Clock {
-       template }  {
-  
-    let timer;
-  
-     render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
+class Clock {
+    constructor({
+        template
+    }) {
+        this.template = template;
     }
-  
-    this.stop = function() {
-      clearInterval(timer);
+    render() {
+        let date = new Date();
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+        console.log(output);
+    }
+    stop() {
+        clearInterval(this.timer);
     };
-  
-    this.start = function() {
-      render();
-      timer = setInterval(render, 1000);
-    };
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+let clock = new Clock({
+    template: 'h:m:s'
+});
+clock.start();
+setTimeout(() => clock.stop(), 1000)
 
-  }
-  
-  let clock = new Clock({template: 'h:m:s'});
-  clock.start();
+console.log("................................");
+
+class Tv {
+    constructor(brand) {
+        this.brand = brand;
+        this.channel = 1;
+        this.volume = 50;
+    }
+    volumeUp() {
+        if (this.volume < 100) {
+            return this.volume++;
+        }
+    }
+    volumeDown() {
+        if (this.volume > 0) {
+            return this.volume--;
+        }
+    }
+    randomChannel() {
+        // return (Math.random() * 50).toFixed(0);
+        this.channel = Math.floor(Math.random() * 49) + 1;
+    }
+    status() {
+        return `${this.brand} at channel ${this.channel}, volume ${this.volume}`
+    }
+}
+const myTv = new Tv("Apple");
+// console.log(myTv);
+console.log(myTv.randomChannel());
+console.log(myTv);
+console.log(myTv.status());
