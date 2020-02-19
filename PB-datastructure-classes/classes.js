7303364926501c2355202848e6cbb3def01585b2
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-02-19 13:36:11 +0100

    cmmt final

--- PB-datastructure-classes/classes.js
+++ PB-datastructure-classes/classes.js
@@ -68,45 +68,6 @@ let myC = new Clock({template: 'h:m:s'});
 myC.start();
 setTimeout(() => myC.stop(), 10000);
 
-
-// class Clock {
-//     constructor(template) {
-//         // let timer;
-//         this.template = template;
-//     }
-
-//     render = function (date, hours, secs) {
-//         let date = new Date();
-
-//         let hours = date.getHours();
-//         if (hours < 10) hours = '0' + hours;
-
-//         let mins = date.getMinutes();
-//         if (mins < 10) mins = '0' + mins;
-
-//         let secs = date.getSeconds();
-//         if (secs < 10) secs = '0' + secs;
-
-//         let output = template
-//             .replace('h', hours)
-//             .replace('m', mins)
-//             .replace('s', secs);
-
-//         console.log(output);
-//     }
-
-//     stop = function () {
-//         clearInterval(timer);
-//     };
-
-//     start = function () {
-//         render();
-//         timer = setInterval(render, 1000);
-//     };
-// }
-
-
-
 let clock = new Clock({
     template: 'h:m:s'
 });
