ca68a1cba3619a7ef76b247dda0822d3e661deb7
grizman040 <grizman040@gmail.com>
2020-02-19 13:58:58 +0100

    TV Brand

--- PB-datastructure-classes/classes.js
+++ PB-datastructure-classes/classes.js
@@ -33,40 +33,90 @@ console.log(`-----------------------ex3---------------------------------`);
 
 
 class Clock {
-    constructor({ rolex }) {
-      this.rolex = rolex;
+    constructor({
+        rolex
+    }) {
+        this.rolex = rolex;
     }
-  
+
     render() {
-      let date = new Date();
-  
-      let hours = date.getHours();
-      if (hours < 10) hours = '0' + hours;
-  
-      let mins = date.getMinutes();
-      if (mins < 10) mins = '0' + mins;
-  
-      let secs = date.getSeconds();
-      if (secs < 10) secs = '0' + secs;
-  
-      let output = this.rolex
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-  
-      console.log(output);
+        let date = new Date();
+
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+
+        let output = this.rolex
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+
+        console.log(output);
     }
-  
+
     stop() {
-      clearInterval(this.timer);
+        clearInterval(this.timer);
     }
-  
+
     start() {
-      this.render();
-      this.timer = setInterval(() => this.render(),1000);
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
     }
-  }
-  
-  
-  let clock = new Clock({rolex: 'h:m:s'});
-  clock.start();
+}
+
+
+let clock = new Clock({
+    rolex: 'h:m:s'
+});
+clock.start();
+setTimeout(() => clock.stop(), 1000);
+
+console.log(`-----------------------------ex4------------`);
+class Tv {
+    constructor(brand) {
+        this.brand = brand;
+        this.reset();
+
+
+    }
+    reset() {
+        this.volume = 50;
+        this.channel = 1;
+        return `Factory settings is: ` + this.status();
+
+    }
+    increaseVolume() {
+        if (this.volume < 100) {
+            ++this.volume;
+            return this.status();
+
+        }
+    }
+    decreaseVolume() {
+        if (this.volume >= 0) {
+            --this.volume;
+            return this.status();
+        }
+    }
+    setChannel() {
+        this.channel = Math.round(Math.random() * 49) + 1;
+        return this.status();
+
+    };
+    status() {
+        return `${this.brand} at channel ${this.channel}, volume ${this.volume}`
+    }
+
+};
+let firstTV = new Tv("Panasonic");
+
+console.log(firstTV.increaseVolume());
+console.log(firstTV.decreaseVolume());;
+console.log(`This TV is: ${firstTV.setChannel()}`);
+console.log(firstTV.reset());
+console.log(firstTV);
