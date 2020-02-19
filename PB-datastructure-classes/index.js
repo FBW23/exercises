f2e738c8f48c5a4c11760221ea32bd8051a444b9
juliapim <54452216+juliapim@users.noreply.github.com>
2020-02-19 10:31:28 +0100

    not finished

--- PB-datastructure-classes/index.js
+++ PB-datastructure-classes/index.js
@@ -1,108 +1,48 @@
-//  1
 class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  describe() {
-    return `${this.name}, ${this.age} years old.`;
-  }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age; // standard age predefined
+        
+    }
+
+    describe () {
+        console.log(`${this.name}, ${this.age} years old`);
+    }
 }
 
-const fran = new Person("Fran", 25);
-console.log(fran.describe());
+const john = new Person ("John", 19);
+john.describe();
 
-// 2
 class Cylinder {
-  constructor(height, radius) {
-    this.height = height;
-    this.radius = radius;
-  }
-  getVolume() {
-    return (Math.PI * this.radius * 2 * this.height).toFixed(4);
-  }
-}
-
-const cylinder = new Cylinder(4, 6);
-console.log(cylinder.getVolume());
-
-// 3
-class Clock {
-  constructor({ template }) {
-    this.template = template;
-    this.timer;
-  }
-
-  render() {
-    let date = new Date();
-
-    let hours = date.getHours();
-    if (hours < 10) hours = "0" + hours;
-
-    let mins = date.getMinutes();
-    if (mins < 10) mins = "0" + mins;
-
-    let secs = date.getSeconds();
-    if (secs < 10) secs = "0" + secs;
-
-    let output = this.template
-      .replace("h", hours)
-      .replace("m", mins)
-      .replace("s", secs);
-
-    console.log(output);
-  }
-
-  stop() {
-    clearInterval(this.timer);
-  }
-
-  start() {
-    this.render();
-    this.timer = setInterval(() => {
-      this.render();
-    }, 1000);
-  }
-}
-
-let clock = new Clock({template: "h:m:s"});
-clock.start();
-
-setTimeout(() => clock.stop(), 4000);
-
-// 4
-
-class TV {
-  constructor(brand) {
-    this.brand = brand;
-    this.channel = 1;
-    this.volume = 50;
-  }
-
-  increaseVolume() {
-    if (this.volume < 100) {
-      this.volume++;
-      console.log("Volume", this.volume);
+    constructor(radio, heigth) {
+        this.radio = radio;
+        this.heigth = heigth;
     }
-  }
-  decreaseVolume() {
-    if (this.volume >= 0) {
-      this.volume--;
-      console.log("Volume", this.volume);
-    }
-  }
-  getNewChannel() {
-    this.channel = Math.floor(Math.random() * 51);
-    console.log(`Channel: ${this.channel}`);
-  }
 
-  getTVstatus() {
-    return `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
-  }
+    volume () {
+        let calc = (Math.PI * Math.pow(this.radio, 2))*this.heigth;
+        console.log(calc.toFixed(4));
+    }
 }
 
-const tv = new TV("Panasonic");
-tv.increaseVolume();
-tv.decreaseVolume();
-tv.getNewChannel();
-console.log(tv.getTVstatus());
+const volumeOfaCylinder = new Cylinder (2, 2);
+volumeOfaCylinder.volume();
+
+/*#### 4. TV Class
+* Create a TV class with properties like brand, channel and volume.
+* Specify brand in a constructor parameter. Channel should be 1 by default. Volume should be 50 by default.
+* Add methods to increase and decrease volume. Volume can't never be below 0 or above 100.
+* Add a method to set the channel randomly. Let's say the TV has only 50 channels.
+* Add a method to reset TV so it goes back to channel 1 and volume 50. 
+* It's useful to write a status, that returns info about the TV status like: "Panasonic at channel 8, volume 75".*/
+
+class Tv {
+    constructor (brand, channel, volume){
+        this.brand = brand;
+        this.channel = 1;
+        this.volume = 50;
+    }
+    volMesure (){
+        
+    }
+}
