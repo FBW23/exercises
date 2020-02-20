e6fc246d491da35d7cc3a6b25d2689651b5af9e6
wagowsky <vagobala@gmail.com>
2020-02-20 15:55:54 +0100

    dsfdsFD

--- PB-error-runtimeerrors/vb.js
+++ PB-error-runtimeerrors/vb.js
@@ -63,6 +63,7 @@ const reverseString = string => {
 
 reverseString("larifari");
 reverseString(25);
+
 // #### 4. Comparing Arrays
 // Create a function that returns true if two arrays contain identical values, and false otherwise.
 
@@ -71,10 +72,38 @@ reverseString(25);
 function compareArrays(arr1, arr2) {
   if (arr1 === arr2) {
     return true
-   } else {
+  } else {
     return false
-   }
-}
+  }
+};
+
+function compareArrays2(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false
+      }
+
+    }
+    return true;
+  }
+
+
+};
+
+console.log(compareArrays([1, 2], [1, 2]));
+console.log(compareArrays([1, 2], [1, 2]));
+console.log(compareArrays([4, 5, 6], [4, 6, 5]));
+console.log(compareArrays([4, 7, 6], [4, 5, 6]));
+
+console.log(compareArrays2([1, 2], [1, 2]));
+console.log(compareArrays2([1, 2], [1, 2]));
+console.log(compareArrays2([4, 5, 6], [4, 6, 5]));
+console.log(compareArrays2([4, 7, 6], [4, 5, 6]));
 
 // * Why doesn't the code work? 
 // * Fix the code (or restart) to solve the problem.
@@ -88,11 +117,20 @@ function compareArrays(arr1, arr2) {
 
 // #### 5. Recursion 
 // The following code should get the sum of the array items, but it does not work. Fix it so that it will. 
-// ```javascript
-// function sum(array) {
-//     let result = array.pop() + sum(array);
-//     return result;
-// }
 
-// console.log(sum([1, 2, 3]));
-// ```
+function sum(array) {
+    let result = array.pop() + sum(array);
+    return result;
+}
+
+console.log(sum([1, 2, 3]));
+
+
+
+function sum2(array) {
+  let result = array.pop() + sum(array);
+  return result;
+}
+
+console.log(sum2([1, 2, 3]));
+
