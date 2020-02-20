02c68f5252eedc1effa3265ce016c196630f057c
hbendis <bendishoxha1@gmail.com>
2020-02-20 15:34:07 +0100

    done

--- PB-error-runtimeerrors/errors.js
+++ PB-error-runtimeerrors/errors.js
@@ -56,8 +56,8 @@ const getMonthName = (month) => {
         }
         if (month === 12) {
             return ` Dec = ${month}`
-        }else {
-            throw new Error ('Invalid Month Number!');
+        } else {
+            throw new Error('Invalid Month Number!');
         }
     } catch (e) {
         console.error(e);
@@ -66,5 +66,47 @@ const getMonthName = (month) => {
 console.log(getMonthName(11));
 console.log(`:::::::::::::::::::::::::::: Task 3 ::::::::::::::::::::::::::`)
 // Create a function that reverses a string. Throw a custom error if the user inputs another type as an argument. 
+
+function reverseString(s) {
+    try {
+        if (typeof (s) === 'string') {
+           const reverseWord = s.split("").reverse().join("");
+           return reverseWord;
+        } else {
+            throw new Error(`ERROR! This is not a string!`);
+        }
+        //  s = s.split("").reverse().join("");
+    } catch (er) {
+        console.error(er);
+    }
+}
+console.log(reverseString('string'));
 console.log(`:::::::::::::::::::::::::::: Task 4 ::::::::::::::::::::::::::`)
+function compareArrays(arr1,arr2){
+  
+    if(!arr1  || !arr2) return
+   
+     let result;
+   
+   arr1.forEach((e1,i)=>arr2.forEach(e2=>{
+     
+          if(e1.length > 1 && e2.length){
+             result = compareArrays(e1,e2);
+          }else if(e1 !== e2 ){
+             result = false
+          }else{
+             result = true
+          }
+     })
+   )
+   
+   return result
+   
+ }
+  console.log(compareArrays([1, 2], [1, 3]));
+  console.log(compareArrays([1, 2], [1, 2]));
+  console.log(compareArrays([4, 5, 6], [4, 5, 6]));
+  console.log(compareArrays([4, 7, 6], [4, 5, 6]));
+
+
 console.log(`:::::::::::::::::::::::::::: Task 5 ::::::::::::::::::::::::::`)
