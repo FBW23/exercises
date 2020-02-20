d79cd5aac18285e58de2ee25b266e42a08e9f5fa
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-02-20 15:56:46 +0100

    not done yet

--- PB-error-runtimeerrors/errors.js
+++ PB-error-runtimeerrors/errors.js
@@ -22,42 +22,92 @@ console.log('#### 2. Month Name');
 // Create a function called `getMonthName`, which takes the number of the month as an argument (Jan = 1 - Dec = 12). Throw an error if a user inputs an invalid number as an argument. 
 // * Example 
 
-let getMonthName = (monthNumber) => {
-    switch (monthNumber) {
+getMonthName = (number) => {
+    switch (number) {
         case 1:
-            return 'january';
+            console.log("January");
             break;
         case 2:
-            return 'february';
+            console.log("February");
             break;
         case 3:
-            return 'march';
+            console.log("April");
             break;
         case 4:
-            return 'april';
+            console.log("May");
+            break;
         case 5:
-            return 'may';
+            console.log("June");
+            break;
         case 6:
-            return 'june';
+            console.log("July");
+            break;
         case 7:
-            return 'july';
+            console.log("August");
+            break;
         case 8:
-            return 'august';
-        case 9:
-            return 'september';
+            console.log("September");
+            break;
         case 10:
-            return 'october';
+            console.log("October");
+            break;
         case 11:
-            return 'november';
+            console.log("November");
+            break;
         case 12:
-            return 'december';
+            console.log("December");
+            break;
         default:
-            return 'ERROR: this is no valid input.';
+            const notAvalidMonth = new Error("This month does not exist");
+            try {
+                if (number > 0 && number < 13) {
+                    console.log(getMonthName(number));
+                } else {
+                    throw `${notAvalidMonth}`;
+                }
+            } catch (e) {
+                console.log(e)
+            }
     }
-}
+};
+getMonthName(109);
 
-console.log(getMonthName(15));
-// -> `Invalid Month Number!`
+// let getMonthName = (monthNumber) => {
+//     switch (monthNumber) {
+//         case 1:
+//             return 'january';
+//             break;
+//         case 2:
+//             return 'february';
+//             break;
+//         case 3:
+//             return 'march';
+//             break;
+//         case 4:
+//             return 'april';
+//         case 5:
+//             return 'may';
+//         case 6:
+//             return 'june';
+//         case 7:
+//             return 'july';
+//         case 8:
+//             return 'august';
+//         case 9:
+//             return 'september';
+//         case 10:
+//             return 'october';
+//         case 11:
+//             return 'november';
+//         case 12:
+//             return 'december';
+//         default:
+//             return 'ERROR: this is no valid input.';
+//     }
+// }
+
+// console.log(getMonthName(15));
+// // -> `Invalid Month Number!`
 
 console.log('#### 3. Reverse');
 // Create a function that reverses a string. Throw a custom error if the user inputs another type as an argument. 
@@ -65,19 +115,22 @@ console.log('#### 3. Reverse');
 // * Example 
 
 const reverseString = string => {
-    if (typeof string !== 'string') {
-        return `ERROR: This is not a string!`;
+    const notAString = new Error("ERROR! This is not a string");
+    try {
+        if (string === 'string') {
+            throw `${string} -> it is a string`;;
+        }
+    } catch (e) {
+        console.log(e)
     }
-    return `${string} -> it is a string`;
+    return notAString;
 };
 
+console.log(reverseString(2019));
+// -> `ERROR! This is not a string!`
 // const reverseString = string => {
 //     if (typeof string !== 'string') {
 //         return `ERROR! This is not a string!`;
 //     }
 //     return `${string} -> it is a string`;
 // };
-
-
-console.log(reverseString(2019));
-// -> `ERROR! This is not a string!`
