feb6379e6879b7b84ed4747d64127c532180ef20
o0pmitev <pmitev89@gmail.com>
2020-03-02 22:03:57 +0100

    02-03-2020 final commit

--- Browser-DOM-querying/index.js
+++ Browser-DOM-querying/index.js
@@ -35,7 +35,9 @@ function randomRgba() {
 let unorderedLists = document.querySelectorAll(".food-category");
 console.log(unorderedLists)
 for(let i = 0; i < unorderedLists.length; i++) {
-  unorderedLists[i].style.color = randomRgba();
+  unorderedLists[i].style.backgroundColor = randomRgba();
+  unorderedLists[i].style.padding = "2em";
+  unorderedLists[i].style.width = "25%";
 }
 
 /*
@@ -45,14 +47,74 @@ page is responsive and also works on mobile.
 */
 
 let main = document.querySelector(".main");
-
+let screenWidth = window.matchMedia("(max-width: 615px)")
 main.style.display = "flex";
-main.style.justifyContent = "space-between";
+main.style.width = "100%";
 
+
+function responsivePage() {
+  if(screenWidth.matches) {
+    main.style.flexDirection = "column";
+    main.style.padding = "1em";
+    main.style.alignItems = "center";
+    footer.style.flexDirection = "column";
+  } else {
+    main.style.flexDirection = "row";
+    main.style.justifyContent = "space-around";
+  }
+}
+ responsivePage(screenWidth);
+ screenWidth.addListener(responsivePage)
 /*
 Select the warning at the end of 
 the page by its id warning. Select 
 the id without using a querySelector. 
 Change the size and font of the warning.
 */
+
+let warning = document.getElementById("warning");
+warning.style.fontSize = "1.5rem";
+warning.style.textAlign = "center";
+warning.style.fontFamily = "serif";
+
+/* 
+Select all even allergy-info items. 
+Give the even items a different background color.
+Make the allergy-warning appear as a column in 
+the center of the page.
+*/
+
+
+let evenAllergyInfo = document.querySelectorAll('.allergy-info');
+for(let i = 0; i < evenAllergyInfo.length; i++) {
+  evenAllergyInfo[i].style.textAlign = "left";
+  evenAllergyInfo[i].style.listStyle = "none";
+  evenAllergyInfo[i].style.display = "flex";
+  evenAllergyInfo[i].style.justifyContent ="flex-start";
+  evenAllergyInfo[i].style.width = "30%";
+  evenAllergyInfo[i].style.margin = "0 auto";
+}
+for(let i = 0; i<evenAllergyInfo.length; i++) {
+  evenAllergyInfo[++i].style.backgroundColor = "lightblue";
+
+}
+
+/*
+The descriptions in the footer should also be styled, 
+e.g. a rounded border with a background color. 
+They should appear 
+in a column for mobile and in a row for desktop.
+*/
+let footer = document.querySelector('.footer');
+footer.style.display = "flex";
+footer.style.justifyContent = "space-around";
+
+let footerDesc = document.querySelectorAll(".food-desc");
+for(let i = 0; i < footerDesc.length; i++) {
+  footerDesc[i].style.fontSize = "0.9em"
+  footerDesc[i].style.border = "5px solid yellow"
+  footerDesc[i].style.padding = "3em";
+  footerDesc[i].style.borderRadius = "50%"
+  footerDesc[i].style.width = "5%";
+}
 
