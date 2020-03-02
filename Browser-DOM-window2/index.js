fe3288817dd50a1c4a7bae132f054c948c840cc3
o0pmitev <pmitev89@gmail.com>
2020-03-02 13:27:04 +0100

    Finished

--- Browser-DOM-window2/index.js
+++ Browser-DOM-window2/index.js
@@ -1,5 +1,20 @@
 const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
 let userNumber = parseInt(prompt(`Guess a number between 1 - 10`));
-function guessTheNumber(number) {
-  
+let attempts = 1;
+let newArr = [];
+newArr.push(userNumber);
+
+for (; attempts < 4; attempts++) {
+  if (newArr.includes(randomNumber)) {
+    alert(`Success, the number was ${randomNumber}. Attempts: ${attempts}`);
+  } else {
+    userNumber = parseInt(prompt('Wrong!!! Try Again!'))
+    newArr.push(userNumber);
+  }
 }
+if (!newArr.includes(randomNumber)) {
+  alert(`Sorry, you failed to guess the number in three attempts. The number was ${randomNumber}!`)
+}
+
+console.log(newArr);
