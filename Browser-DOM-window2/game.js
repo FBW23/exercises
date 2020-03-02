a3edfe2f94b1211a2b37639e192d84353cad7254
hbendis <bendishoxha1@gmail.com>
2020-03-02 13:56:53 +0100

    done

--- Browser-DOM-window2/game.js
+++ Browser-DOM-window2/game.js
@@ -1,4 +1,12 @@
-
-const randomNumber = Math.floor(Math.random()*10) +1;
-let name=  prompt('Guess a number between 1-10')
-alert(randomNumber);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+//alert(randomNumber);
+let guess = prompt("Guess a number between 1-10");
+for (let i = 1; i < 3; i++) {
+    if (parseInt(randomNumber) === guess) { //randomNumber == guess
+        alert("Congratulation you Got right NUMBER. \n \nThe Correct number is " + randomNumber);
+        break; // \n \ is to move into a new line
+    } else {
+        guess = prompt("Please try again");
+    }
+}
+alert("Sorry !!! No more Attemps. The correct Number was " + randomNumber);
