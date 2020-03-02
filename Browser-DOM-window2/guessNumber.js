c21058be91ba01c15654def31d61bc525ceb7192
grizman040 <grizman040@gmail.com>
2020-03-02 12:58:07 +0100

    fixed bug with equals and beautify

--- Browser-DOM-window2/guessNumber.js
+++ Browser-DOM-window2/guessNumber.js
@@ -1,12 +1,15 @@
 let randomNumber = Math.floor(Math.random() * 10 + 1);
-alert(randomNumber);
+alert("You have 3 Attempts to guess a Number from 1-10 ");
 let guess = prompt("Guess a number between 1-10");
-for (let i = 0; i < 3; i++) {
+for (let i = 1; i < 3; i++) {
   if (randomNumber == guess) {
-    alert("Congratulation you Got right NUMBER. \n \nThe Correct number is " +randomNumber );
+    alert(
+      "Congratulation you Got right NUMBER. \n \nThe Correct number is " +
+        randomNumber
+    );
     break;
   } else {
     guess = prompt("Please try again");
   }
 }
-alert("Sorry !!! No more Attemps. The correct Number was " + randomNumber);
+alert("Sorry !!! No more Attempts. The correct Number was " + randomNumber);
