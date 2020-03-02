6a5c55983ffddbf4ae6a49aadcb17d87cb3dd758
grizman040 <grizman040@gmail.com>
2020-03-02 10:32:31 +0100

    dsa

--- PB-arrays-pt1/arrays.js
+++ PB-arrays-pt1/arrays.js
@@ -1,5 +1,5 @@
 // 1. Declare a variable named "euroCities" and assign an array to the variable e.g. ["Paris", "London", "Valletta", "Prague", "Rome"]. Declare another variable and assign the second item of the array as a value.
-const euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
+let euroCities = ["Paris", "London", "Valletta", "Prague", "Rome"];
 console.log(euroCities);
 const second = euroCities[1];
 console.log(second); // London
@@ -9,14 +9,14 @@ console.log(euroCities);
 // 3. Print the length of the array "euroCities"
 console.log(euroCities.length); // 5
 // 4. Remove the last item of the array "euroCities"
-// POP 
+// POP
 euroCities.pop();
 console.log(euroCities);
 // 5. Use an array method add "Budapest"
 euroCities.push("Budapest");
 console.log(euroCities);
-// 6. Bonus: 
-euroCities[1] = undefined; // not enough??? 
+// 6. Bonus:
+euroCities[1] = undefined; // not enough???
 console.log(euroCities);
 euroCities.splice(1, 2); // delete starts at 1, delete 2 items
 console.log(euroCities);
@@ -24,14 +24,14 @@ console.log(euroCities);
 const asianCities = ["Manila", "Bangkok", "Beijing", "Kuala Lumpur", "Tokyo"];
 // 8. Use an array method to select items 2-4 from the array of asianCities and store this in another variable
 
-const newCities = asianCities.splice(2, 4); // cuts out 3 from the original 
+const newCities = asianCities.splice(2, 4); // cuts out 3 from the original
 console.log(newCities);
-// only 2 cities left 
+// only 2 cities left
 console.log(asianCities.length); // 2
 
 // 9. concat() putting arrays together in one array
 const worldCities = euroCities.concat(asianCities);
-console.log(worldCities); // length of 5 
+console.log(worldCities); // length of 5
 
 // 10. Reverse the order ??? reverse()
 worldCities.reverse();
@@ -45,30 +45,35 @@ console.log(worldCities);
 worldCities.splice(1, 0, "Washington");
 console.log(worldCities);
 
-// 13. write a program to put an array into a string 
+// 13. write a program to put an array into a string
 console.log(worldCities.toString()); // puts everything together with a comma
-console.log(worldCities.join(", ")); // puts all arrays together into one string with a seperator 
+console.log(worldCities.join(", ")); // puts all arrays together into one string with a seperator
 console.log(worldCities.join("+"));
 
-// 14. program to reverse the string hello world 
+// 14. program to reverse the string hello world
 const myString = "hello world";
 const myArray = myString.split("");
 console.log(myArray.length);
 myArray.reverse();
 console.log(myArray);
-console.log(myArray.join("")); // Without seperator 
-// shorter!!! 
-console.log(myString.split("").reverse().join(""));
+console.log(myArray.join("")); // Without seperator
+// shorter!!!
+console.log(
+  myString
+    .split("")
+    .reverse()
+    .join("")
+);
 
-// Sibling names = Katharina and Isabella 
-// 15. 
-const mySiblings = ['Katharina', 'Isabella'];
-const parentsNames = ['Josef', 'Claudia'];
+// Sibling names = Katharina and Isabella
+// 15.
+const mySiblings = ["Katharina", "Isabella"];
+const parentsNames = ["Josef", "Claudia"];
 const allNames = mySiblings.concat(parentsNames); // add both together
 console.log(allNames);
 
 //const catsNames = ['Tosun', 'Fatma']; // add ??? PUSH
-allNames.push('Tosun', 'Fatma');
+allNames.push("Tosun", "Fatma");
 console.log(allNames);
 // 5. reverse order
 allNames.reverse();
@@ -76,46 +81,45 @@ console.log(allNames);
 // 6. access parent
 console.log(allNames[3]);
 // 7. change
-allNames[2] = 'Felizitas';
+allNames[2] = "Felizitas";
 console.log(allNames);
 
 // 1. The Greater Numbers. Create a function which accepts two arguments: the first argument being an array of numbers, and the second argument being a number. The function should return the elements of the array which are greater than the second argument.
 
 const findGreatest = (myArray, myNumber) => {
-    if (myArray[0] > myNumber) {
-        console.log(myArray[0]);
-    }
-    if (myArray[1] > myNumber) {
-        console.log(myArray[1]);
-    }
-    if (myArray[2] > myNumber) {
-        console.log(myArray[2]);
-    }
+  if (myArray[0] > myNumber) {
+    console.log(myArray[0]);
+  }
+  if (myArray[1] > myNumber) {
+    console.log(myArray[1]);
+  }
+  if (myArray[2] > myNumber) {
+    console.log(myArray[2]);
+  }
 };
 findGreatest([3, 4, 5], 4); // ➞ 5
 findGreatest([10, 20, 30], 12); // ➞ 20, 30
 findGreatest([0, 10, 3], 4); // ➞ 10
 
-// 2. For the longest word. Create a function to find the longest word in a given string. i.e. tip: use split method!  
+// 2. For the longest word. Create a function to find the longest word in a given string. i.e. tip: use split method!
 
 // longestWord("this is a web development course"); // ➞ "development"
 
-
 const findVowels = str => {
-    let sum = 0;
-    let strVowels = "aeiou";
-    for (let i = 0; i < str.length; i++) {
-        if (strVowels.indexOf(str[i]) !== -1) {
-            sum++;
-        }
+  let sum = 0;
+  let strVowels = "aeiou";
+  for (let i = 0; i < str.length; i++) {
+    if (strVowels.indexOf(str[i]) !== -1) {
+      sum++;
     }
-    return sum;
+  }
+  return sum;
 };
 console.log(findVowels("this is a string"));
 
-const myVowels = mV => mV.replace(/[^aeiou]/gi, '').length;
+const myVowels = mV => mV.replace(/[^aeiou]/gi, "").length;
 
-console.log(myVowels('this is a string'));
+console.log(myVowels("this is a string"));
 
 //5. Missing Number. Create a function that takes an array of all integers between 1 and 10 (excluding one) and returns the missing integer.
 
