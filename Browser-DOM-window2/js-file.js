e78e66e545625033daf40a751962f3c15209e6be
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-02 13:30:37 +0100

    finished

--- Browser-DOM-window2/js-file.js
+++ Browser-DOM-window2/js-file.js
@@ -11,19 +11,20 @@
 function randomFunction() {
   let randomNumber = Math.floor(Math.random() * 11);
 
-  let message = prompt(`Guess the number between 0 and 10. (The right number is ${randomNumber}).`);
+  let message = prompt(`Guess the number between 1 and 10. (The right number is ${randomNumber}).`);
 
   let attempts = 3;
 
-  for (let i = 0; i < attempts; i++) {
+  for (let i = 0; i < 4; i++) {
     if (message == randomNumber) {
       alert(`Congratulations! You guessed the right number.`);
       return `The right number was ${randomNumber}.`;
     } else {
-      prompt(`Your number was wrong. You have ${--attempts} attempts left.`);
+      message = prompt(`Your number was wrong. You have ${--attempts} attempts left. Type a new number!`);
     }
   }
   alert(`You ran out of attempts. The correct number is ${randomNumber}.`);
+
 }
 
-  randomFunction();
+randomFunction();
