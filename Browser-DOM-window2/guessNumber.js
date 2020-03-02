97672e45d076621d2049a7d24b26c0fb2b247e53
grizman040 <grizman040@gmail.com>
2020-03-02 12:05:51 +0100

    da

--- Browser-DOM-window2/guessNumber.js
+++ Browser-DOM-window2/guessNumber.js
@@ -2,12 +2,12 @@ let randomNumber = Math.floor(Math.random() * 10 + 1);
 alert(randomNumber);
 let guess = prompt("Guess a number between 1-10");
 // let answer = 3;
-for (let i = 1; i < 3; i++) {
-  if (guess === randomNumber) {
+for (let i = 1; i < 10; i++) {
+  if (randomNumber === guess) {
     alert("Congratulation you Got right NUMBER");
     break;
   } else {
     guess = prompt("Please try again");
   }
 }
-alert("the corect Number was " + randomNumber);
+alert("the correct Number was " + randomNumber);
