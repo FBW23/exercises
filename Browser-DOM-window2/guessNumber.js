01ef13d7a8cac648d2d1a8f0b553c95850eeb4f7
grizman040 <grizman040@gmail.com>
2020-03-02 12:28:56 +0100

    fixed bug with

--- Browser-DOM-window2/guessNumber.js
+++ Browser-DOM-window2/guessNumber.js
@@ -1,13 +1,12 @@
 let randomNumber = Math.floor(Math.random() * 10 + 1);
 alert(randomNumber);
 let guess = prompt("Guess a number between 1-10");
-// let answer = 3;
-for (let i = 1; i < 10; i++) {
-  if (randomNumber === guess) {
-    alert("Congratulation you Got right NUMBER");
+for (let i = 0; i < 3; i++) {
+  if (randomNumber == guess) {
+    alert("Congratulation you Got right NUMBER. \n \nThe Correct number is " +randomNumber );
     break;
   } else {
     guess = prompt("Please try again");
   }
 }
-alert("the correct Number was " + randomNumber);
+alert("Sorry !!! No more Attemps. The correct Number was " + randomNumber);
