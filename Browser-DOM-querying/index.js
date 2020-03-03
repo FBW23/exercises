d6485aba753e35e0aeffd35607621c10985b2c5b
juliapim <54452216+juliapim@users.noreply.github.com>
2020-03-03 11:49:00 +0100

    dom

--- Browser-DOM-querying/index.js
+++ Browser-DOM-querying/index.js
@@ -1,17 +1,31 @@
 let body = document.querySelector('body');
-console.log(body);
+
 
 body.style.fontFamily = 'arial';
 
 document.querySelector('.title').style.textAlign = ('center');
-let categories = document.querySelector('.category');
+let categories = document.querySelectorAll('.category');
 
 
-    for (let i=0; i<categories.length; i++){
-        categories[i].style.color = ('blue')
-    }
+for (let i = 0; i < categories.length; i++) {
+    categories[i].style.color = 'pink';
+    categories[i].style.fontStyle = "italic";
+}
 
 
 
+let foodCategories = document.querySelectorAll('.food-category');
 
-// category.style.fontStyle = ('italic');
+function colorGenerator() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
+
+
+for (let i = 0; i < foodCategories.length; i++) {
+    foodCategories[i].style.backgroundColor = colorGenerator();
+    foodCategories[i].style.display = 'flex';
+    foodCategories[i].style.flexDirection = 'row';
+    foodCategories[i].style.width = '200px';
+    foodCategories[i].style.heigth = '500px';
+    foodCategories[i].style.alignItems = ('')
+}
