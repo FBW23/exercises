b686c80e9959e79e5890e856bafce29b6e9e0599
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-03 14:31:53 +0100

    cmmtt

--- Browser-DOM-querying/jscript.js
+++ Browser-DOM-querying/jscript.js
@@ -34,6 +34,15 @@ for (let i = 0; i < foodCategory.length; i++) {
     foodCategory[i].style.background = colorGenerator();
 }
 
+// let unorderedLists = document.querySelectorAll(".food-category");
+// console.log(unorderedLists);
+// unorderedLists.forEach((item) => {
+//   item.style.backgroundColor = colorGenerator();
+//   item.style.padding = "0 2em 2em";
+//   item.style.width = "80%";
+//   item.style.margin = "1em 0";
+// });
+
 // * Align the `food-category` lists in a row on desktop. Make sure that the page is responsive and also works on mobile.
 
 let ulAlign = document.querySelectorAll('ul');
@@ -41,11 +50,51 @@ let ulAlign = document.querySelectorAll('ul');
 if (window.matchMedia("(min-width: 400px)").matches) {
     /* the viewport is at least 400 pixels wide */
     for (let i = 0; i < foodCategory.length; i++) {
-        foodCategory[i].style.width = '25%';
-        // foodCategory[i].style.textAlign = 'center';
-        foodCategory[i].style.flex = 'flex-grow';
-        foodCategory[i].style.margin = "30px 10px 10px 30px";
+        foodCategory[i].style.width = '27%';
+        // foodCategory[i].style.textAlign = 'center';        
     }
 } else {
     /* the viewport is less than 400 pixels wide */
 }
+
+const main = document.querySelector('main');
+main.style.display = 'flex';
+main.style.justifyContent = "space-around";
+
+// * Select the warning at the end of the page by its id `warning`. Select the id without using a querySelector. Change the size and font of the `warning`.
+
+const warningSize = document.getElementById('warning');
+warningSize.style.fontSize = '77px';
+warningSize.style.fontFamily = 'Roboto';
+
+// * Select all _even_ `allergy-info` items. Give the even items a different background color.
+
+// const evenElements = document.querySelectorAll('.allergy-info:nth-child(even)');
+
+// for (let item in evenElements) {
+//     evenElements[item].style.background = "rgb(135, 200, 238)";
+//     // evenElements[item].style.width = '30%';
+// }
+
+const evenElements = document.querySelectorAll('.allergy-info');
+
+function evenSort() {
+    for (let i = 0; i < evenElements.length; i++) {
+        if (i % 2 !== 0) {
+            evenElements[i].style.background = 'rgb(135, 200, 238)';
+        }
+    }
+    for (let item of evenElements) {
+        item.style.width = '30%';
+    }
+}
+
+evenSort();
+
+// * Make the `allergy-warning` appear as a column in the center of the page.
+
+const section = document.querySelector('.allergy-warning');
+
+console.log(section)
+
+// KEINE AHNUNG ICH DREHE DURCH
