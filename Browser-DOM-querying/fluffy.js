dc778b52d2817c1a496f957a2d56f401af552fe4
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-03-03 14:31:36 +0100

    bbbbb

--- Browser-DOM-querying/fluffy.js
+++ Browser-DOM-querying/fluffy.js
@@ -3,14 +3,62 @@ title.style.textAlign = "center";
 
 
 document.querySelector("body").style.fontFamily = "arial";
-document.querySelector(".main").style.color='purple';
+document.querySelector(".main").style.color = 'black';
 document.querySelector(".food-category").style.display = "row";
-document.querySelector(".category").style.fontFamily="helvetica";
-let category=document.querySelectorAll(".category");
-for(let i=0;i<category.length;i++){
-    category[i].style.color='red';
-    }
-    for(let i=0;i<category.length;i++){
-        category[i].fontSize="10";
-    }
+document.querySelector(".category").style.fontFamily = "helvetica";
+let category = document.querySelectorAll(".category");
+for (let i = 0; i < category.length; i++) {
+    category[i].style.color = 'grey';
+    category[i].style.fontSize = "30px";
+}
+
+function randomColors() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 0.8)';
+}
+
+let foodie = document.querySelectorAll('.food-category');
+for (let i = 0; i < foodie.length; i++) {
+    foodie[i].style.background = randomColors();
+        foodie[i].style.width="30%";
+  
+    
+}
+const main=document.querySelector('main');
+main.style.display='flex';
+main.style.justifyContent='space-between';
+
+document.getElementById("warning").style.fontFamily = "roboto";
+document.getElementById('warning').style.fontSize="50px";
+document.getElementById('warning').style.textAlign='center';
+
+let infofo = document.querySelectorAll(".allergy-info:nth-child(even)");
+
+for (let i = 0; i < infofo.length; i++) {
+    infofo[i].style.background="red";
+         
+}
+const labrador=document.querySelector('.allergies');
+labrador.style.listStyleType="none";
+labrador.style.display='flex';
+labrador.style.flexDirection='column';
+labrador.style.width="30%";
+labrador.style.margin="auto";
+
+labrador.style.textAlign="center";
+
+let roundy = document.querySelectorAll(".food-desc");
+for (let i = 0; i < roundy.length; i++) {
+    roundy[i].style.background="#BADA55";
+    roundy[i].style.borderRadius='50%';
+    roundy[i].style.textAlign="center";
+    roundy[i].style.width="250px";
+    roundy[i].style.height="250px";
+    
+    roundy[i].style.display="inline-block";
+        
+}
+
+let footy=document.querySelector('.footer');
+footy.style.margin="auto";
+
 
