3192850f5bb7503cc90c56ac7f07bb15871badbe
juliapim <54452216+juliapim@users.noreply.github.com>
2020-03-03 10:33:39 +0100

    dom

---
+++ Browser-DOM-querying/index.js
@@ -0,0 +1,17 @@
+let body = document.querySelector('body');
+console.log(body);
+
+body.style.fontFamily = 'arial';
+
+document.querySelector('.title').style.textAlign = ('center');
+let categories = document.querySelector('.category');
+
+
+    for (let i=0; i<categories.length; i++){
+        categories[i].style.color = ('blue')
+    }
+
+
+
+
+// category.style.fontStyle = ('italic');
