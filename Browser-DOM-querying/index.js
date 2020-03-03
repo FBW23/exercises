667cff37e88afb38380c37b30bdf3eb57df54f96
o0pmitev <pmitev89@gmail.com>
2020-03-03 10:09:03 +0100

    Finished Browser-DOM-queryng

--- Browser-DOM-querying/index.js
+++ Browser-DOM-querying/index.js
@@ -3,6 +3,8 @@
 let body = document.body;
 
 body.style.fontFamily = "mono";
+body.style.lineHeight = "1.5";
+body.style.letterSpacing = ".1rem";
 
 //Center the text of h1 on the page.
 
@@ -14,9 +16,13 @@ heading.style.textAlign = "center";
 //the color and font-style of each element.
 
 let menuHeadings = document.querySelectorAll(".category");
-for(let i =0; i < menuHeadings.length; i++) {
+for (let i = 0; i < menuHeadings.length; i++) {
   menuHeadings[i].style.color = "red";
   menuHeadings[i].style.fontFamily = "serif";
+  menuHeadings[i].style.fontSize = "2rem";
+  menuHeadings[i].style.borderBottom = "2px solid black";
+  menuHeadings[i].style.display = "block";
+  menuHeadings[i].style.marginBottom = "1em"
 }
 
 /*
@@ -28,16 +34,19 @@ background color using the function that you created.
 */
 
 function randomRgba() {
-  let o = Math.round, r = Math.random, s = 255;
-  return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+  let o = Math.round,
+    r = Math.random,
+    s = 255;
+  return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
 }
 
 let unorderedLists = document.querySelectorAll(".food-category");
 console.log(unorderedLists)
-for(let i = 0; i < unorderedLists.length; i++) {
+for (let i = 0; i < unorderedLists.length; i++) {
   unorderedLists[i].style.backgroundColor = randomRgba();
   unorderedLists[i].style.padding = "2em";
-  unorderedLists[i].style.width = "25%";
+  unorderedLists[i].style.width = "80%";
+  unorderedLists[i].style.margin = "1em 0"
 }
 
 /*
@@ -53,7 +62,7 @@ main.style.width = "100%";
 
 
 function responsivePage() {
-  if(screenWidth.matches) {
+  if (screenWidth.matches) {
     main.style.flexDirection = "column";
     main.style.padding = "1em";
     main.style.alignItems = "center";
@@ -61,10 +70,13 @@ function responsivePage() {
   } else {
     main.style.flexDirection = "row";
     main.style.justifyContent = "space-around";
+    for (let i = 0; i < unorderedLists.length; i++) {
+      unorderedLists[i].style.margin = "1em";
+    }
   }
 }
- responsivePage(screenWidth);
- screenWidth.addListener(responsivePage)
+responsivePage(screenWidth);
+screenWidth.addListener(responsivePage);
 /*
 Select the warning at the end of 
 the page by its id warning. Select 
@@ -86,15 +98,15 @@ the center of the page.
 
 
 let evenAllergyInfo = document.querySelectorAll('.allergy-info');
-for(let i = 0; i < evenAllergyInfo.length; i++) {
+for (let i = 0; i < evenAllergyInfo.length; i++) {
   evenAllergyInfo[i].style.textAlign = "left";
   evenAllergyInfo[i].style.listStyle = "none";
   evenAllergyInfo[i].style.display = "flex";
-  evenAllergyInfo[i].style.justifyContent ="flex-start";
+  evenAllergyInfo[i].style.justifyContent = "flex-start";
   evenAllergyInfo[i].style.width = "30%";
   evenAllergyInfo[i].style.margin = "0 auto";
 }
-for(let i = 0; i<evenAllergyInfo.length; i++) {
+for (let i = 0; i < evenAllergyInfo.length; i++) {
   evenAllergyInfo[++i].style.backgroundColor = "lightblue";
 
 }
@@ -107,14 +119,19 @@ in a column for mobile and in a row for desktop.
 */
 let footer = document.querySelector('.footer');
 footer.style.display = "flex";
-footer.style.justifyContent = "space-around";
+footer.style.justifyContent = "center";
+footer.style.marginTop = "3em";
+footer.style.alignItems = "center";
+
 
 let footerDesc = document.querySelectorAll(".food-desc");
-for(let i = 0; i < footerDesc.length; i++) {
-  footerDesc[i].style.fontSize = "0.9em"
-  footerDesc[i].style.border = "5px solid yellow"
-  footerDesc[i].style.padding = "3em";
+for (let i = 0; i < footerDesc.length; i++) {
+  footerDesc[i].style.fontSize = "0.5em"
+  footerDesc[i].style.border = "5px solid orange"
+  footerDesc[i].style.padding = "5em";
   footerDesc[i].style.borderRadius = "50%"
   footerDesc[i].style.width = "5%";
-}
+  footerDesc[i].style.margin = "1em 1.5em";
+  footerDesc[i].style.textAlign = "center";
 
+}
