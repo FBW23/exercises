6820c3afd42a78124fb643e0a83aa21ab7e9e4cf
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-03-03 10:32:01 +0100

    mmmm

---
+++ Browser-DOM-querying/fluffy.js
@@ -0,0 +1,16 @@
+let title = document.querySelector('.title');
+title.style.textAlign = "center";
+
+
+document.querySelector("body").style.fontFamily = "arial";
+document.querySelector(".main").style.color='purple';
+document.querySelector(".food-category").style.display = "row";
+document.querySelector(".category").style.fontFamily="helvetica";
+let category=document.querySelectorAll(".category");
+for(let i=0;i<category.length;i++){
+    category[i].style.color='red';
+    }
+    for(let i=0;i<category.length;i++){
+        category[i].fontSize="10";
+    }
+
