b35dd7f6f1d26e4e305d9d28398d2b2faa6ecb58
nagster23 <nagsster.s@gmail.com>
2020-03-03 15:20:16 +0100

    finish DOM

--- Browser-DOM-querying/dom.js
+++ Browser-DOM-querying/dom.js
@@ -2,35 +2,104 @@
 // console.log(category);
 // category.style.color= 'blue';
 
-document.querySelector('body').style.fontFamily= "Impact,Charcoal,sans-serif";
+document.querySelector('body').style.fontFamily = "Impact,Charcoal,sans-serif";
 
-document.querySelector('h1').style.textAlign= "center";
+document.querySelector('h1').style.textAlign = "center";
+
+//Food Category
 
 let category = document.querySelectorAll('.category');
-for (let i =0; i<category.length; i++) {
-    category[i].style.color = 'blue';
+for (let i = 0; i < category.length; i++) {
     category[i].style.fontFamily = "Andale Mono, monospace";
+    category[i].style.fontSize = '1.5rem';
+    category[i].style.fontStyle = 'italic';
+    category[i].style.textDecoration = 'underline';
 }
 
 function generateRandomColor() {
     const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
 }
-let a=document.querySelectorAll('.food-category');
+let a = document.querySelectorAll('.food-category');
 for (let b = 0; b < a.length; b++) {
     a[b].style.backgroundColor = generateRandomColor();
-    // a[b].style.flexbox.insertRow();
 }
 function myFunction(b) {
     if (b.matches) { // If media query matches
-      document.body.style.backgroundColor = "yellow";
+        document.body.style.backgroundColor = "yellow";
     } else {
-     document.body.style.backgroundColor = "white";
+        document.body.style.backgroundColor = "white";
     }
-  }
-  let b = window.matchMedia("(max-width: 700px)")
-  myFunction(b) // Call listener function at run time
-  b.addListener(myFunction) // Attach listener function on state changes
+}
+let b = window.matchMedia("(max-width: 700px)")
+myFunction(b) // Call listener function at run time
+b.addListener(myFunction) // Attach listener function on state changes
 
-  
- 
+
+const foodCategories = document.querySelectorAll(".food-category");
+foodCategories.forEach(({
+    style: categoryBox
+}) => {
+    categoryBox.backgroundColor = generateRandomColor();
+    categoryBox.width = '29%';
+    categoryBox.border = 'solid', '1px';
+    categoryBox.lineHeight = '2rem';
+
+
+})
+
+//Allergy Warning
+
+const main = document.querySelector('main');
+main.style.display = 'flex';
+main.style.justifyContent = 'space-between';
+
+
+let warning = document.getElementById("warning");
+warning.style.fontSize = '2rem';
+warning.style.fontFamily = 'Garamond';
+warning.style.textAlign = 'center';
+
+let evenNumber = document.querySelectorAll('.allergy-info:nth-child(even)');
+
+for (let i = 0; i < evenNumber.length; i++) {
+    evenNumber[i].style.backgroundColor = 'green';
+}
+
+
+const secondCenter = document.querySelectorAll('.allergy-info');
+secondCenter.forEach(({
+    style: alergyBox
+}) => {
+    alergyBox.display = 'flex';
+    alergyBox.margin = '0 auto';
+    alergyBox.width = '30%';
+    alergyBox.listStyle = 'none';
+
+});
+
+//Footer
+
+let footer = document.querySelector('.footer');
+footer.style.display = 'flex';
+footer.style.marginTop = '3em';
+footer.style.justifyContent = 'center';
+footer.style.alignItems = 'center';
+
+let borderRad = document.querySelectorAll('.food-desc');
+borderRad.forEach((data) => {
+    data.style.borderRadius = '50%';
+    data.style.border = '5px solid orange';
+    data.style.width = '150px';
+    data.style.height = '150px';
+    data.style.textAlign = 'center';
+    data.style.padding = '1em';
+    data.style.margin = '1em 1.5em';
+});
+
+let lastMargin = document.querySelectorAll('h3');
+lastMargin.forEach((last) => {
+    last.style.marginTop = '65px';
+});
+
+
