b0b1493f9feb4254608cfb2369086cf115a23db3
Julien Montagnon <jumontagnon@gmail.com>
2020-03-04 14:32:55 +0100

    bla

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -10,8 +10,7 @@ const books = [
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
+    img:  "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
   },
   {
     title: "Thinking with Type",
@@ -32,18 +31,35 @@ const books = [
 
 let firstBook = document.createElement('li');
 document.querySelector('.book-list').appendChild(firstBook);
+let im1 = document.createElement('IMG');
+document.querySelector('li').appendChild(im1);
+document.querySelector('li').classList.add('card');
+im1.src = "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy";
+document.querySelector('IMG').classList.add('book-cover');
+document.querySelector('IMG');
+let div = document.createElement('div');
+document.querySelector('.card').appendChild(div);
+document.querySelector('div').classList.add('card-body');
+let h5 = document.createElement('h5');
+document.querySelector('.card-body').appendChild(h5);
+document.querySelector('h5').classList.add('card-title');
+document.querySelector('h5').innerText = 'The Most Human Human';
+
+let p = document.createElement('p');
+document.querySelector('.card-body').appendChild(p);
+document.querySelector('p').classList.add('card-text');
+document.querySelector('p').innerText = 'Brian Christian';
+
+let a = document.createElement('a');
+document.querySelector('.card-body').appendChild(a);
+document.querySelector('a').classList.add('btn', 'btn-primary');
+document.querySelector('a').innerText = 'Read';
 
 
-let secondBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(secondBook);
 
-let thirdBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(thirdBook);
 
-let forthBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(forthBook);
 
-document.querySelectorAll('li').forEach(item => item.classList.add('book-cover'));
+document.querySelectorAll('li').forEach(item => item.classList.add('book-cover', 'book'));
 
-firstBook.setAttribute('href', books[0][3]);
-document.querySelector('.book-cover').appendChild();
+firstBook.setAttribute('href');
+document.querySelector('.book-cover').appendChild(firstBook);
