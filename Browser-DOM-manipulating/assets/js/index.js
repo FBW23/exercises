b1a24ac4d14fd60c5cecc792aafec3aebc96f1d8
o0pmitev <pmitev89@gmail.com>
2020-03-04 11:49:02 +0100

    Browser-DOM-manipulating

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -27,3 +27,53 @@ const books = [
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
 ];
+// bookImgLinks <- an Array with book image links
+// bookAuthors <- an Array with author names
+// booksTitle  <- an Array with books titles
+
+// const bookImgLinks = books.map(item => {
+//   return item.img;
+// });
+
+// // author names
+// const bookAuthors = books.map(item => {
+//   return item.author;
+// })
+
+// // book title
+// const booksTitle = books.map(item => {
+//   return item.title;
+// })
+// console.log(booksTitle);
+
+// // true or false arr
+// const readOrNot = books.map(item => {
+//   return item.alreadyRead;
+// })
+
+// console.log(readOrNot)
+
+function compare(a, b) {
+  const bookA = a.author.split(' ').reverse().join(' ').toUpperCase();
+  const bookB = b.author.split(' ').reverse().join(' ').toUpperCase();
+  
+  let comparison = 0;
+
+  if(bookA > bookB) {
+    comparison = 1;
+  } else if(bookA < bookB) {
+    comparison = -1
+  }
+  return comparison;
+}
+
+// [orderedObject] - alphabetical order according to the authors' last names
+const orderedObject = books.sort(compare);
+console.log(orderedObject);
+
+
+const bookList = document.querySelector(".book-list");
+// Add row class to the bookList
+bookList.classList.add("row");
+
+bookList.innerHTML += '<li>hello</li>'
