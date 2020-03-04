2f1a01930eaaf5d67236ea2c72a966ba72c10d01
o0pmitev <pmitev89@gmail.com>
2020-03-04 15:24:44 +0100

    FINISHED Browser DOM Manipulation

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -1,24 +1,20 @@
-const books = [
-  {
+const books = [{
     title: "The Design of EveryDay Things",
     author: "Don Norman",
     alreadyRead: false,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
   },
   {
     title: "Thinking with Type",
     author: "Ellen Lupton",
     alreadyRead: true,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
+    img: "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
   },
   {
     title: "Eloquent JavaScript",
@@ -56,12 +52,12 @@ const books = [
 function compare(a, b) {
   const bookA = a.author.split(' ').reverse().join(' ').toUpperCase();
   const bookB = b.author.split(' ').reverse().join(' ').toUpperCase();
-  
+
   let comparison = 0;
 
-  if(bookA > bookB) {
+  if (bookA > bookB) {
     comparison = 1;
-  } else if(bookA < bookB) {
+  } else if (bookA < bookB) {
     comparison = -1
   }
   return comparison;
@@ -76,7 +72,7 @@ const bookList = document.querySelector(".book-list");
 // Add row class to the bookList
 bookList.style.listStyle = "none";
 // create 4 <li> items with class col-sm
-for( let i = 0; i < 4; i ++) {
+for (let i = 0; i < 4; i++) {
   bookList.innerHTML += '<li class="book card"></li>'
 }
 
@@ -88,7 +84,7 @@ const bookImgLinks = orderedObject.map(item => {
 });
 
 console.log(bookImgLinks);
-allListElements.forEach((item,index) => {
+allListElements.forEach((item, index) => {
   item.innerHTML += `<a href="${bookImgLinks[index]}"><img class="book-cover card-img-top" src="${bookImgLinks[index]}"></a>`;
 })
 
@@ -101,7 +97,7 @@ const booksTitle = orderedObject.map(item => {
   return item.title;
 })
 
-allDivElements.forEach((item, index) => { 
+allDivElements.forEach((item, index) => {
   item.innerHTML += `<h5 class="card-title">${booksTitle[index]}</h5>`;
 })
 
@@ -110,7 +106,7 @@ const bookAuthors = orderedObject.map(item => {
   return item.author;
 })
 
-allDivElements.forEach((item, index) => { 
+allDivElements.forEach((item, index) => {
   item.innerHTML += `<p class="card-text">${bookAuthors[index].split(' ').reverse().join(' ')}</p>`;
 })
 
@@ -120,7 +116,34 @@ const readOrNot = orderedObject.map(item => {
   return item.alreadyRead;
 })
 
-allDivElements.forEach((item, index) => { 
-  item.innerHTML += `<button type="button" class="status btn btn-primary">${readOrNot[index]}</button>`;
+allDivElements.forEach((item, index) => {
+  item.innerHTML += `<button type="button" class="status btn-sm btn btn-success">${readOrNot[index]}</button>`;
 })
 
+
+// style
+const cardBody = document.querySelectorAll(".card-body");
+cardBody.forEach(item => {
+  item.style.display = "flex";
+  item.style.flexDirection = "column";
+  item.style.justifyContent = "space-between";
+})
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(item => {
+  item.style.width = "70%";
+  item.style.borderRadius = "35%";
+  item.style.placeSelf = "flex-end"
+});
+
+// read or to read 
+
+buttons.forEach(item => {
+  if (item.innerHTML === "true") {
+    item.innerHTML = "Read";
+  } else if (item.innerHTML === "false") {
+    item.innerHTML = "To Read";
+    item.classList.remove("btn-success");
+    item.classList.add("btn-secondary");
+  }
+});
