38c048dc00106ba6e1fd3b015f56a111bb8e38d4
wagowsky <vagobala@gmail.com>
2020-03-04 14:34:18 +0100

    SDFD

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -27,3 +27,19 @@ const books = [
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
 ];
+
+
+
+// for(i=0;i<3;i++){
+//   document.querySelector('.book-list.card-column').innerHTML +=  '<li></li>';
+// };
+let img1=books[0].img;
+document.querySelector('.book-list.card-column').innerHTML +=  `<div class="card" style="width: 18rem;">
+<img class="card-img-top" src="books[0].img" alt="Card image cap">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>`:
+document.querySelector('.book-list.card-column').style.listStyleType=("none");
