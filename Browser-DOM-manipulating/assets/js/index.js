4bd82bbfd27a40982f2977dab5e0862393089792
hbendis <bendishoxha1@gmail.com>
2020-03-04 16:01:25 +0100

    done

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -1,24 +1,20 @@
-const books = [
-  {
+const books = [{
     title: "The Design of EveryDay Things",
     author: "Don Norman",
     alreadyRead: false,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
   },
   {
     title: "Thinking with Type",
     author: "Ellen Lupton",
     alreadyRead: true,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
+    img: "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
   },
   {
     title: "Eloquent JavaScript",
@@ -28,7 +24,7 @@ const books = [
   }
 ];
 
-const createParagraph = (book) => {
+/*const createParagraph = (book) => {
   const paragraph = document.createElement("p");
   // write content to paragraph
   paragraph.innerText = `${book.title} - by ${book.author}`;
@@ -46,7 +42,29 @@ books.forEach((book) => {
   // insertAdjacentElement because `bookParagraph` is an HTML element
   // not a string
   h1.insertAdjacentElement('afterend', bookParagraph);
-})
+})*/
 
-
-
+const bookList = document.createElement('ul');
+for (var i = 0; i < books.length; i++) {
+ // let bookItem = document.createElement('li');
+//  let bookImg = document.createElement('img');
+//  bookImg.src = books[i].img;
+//  bookItem.appendChild(bookImg);
+ // const bookDescription = document.createTextNode(books[i].title + ' by ' + books[i].author);
+ // bookItem.appendChild(bookDescription);
+  //if (books[i].alreadyRead) {
+ //   bookItem.style.color = 'grey';
+ // }
+ // bookList.appendChild(bookItem);
+//}
+//document.body.appendChild(bookList);
+document.querySelector('ul').innerHTML +=
+  `<li class="card book" style="width: 18rem;">
+  <img class="card-img-top book-cover" src=${books[i].img} alt="Card image cap">
+  <section class="card-body">
+    <h5 class="card-title">${books[i].title}</h5>
+    <p class="card-text text-muted">${books[i].author}</p>
+    <a href="#" class="btn btn-primary status">${'Read'}</a>
+  </section>
+</li>`
+}
