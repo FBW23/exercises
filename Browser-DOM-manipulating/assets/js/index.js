f8b0de2d34a818895a6bc5901c707bc1159eaa48
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-05 09:35:19 +0100

    cmmt

--- Browser-DOM-manipulating/assets/js/index.js
+++ Browser-DOM-manipulating/assets/js/index.js
@@ -1,24 +1,20 @@
-const books = [
-  {
+const books = [{
     title: "The Design of EveryDay Things",
     author: "Don Norman",
     alreadyRead: false,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcTQEZhxiVNZAeKa1dGfEzKwLXiyY_78i08Gfhwn53k-JYin9TDO"
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
     alreadyRead: true,
-    img:
-      "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
+    img: "http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy"
   },
   {
     title: "Thinking with Type",
     author: "Ellen Lupton",
     alreadyRead: true,
-    img:
-      "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
+    img: "https://images-na.ssl-images-amazon.com/images/I/518%2BxIiELFL._SX258_BO1,204,203,200_.jpg"
   },
   {
     title: "Eloquent JavaScript",
@@ -28,37 +24,87 @@ const books = [
   }
 ];
 
-// FLEX CONTAINER
-document.querySelector('ul').classList.add('book-list');
+// arr[0].author ...
 
+books.forEach(book => {
 // FIRST BOOK
-
 
 let firstBook = document.createElement('li');
 document.querySelector('.book-list').appendChild(firstBook);
+document.querySelector('li').classList.add('card','book'); // MAIN CARD - class: card
 
-let image = document.createElement('img');
+let image = document.createElement('img'); // CARD IMG-TOP
 document.querySelector('li').appendChild(image);
-image.setAttribute('src', 'http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy');
-document.querySelector('img').classList.add('book', 'book-cover');
+image.setAttribute('src', book.img);
+image.classList.add('card-img-top', 'book-cover');
 
-let h4 = document.createElement('h4');
-document.querySelector('li').appendChild(h4);
-document.querySelector('h4').innerText = "The Most Human Human";
-document.querySelector('h4').classList.add('book');
+let section = document.createElement('section'); // CARD BODY
+document.querySelector('li').appendChild(section);
+section.classList.add('card-body');
 
-// SECOND BOOK
-let secondBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(secondBook);
+let title = document.createElement('h5'); // Book Title
+document.querySelector('section').appendChild(title);
+title.innerText = book.title;
+title.classList.add('card-title');
+
+let author = document.createElement('p'); // Author
+document.querySelector('section').appendChild(author);
+author.innerText = book.author;
+author.classList.add('card-text', 'text-muted');
+
+let link = document.createElement('a'); // Button link
+document.querySelector('section').appendChild(link);
+link.setAttribute('href', '#');
+link.innerText = 'Read';
+link.style.margin = '0rem 0rem 0rem 6rem';
+document.querySelector('a').classList.add('btn', 'btn-success', 'status');
+
+});
+
+// let firstBook = document.createElement('li');
+// document.querySelector('.book-list').appendChild(firstBook);
+// document.querySelector('li').classList.add('card','book'); // MAIN CARD - class: card
+
+// let image = document.createElement('img'); // CARD IMG-TOP
+// document.querySelector('li').appendChild(image);
+// image.setAttribute('src', 'http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy');
+// document.querySelector('img').classList.add('card-img-top', 'book-cover');
+
+// let section = document.createElement('section'); // CARD BODY
+// document.querySelector('li').appendChild(section);
+// document.querySelector('section').classList.add('card-body');
+
+// let title = document.createElement('h5'); // Book Title
+// document.querySelector('section').appendChild(title);
+// document.querySelector('h5').innerText = 'The Most Human Human';
+// document.querySelector('h5').classList.add('card-title');
+
+// let author = document.createElement('p'); // Author
+// document.querySelector('section').appendChild(author);
+// document.querySelector('p').innerText = 'Brian Christian';
+// document.querySelector('p').classList.add('card-text', 'text-muted');
+
+// let link = document.createElement('a'); // Button link
+// document.querySelector('section').appendChild(link);
+// link.setAttribute('href', '#');
+// link.innerText = 'Read';
+// link.style.margin = '0rem 0rem 0rem 6rem';
+// document.querySelector('a').classList.add('btn', 'btn-success', 'status');
 
 
-// THIRD BOOK
-let thirdBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(thirdBook);
 
-// FOURTH BOOK
-let fourthBook = document.createElement('li');
-document.querySelector('.book-list').appendChild(fourthBook);
 
-document.querySelectorAll('li').forEach(item => item.classList.add('book-cover', 'li-dot',));
+// // SECOND BOOK
+// let secondBook = document.createElement('li');
+// document.querySelector('.book-list').appendChild(secondBook);
 
+
+// // THIRD BOOK
+// let thirdBook = document.createElement('li');
+// document.querySelector('.book-list').appendChild(thirdBook);
+
+// // FOURTH BOOK
+// let fourthBook = document.createElement('li');
+// document.querySelector('.book-list').appendChild(fourthBook);
+
+// document.querySelectorAll('li').forEach(item => item.classList.add('book-cover', 'li-dot', ));
