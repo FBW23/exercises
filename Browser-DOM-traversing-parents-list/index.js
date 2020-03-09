cd394eaa8c4629873921b1d5cddb22934980d9b0
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-03-09 14:29:06 +0100

    yep

---
+++ Browser-DOM-traversing-parents-list/index.js
@@ -0,0 +1,32 @@
+// let c = document.body.children;
+// var x = document.getElementById("myLI").parentElement.nodeName;
+// allParents = document.querySelector("cite").parents("*")
+// let c = document.body.childNodes;
+
+// function GetParents(id) {
+//     let parents = document.querySelector("html" + id).childNodes("cite");
+//     let selector = "";
+//     for (var i = parents.length-1; i >= 0; i--) {
+//         selector += parents[i].tagName + " ";
+//     }
+
+//     selector += "#" + id;
+
+//     return selector;
+// }
+
+// let x = document.querySelector("cite");
+
+
+
+for (let i=0;x.parentElement;i++){
+    x=x.parentElement;
+    console.log(x)
+    
+   
+    }
+// element.parentNode
+// const listItems = document.querySelector('cite');
+// const listArray = Array.from(x);
+// listArray.forEach((x) => {console.log(...()});
+
