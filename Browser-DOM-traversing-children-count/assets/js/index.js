a984cef9b98fe865ad2d7fb35ab56a3018cb30b3
Julien Montagnon <jumontagnon@gmail.com>
2020-03-09 15:59:53 +0100

    bla

---
+++ Browser-DOM-traversing-children-count/assets/js/index.js
@@ -0,0 +1,10 @@
+// * Get the children of the element with id `wrapper` and print them to the console as a list.
+
+const a = document.getElementById('wrapper');
+console.log(a);
+
+// * Iterate over them and print their children to the console, as well as how many they are.
+
+while(a.childNodes) {
+    
+}
--- Browser-DOM-traversing-parents-list/parents.js
+++ Browser-DOM-traversing-parents-list/parents.js
@@ -6,11 +6,16 @@
 
 const x = document.querySelector('cite');
 let string = '';
-
-while(x.parentNode) {
+while (x.parentNode) {
     string += x.tagName.toLowerCase() + ' ';
     x = x.parentNode;
-
+    console.log(string)
+    console.log(x);
 }
+let array = string.trim().split(' ');
 
-
+console.log(array);
+array = array.reverse();
+console.log(array);
+string = array.join(' > ');
+console.log(string);
