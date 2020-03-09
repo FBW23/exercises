e6ffab85ef332bd6d1d9a8468815d320c3ab8c23
o0pmitev <pmitev89@gmail.com>
2020-03-09 14:11:41 +0100

    Finished

--- Browser-DOM-traversing-parents-list/script.js
+++ Browser-DOM-traversing-parents-list/script.js
@@ -1,11 +1,27 @@
-let elements = document.getElementsByTagName("*");
+// let elements = document.getElementsByTagName("*");
 
-const printTheDomTree = arr => {
-  let string = '';
-  for (let i = 0; i < arr.length - 1; i++) {
-    string += arr[i].tagName + '>';
-  }
-  console.log(string.toLowerCase());
+// const printTheDomTree = arr => {
+//   let string = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     string += arr[i].tagName + '>';
+//   }
+//   console.log(string.toLowerCase());
+// }
+
+// printTheDomTree(elements);
+
+let nodes = [];
+let str = '';
+let element = document.querySelector('cite');
+
+nodes.push(element);
+while (element.parentNode) {
+  nodes.unshift(element.parentNode);
+  element = element.parentNode;
 }
 
-printTheDomTree(elements);
+for (let i = 1; i < nodes.length; i++) {
+  str += nodes[i].tagName + ' > ';
+}
+
+console.log(str.toLowerCase());
