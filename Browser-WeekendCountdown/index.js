7768d354f21dd608311634163aebe5a3b06f2db5
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-10 15:01:48 +0100

    sdf

--- Browser-WeekendCountdown/index.js
+++ Browser-WeekendCountdown/index.js
@@ -1,14 +1,31 @@
 const submitButton = document.querySelector('#submit');
 const text = document.querySelector('p');
 let date = new Date();
+let day = date.getDay();
+let week = ['Sunday', 'Monday', 'Tuesday', 'Wednseday', 'Thursday', 'Friday', 'Saturday'];
+let currentDay = week[day];
+let daysLeft;
+
+if (currentDay === 'Monday') {
+    daysLeft = 5;
+} else if (currentDay === 'Tuesday') {
+    daysLeft = 4; 
+} else if (currentDay === 'Wednesday') {
+    daysLeft = 3;
+} else if (currentDay === 'Thursday') {
+    daysLeft = 2;
+} else if (currentDay === 'Friday') {
+    daysLeft = 1;
+}
 
 function modifyText() { 
     let inputValue = document.querySelector('#yourname').value;
-    text.innerText = `Hello ${inputValue}! Today is ${date}`;
+    text.innerText = `Hello ${inputValue}! Today is ${currentDay}. Only ${daysLeft} days left to the Weekend!`;
 }
 
 function clearText() {
     text.innerText = `Please enter your name`;
+    text.inputMode = 'reset';
 }
 
 const nameEnter = document.querySelector('#submit');
