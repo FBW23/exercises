0ca001cf7458146207b55a8dfba2b589d4dd88e6
aghy <zenlogie@gmail.com>
2020-03-10 14:52:11 +0100

    use form

--- Browser-WeekendCountdown/script.js
+++ Browser-WeekendCountdown/script.js
@@ -5,11 +5,8 @@ const text = document.querySelector("#text");
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let d = new Date();
-    let day = d.getDay();
+    let day = new Date().getDay();
     let num = 5 - day;
     let name = text.value;
-    console.log(day);
-    
-    target.innerText = `Hello ${name}. Today is ${weekdays[day]}. Only ${num} days left until weekend!`
+    target.innerText = `Hello ${name}. Today is ${weekdays[day]}. Only ${num} days left until weekend!`;
 });
