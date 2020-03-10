fcd712a163ef0ade88ae377fa5fd838ead49e495
grizman040 <grizman040@gmail.com>
2020-03-10 13:34:19 +0100

    exercise

---
+++ Browser-DOM-traversing-children-count/assets/js/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+// console.dir(document.all);
+const wrapper = document.getElementById('wrapper');
+console.log(wrapper);
+const children = wrapper.children;
+console.log(children);
+console.log(wrapper.lastChild);
+
+
+
+function highestNumberOfClasses(node) {
+let returnObject = {
+    elementWithTheMostClasses:undefined,
+    totalNumberOfClasses:0
+}
+
+
+return {elementWithTheMostClasses: 'citie'
+totalNumberOfClasses:'10'}
+}
+console.log(highestNumberOfClasses(children));
