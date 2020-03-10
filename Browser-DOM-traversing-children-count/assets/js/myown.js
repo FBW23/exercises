688f3e84a6e1a55d6c4da7bc9651159ee35b47a5
nagster23 <nagsster.s@gmail.com>
2020-03-10 11:13:16 +0100

    done

--- Browser-DOM-traversing-children-count/assets/js/myown.js
+++ Browser-DOM-traversing-children-count/assets/js/myown.js
@@ -1,7 +1,40 @@
-const childwrap = document.querySelector('wrapper');
-console.log(childwrap);
 
 const wrapper = document.getElementById('wrapper');
-for (let i = 0; i < wrapper.children.length; i++) {
-  console.log(wrapper.children[i].tagName);
+console.log('wrapper');
+const children = wrapper.children;
+console.log(children);
+console.log(children.length);
+
+for (let i = 0; i < children.length; i++) {
+    const element = children[i];
+    console.log(element.children, element.children.length);
+    console.log(element.children, element.childElementCount); // second method
 }
+
+console.log(wrapper.childElementCount);
+
+// Second Method counting children
+
+// no.2
+
+function highestNumberOfClasses(node) {
+    let returnObject = {
+        elementWithMostClasses: undefined,
+        totalNumberOfClasses: 0
+    };
+    // now the logical part
+let highestNumberOfClasses = node[0].classList.length;
+
+    for (let i = 0; i < node.length; i++) {
+        const element = node[i];
+        console.log(element.classList, element.classList.length);
+        if (element.classList.length > highestNumberOfClasses) {
+            highestNumberOfClasses = element.classList.length;
+            elementWithMostClasses =element;
+        }
+    }
+
+    return returnObject;
+}
+
+console.log(highestNumberOfClasses(children));
