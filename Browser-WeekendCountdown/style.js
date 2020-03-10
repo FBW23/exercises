0fa53bc1fdbcb61c024cfdc9d5665ef4820ec1a1
nagster23 <nagsster.s@gmail.com>
2020-03-10 15:29:05 +0100

    done

--- Browser-WeekendCountdown/style.js
+++ Browser-WeekendCountdown/style.js
@@ -4,10 +4,17 @@ let output = document.querySelector(".d"); // before the box
 let button = document.querySelector("#btn"); // button box
 
 const myFunction=()=>{
-    let today = new Date().getDay(); // it give the output of number of days
-    let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    day=day[today -1]; // will put numbers into string
+     let today = new Date().getDay(); // it give the output of number of days
+    // let day = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    // day=day[today -1]; // will put numbers into string
     let daysRemain = 5- today;
+    day=new Date().toLocaleString('en-EN',{
+        weekday:'long'
+    })
+
+    if(daysRemain<1){
+        daysRemain=0;
+    }
     if (input.value){
         output.innerText=`${input.value}, Today is ${day}, Only ${daysRemain} days until Weekend`;
     }// this will change the the result after you click.
