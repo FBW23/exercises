f765b8070b5a0243a26b63ef233c9e279a333468
hbendis <bendishoxha1@gmail.com>
2020-03-10 14:57:50 +0100

    done

--- Browser-WeekendCountdown/index.js
+++ Browser-WeekendCountdown/index.js
@@ -3,16 +3,18 @@
     - A greeting with the users name.
     - Current day
     - Days left till the weekend*/
+
 const target = document.querySelector("#target");
 const btn = document.querySelector("#btn");
-
+const text = document.querySelector("#text")
 btn.addEventListener("click", () => {
-    let weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    let weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
     let d = new Date();
     let day = d.getDay();
     let num = 5 - day;
     let name = text.value;
     console.log(day);
 
-    target.innerText = `Hello ${name}. Today is ${weekdays[day]}. Only ${num} days left until weekend!`
+    target.innerText = `Hello ${name}, today is ${weekdays[day]}. Only ${num} days left until weekend!`
 });
