83a8f4cc40696893d55325fac676b0b8de065a76
DCI-PC20 <juliarpim@gmail.com>
2020-03-10 16:01:34 +0100

    julia

---
+++ Browser-ToggleItems/index.js
@@ -0,0 +1,22 @@
+// # Toggle Favourite Destinations
+
+// Create a list of your favourite destinations with a button to toggle the list
+
+// **Requirements:**
+// - use Bootstrap CDN
+// - use `Element.classList` methods
+
+// **Example:**
+
+// ![screenshot](toggle.gif)
+
+// **Bonus:**
+
+// - Let the user select the favourite destination
+// - Show in the event object
+// - Style the list to your pleasing.
+
+// ![screenshot](select.gif)
+
+const button = document.querySelector('button');
+button.style.textAlign = 'left'
