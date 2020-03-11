27633526f0d89014963b129e0a6bfffa6584b421
o0pmitev <pmitev89@gmail.com>
2020-03-11 15:45:17 +0100

    FINISHED NewsletterSubscription

---
+++ Browser-NewsletterSubscription/popup.js
@@ -0,0 +1,9 @@
+const container = document.querySelector('.container');
+
+function showPopup() {
+  if (event.clientY > 100) {
+    container.classList.remove('hide')
+  }
+}
+
+window.addEventListener('mousemove', showPopup)
