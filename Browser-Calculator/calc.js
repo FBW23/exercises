964a05c9cdda19272bea8fb273251adb12543d08
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-03-11 15:59:10 +0100

    done

--- Browser-Calculator/calc.js
+++ Browser-Calculator/calc.js
@@ -1,27 +1,36 @@
-function showresult(choice) {
-    let n1 = parseFloat(document.getElementById('num1').value);
-    let n2 = parseFloat(document.getElementById('num2').value);
-    let r = 0;
-    let c = choice;
+function multiplyBy() {
+    num5 = document.getElementById("fifthNumber").value;
+    num6 = document.getElementById("sixthNumber").value;
+    let result3 = Number(num5) * Number(num6);
+    document.getElementById("result3").value = result3;
+}
 
-    switch (c) {
-        case '1':
-            r = n1 + n2;
-            break;
-        case '2':
-            r = n1 - n2;
-            break;
-        case '3':
-            r = n1 * n2;
-            break;
-        case '4':
-            r = n1 / n2;
-            break;
-        case '5':
-            r = n2 * 100 / n1;
-            break;
-        default:
-            break;
+document.getElementById("fifthNumber").addEventListener('change', multiplyBy);
+document.getElementById("sixthNumber").addEventListener('change', multiplyBy);
 
-    }
-    document.getElementById('result').value = r;}
+function divideBy() {
+    num7 = document.getElementById("seventhNumber").value;
+    num8 = document.getElementById("eigthNumber").value;
+    let result4 = Number(num7) / Number(num8)
+    document.getElementById("result4").value = result4;
+}
+document.getElementById("seventhNumber").addEventListener('change', divideBy);
+document.getElementById("eigthNumber").addEventListener('change', divideBy);
+
+function add() {
+    num3 = document.getElementById("thirdNumber").value;
+    num4 = document.getElementById("fourthNumber").value;
+    let result2 = Number(num3) + Number(num4);
+    document.getElementById("result2").value = result2;
+}
+document.getElementById("thirdNumber").addEventListener('change', add);
+document.getElementById("fourthNumber").addEventListener('change', add);
+
+function sub() {
+    num1 = document.getElementById("firstNumber").value;
+    num2 = document.getElementById("secondNumber").value;
+    let result = Number(num1) - Number(num2);
+    document.getElementById("result").value = result;
+}
+document.getElementById("firstNumber").addEventListener('change', sub);
+document.getElementById("secondNumber").addEventListener('change', sub);
