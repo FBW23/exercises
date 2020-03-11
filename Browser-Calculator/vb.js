8edf808014df3fb91fa6aa785614701fd5f332df
wagowsky <vagobala@gmail.com>
2020-03-11 11:49:10 +0100

    dsfds

--- Browser-Calculator/vb.js
+++ Browser-Calculator/vb.js
@@ -1,45 +1,102 @@
-# Calculator
+// # Calculator
 
-Description:
+// Description:
 
-- The user will be able to input two numbers for which they would like to do two types of calculations: Basic Arithmatic and Special Operations.
+// - The user will be able to input two numbers for which they would like to do two types of calculations: Basic Arithmatic and Special Operations.
 
-- Basic arithmatic will involve addition, subtraction, multiplication and division.
+// - Basic arithmatic will involve addition, subtraction, multiplication and division.
 
-- Special operations will deal with the calculation of modulo. 
+// - Special operations will deal with the calculation of modulo. 
 
-- As soon as the users input the numbers, they will see an immediate result on the page as an outcome of the operation.
+// - As soon as the users input the numbers, they will see an immediate result on the page as an outcome of the operation.
 
-Instructions:
+// Instructions:
 
-Create a Calculator app with the following operations:
+// Create a Calculator app with the following operations:
 
-1. Basic Arithmatic
+// 1. Basic Arithmatic
 
-- Addition
-- Subtraction
-- Multiplication
-- Division
+// - Addition
+// - Subtraction
+// - Multiplication
+// - Division
 
-2. Special Operations
+// 2. Special Operations
 
-- Modulo
+// - Modulo
 
-3. Create a function which calculates the result of two numbers depending on whether the inputs are being added, subtracted, multiplied or divided.
+// 3. Create a function which calculates the result of two numbers depending on whether the inputs are being added, subtracted, multiplied or divided.
 
-4. Required Calculations: addition, subtraction, multiplication, division and remainder of division.
+// 4. Required Calculations: addition, subtraction, multiplication, division and remainder of division.
 
-5. Display the result of the calculation to the user immediately.
+// 5. Display the result of the calculation to the user immediately.
 
-6. Include a header on the page.
+// 6. Include a header on the page.
 
-7. Create a simple UI so that the user can input two numbers to perform a calculation.
+// 7. Create a simple UI so that the user can input two numbers to perform a calculation.
 
-Note: the calculations should only include two operands.
+// Note: the calculations should only include two operands.
 
-8. **Optional:** 
-- Feel free to use Bootstrap for styling.
+// 8. **Optional:** 
+// - Feel free to use Bootstrap for styling.
 
-> Hint: Refer to the representation below, for an idea of what your result should look like.
+// > Hint: Refer to the representation below, for an idea of what your result should look like.
 
-![demo](demo.gif)
+// ![demo](demo.gif)
+
+
+let input1 = document.querySelector(".input1");
+let input2 = document.querySelector(".input2");
+
+//let input=document.querySelector("input");
+
+let sum = document.querySelector("#sum");
+let deduct = document.querySelector("#deduct");
+let multiply = document.querySelector("#multiply");
+let devide = document.querySelector("#devide");
+let modulo = document.querySelector("#modulo");
+
+
+
+
+const sumFunction = () => {
+    let result = document.querySelector("#sum .result");
+    let input1 = document.querySelector("#sum .input1");
+    let input2 = document.querySelector("#sum .input2");
+    result.value = Number(input1.value) + Number(input2.value);
+};
+const deductFunction = () => {
+    let result = document.querySelector("#deduct .result");
+    let input1 = document.querySelector("#deduct .input1");
+    let input2 = document.querySelector("#deduct .input2");
+    result.value = Number(input1.value) - Number(input2.value);
+};
+
+const devideFunction = () => {
+    let result = document.querySelector("#devide .result");
+    let input1 = document.querySelector("#devide .input1");
+    let input2 = document.querySelector("#devide .input2");
+    result.value = Number(input1.value) / Number(input2.value);
+};
+
+const multiplyFunction = () => {
+    let result = document.querySelector("#multiply .result");
+    let input1 = document.querySelector("#multiply .input1");
+    let input2 = document.querySelector("#multiply .input2");
+    result.value = Number(input1.value) * Number(input2.value);
+};
+
+const moduloFunction = () => {
+    let result = document.querySelector("#modulo .result");
+    let input1 = document.querySelector("#modulo .input1");
+    let input2 = document.querySelector("#modulo .input2");
+    result.value = Number(input1.value) % Number(input2.value);
+};
+
+
+
+sum.addEventListener("change", sumFunction);
+deduct.addEventListener("change", deductFunction);
+devide.addEventListener("change", devideFunction);
+multiply.addEventListener("change", multiplyFunction);
+modulo.addEventListener("change", moduloFunction);
