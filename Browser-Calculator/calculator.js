6d556db4be3fff472f50de6e95d842440a0a408a
o0pmitev <pmitev89@gmail.com>
2020-03-11 12:05:33 +0100

    Before the second break

--- Browser-Calculator/calculator.js
+++ Browser-Calculator/calculator.js
@@ -1,37 +1,75 @@
-// Calculator
+// ADDITION
+const additionElement = document.querySelector('.addition');
 
-let num1, num2;
+additionElement.addEventListener('change', (event) => {
+  const num1 = parseInt(document.querySelector('.add1').value);
+  const num2 = parseInt(document.querySelector('.add2').value);
+  const result = document.querySelector('.result-addition');
+  // Addition function
+  const addition = (x, y) => {
+    return x + y;
+  }
+  // adding the result from the addition to the input field
+  result.value = addition(num1, num2); // calling the addition function
+});
 
-const addition = (x, y) => {
-  return x + y;
-}
+// SUBTRACTION
+const subtractionElement = document.querySelector('.subtraction');
 
+subtractionElement.addEventListener('change', (event) => {
+  const num1 = document.querySelector('.sub1').value;
+  const num2 = document.querySelector('.sub2').value;
+  const result = document.querySelector('.result-subtraction');
+  // subtraction function
+  const subtraction = (x, y) => {
+    return x - y;
+  }
+  // adding the result from the subtraction to the input field
+  result.value = subtraction(num1, num2); // calling the subtraction function
+});
 
-const subtraction = (x, y) => {
-  return x - y;
-}
+// MULTIPLICATION
 
+const multiplicationElement = document.querySelector('.multiplication');
 
-const multiplication = (x, y) => {
-  return x * y;
-}
+multiplicationElement.addEventListener('change', (event) => {
+  const num1 = document.querySelector('.add1').value;
+  const num2 = document.querySelector('.add2').value;
+  const result = document.querySelector('.result-multiplication');
+  // multiplication function
+  const multiplication = (x, y) => {
+    return x * y;
+  }
+  // adding the result from the multiplication to the input field
+  result.value = multiplication(num1, num2); // calling the multiplication function
+});
 
+// DIVISION
 
-const division = (x, y) => {
-  return x / y;
-}
+const divisionElement = document.querySelector('.division');
 
-// special
-const modulo = (x, y) => {
-  return x % y;
-}
+divisionElement.addEventListener('change', (event) => {
+  const num1 = document.querySelector('.div1').value;
+  const num2 = document.querySelector('.div2').value;
+  const result = document.querySelector('.result-division');
+  // multiplication function
+  const division = (x, y) => {
+    return x / y;
+  }
+  // adding the result from the multiplication to the input field
+  result.value = division(num1, num2); // calling the multiplication function
+});
 
-// test 
-num1 = 2;
-num2 = 2;
-
-console.log(addition(num1,num2)); 
-console.log(subtraction(num1,num2)); 
-console.log(multiplication(num1,num2)); 
-console.log(division(num1,num2)); 
-console.log(modulo(num1,num2)); 
+// MODULO
+const moduloElement = document.querySelector('.modulo')
+moduloElement.addEventListener('change', (event) => {
+  const num1 = document.querySelector('.mod1').value;
+  const num2 = document.querySelector('.mod2').value;
+  const result = document.querySelector('.result-modulo');
+  // multiplication function
+  const modulo = (x, y) => {
+    return x % y;
+  }
+  // adding the result from the multiplication to the input field
+  result.value = modulo(num1, num2); // calling the multiplication function
+});
