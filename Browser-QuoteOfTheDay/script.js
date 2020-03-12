bdb7772bf0160c3f86ef39d805fbe7f121ebc25f
hbendis <bendishoxha1@gmail.com>
2020-03-12 11:14:13 +0100

    done

--- Browser-QuoteOfTheDay/script.js
+++ Browser-QuoteOfTheDay/script.js
@@ -1,41 +1,47 @@
-//const quotes = [
-    "Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.",
-    "Don't watch the clock; do what it does. Keep going.",
-    "The secret of getting ahead is getting started.",
-    "Well done is better than well said.",
-    "You miss 100% of the shots you don’t take.",
-    "A goal is a dream with a deadline.",
-    "Outstanding people have one thing in common: An absolute sense of mission.",
-    "Trying is winning in the moment.",
-    "Fall down seven times and stand up eight.",
-    "You just can't beat the person who never gives up.",
-    "There is little success where there is little laughter.",
-    "We cannot solve our problems with the same thinking we used when we created them."
-  //]; 
-
+// quotes are stored in an array of objects
+var quotes = [{
+    "attrib": "Steve Jobs",
+    "quote": "Design is not just what it looks like and feels like. Design is how it works."
+  }, {
+    "attrib": "Steve Jobs",
+    "quote": "Innovation distinguishes between a leader and a follower."
+  }, {
+    "attrib": "Albert Einstein",
+    "quote": "Reality is merely an illusion, albeit a very persistent one."
+  }, {
+    "attrib": "Albert Einstein",
+    "quote": "If you can't explain it simply, you don't understand it well enough."
+  }, {
+    "attrib": "Albert Einstein",
+    "quote": "Strive not to be a success, but rather to be of value."
+  }, {
+    "attrib": "Steve Jobs",
+    "quote": "Your time is limited, so don’t waste it living someone else’s life."
+  }, {
+    "attrib": "Bill Gates",
+    "quote": "Success is a lousy teacher. It seduces smart people into thinking they can't lose."
+  }, {
+    "attrib": "Albert Einstein",
+    "quote": "Science without religion is lame, religion without science is blind."
+  }, {
+    "attrib": "Henry Ford",
+    "quote": "Whether you think you can or you think you can’t, you’re right."
+  }];
   
- const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
-
-function getQuote() {
-  fetch(endpoint)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      displayQuote(data.message);
-    })
-    .catch(function () {
-      console.log("An error occurred");
+  // function to load and display a new quote
+  function newQuote() {
+    var quoteID = Math.floor(Math.random() * (quotes.length));
+    $("#quote").html(quotes[quoteID].quote);
+    $("#attrib").html(quotes[quoteID].attrib);
+  }
+  
+  // wait for page load before displaying content
+  $(document).ready(function() {
+    // load an initial quote
+    newQuote();
+  
+    // retrieve a new quote when the button is clicked
+    $("#generateButton").on("click", function() {
+      newQuote();
     });
-}
-
-function displayQuote(quote) {
-  const quoteText = document.querySelector('.quote-text');
-  quoteText.textContent = quote;
-
-}
-
-const newQuoteButton = document.querySelector('.new-quote');
-newQuoteButton.addEventListener('click', getQuote);
-
-getQuote();
+  });
