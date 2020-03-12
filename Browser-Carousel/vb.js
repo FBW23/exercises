bd9e15e78ba07f2341d8f323a52c0502161a0136
wagowsky <vagobala@gmail.com>
2020-03-12 13:32:44 +0100

    DSFDSFDSFDSF

--- Browser-Carousel/vb.js
+++ Browser-Carousel/vb.js
@@ -5,15 +5,16 @@ let img = document.querySelector("#images");
 
 
 const previous = () => {
+    
     if (img.style.backgroundImage = 'url(1.jpg)') {
         img.style.backgroundImage = 'url(2.jpg)';
     }
     else if (img.style.backgroundImage = 'url(/2.jpg)') {
         img.style.backgroundImage = 'url(/3.jpg)';
     }
-    // if (img.style.backgroundImage = 'url(/4.jpg)') {
-    //     img.style.backgroundImage = 'url(/5.jpg)';
-    // }
+    if (img.style.backgroundImage = 'url(/4.jpg)') {
+        img.style.backgroundImage = 'url(/5.jpg)';
+    }
     // if (img.style.backgroundImage = 'url(/5.jpg)') {
     //     img.style.backgroundImage = 'url(/6.jpg)';
     // }
--- Browser-NewsletterSubscription/vb.js
+++ Browser-NewsletterSubscription/vb.js
@@ -1,14 +1,3 @@
-// # Newsletter Subscription Overlay (Popup)
-
-// 1. Create a form item or popup that requests a user to sign up for your newsletter.
-
-// 1. Using the `MouseEvent.clientY`, write a function that triggers the _Newsletter Subscription Overlay_ when the user moves the mouse `< 10` pixels of the browsers **Y-axis**.
-
-// 1. **Optional:** Style to your pleasing.
-
-// > Hint: Refer to the representation below and watch the mouse behaviour, for an idea of what your result should look like.
-
-// ![demo](demo.gif)
 
 
 
@@ -16,13 +5,44 @@
 
 
 
-const pop = () => {
-   
-    if (event.clientY < 10) {
-        document.querySelector("#popup").style.display = "flex";
-        document.querySelector("body").style.background= "grey";
+
+// const pop = () => {
+
+//     if (event.clientY < 10) {
+//         document.querySelector("#popup").style.display = "flex";
+//         document.querySelector("body").style.background = "grey";
+//     }
+// }
+
+
+// document.addEventListener("mousemove", pop);
+
+
+let closeIt = () => {
+    if (event.keyCode === 27) {
+        document.querySelector("#popup").style.display = 'none';
+        document.querySelector("body").style.background = "none";
     }
 }
 
+document.addEventListener("keydown", closeIt);
 
-document.addEventListener("mousemove", pop);
+let openIt = () => {
+    if (event.keyCode === 13) {
+        document.querySelector("#popup").style.display = 'flex';
+        document.querySelector("body").style.background = "gray";
+    }
+}
+
+document.addEventListener("keyup", openIt);
+
+const form=document.querySelector("form");
+
+
+const mySubmit=()=>{
+    event.preventDefault();
+    }
+
+
+form.addEventListener("submit",mySubmit);
+
