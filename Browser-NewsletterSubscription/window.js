225399cbaa229841eb50c54d5b6e989e89f78359
grizman040 <grizman040@gmail.com>
2020-03-12 11:16:51 +0100

    c

--- Browser-NewsletterSubscription/window.js
+++ Browser-NewsletterSubscription/window.js
@@ -1,22 +1,5 @@
-// function tool(ctr) {
-//   let tooltip = document.getElementById("pop-up-window");
-//   let posY = ctr.offsetLeft;
-//   let posX = ctr.offsetTop;
-//   tooltip.style.left = posY + 100 + "px";
-//   tooltip.style.top = posX + 10 + "px";
-//   tooltip.style.display = "block";
-// }
-// function hide() {
-//   document.getElementById("pop-up-window").style.display = "none";
-// }
-// function show() {
-//   document.getElementById("pop-up-window").style.display = "block";
-// }
-// tooltip.addEventListener('mousemove', show);
-const container = document.getElementById('pop-up-window');
-
 function showPopup() {
-  if (event.clientY > 200) {
+  if (event.clientY > 10) {
     document.getElementById("pop-up-window").style.display = "block";
   }
 }
