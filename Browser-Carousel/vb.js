c04cda489da2d4ccc69966b91b62cd692a81e13e
wagowsky <vagobala@gmail.com>
2020-03-12 11:45:33 +0100

    carousel

---
+++ Browser-Carousel/vb.js
@@ -0,0 +1,22 @@
+let prev = document.querySelector("#prev");
+let next = document.querySelector("#next");
+let img = document.querySelector("#images");
+
+
+
+const previous = () => {
+    if (img.style.backgroundImage = 'url(1.jpg)') {
+        img.style.backgroundImage = 'url(2.jpg)';
+    }
+    else if (img.style.backgroundImage = 'url(/2.jpg)') {
+        img.style.backgroundImage = 'url(/3.jpg)';
+    }
+    // if (img.style.backgroundImage = 'url(/4.jpg)') {
+    //     img.style.backgroundImage = 'url(/5.jpg)';
+    // }
+    // if (img.style.backgroundImage = 'url(/5.jpg)') {
+    //     img.style.backgroundImage = 'url(/6.jpg)';
+    // }
+}
+
+prev.addEventListener("click", previous)
