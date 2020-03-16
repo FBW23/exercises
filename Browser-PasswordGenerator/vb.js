21b644f499e9b363f77af1d20e8a5d12ee0fa9af
wagowsky <vagobala@gmail.com>
2020-03-16 13:22:22 +0100

    pasword still

--- Browser-PasswordGenerator/vb.js
+++ Browser-PasswordGenerator/vb.js
@@ -6,60 +6,49 @@
 
 // - There should be a Generate Password button which the user can click to show a new password using all letters and different characters. 
 
-// - By default, the password generated should use mixed cases (both lowercase and uppercase).
-
-// Create a password generator WebApp with the following instructions:
-
-// 1. Use the constant "charset" in your function (move it there). It contains all characters to use.
-
-// ```javascript
-// const charset = 'abcdefghijklmnopqrstuvwxyz0123456789!§$%&/()=?#,;.:-_';
-// ```
-
-// 2. Your function should have 2 arguments:
-
-//    - length (Number) The length of the password
-//    - mixedCase (Boolean) Whether or not to use mixed cases (lowercase + uppercase)
 
 // 3. If the second argument is true, use uppercase for every third character.
-// 4. Use the function `getRandomInt()` to generate random numbers where you need them.
-// 5. Register an onClick event for the button to generate a new password.
-// 6. Update the generated password inside of the "output" element.
-// 7. Generate the first password as soon as the page is loaded, using a default length of 20.
 
-// Examples for generated passwords: - 876FocMkcCv98teXc(Ov - %lfXcrLt-#ke3r6R5§-? - Nd1Z\$yRx
 
-// Bonus version: 
-// 1. Add an input field to define the length of the password. 
-
-// 2. Add a checkbox to define if the password should use mixed cases. 
-
-// 3. Generate a new password on change of the input elements.
-
-// 4. **Optional:** Free free to use Bootstrap for styling.
-
-// > Hint: Refer to the representation below, for an idea of what your result should look like.
-
-// ![demo](demo.gif)
 const form = document.querySelector("form");
 const tick = document.querySelector("#tick");
 const text = document.querySelector("#text");
-
+const button = document.querySelector("#button");
 const passGen = () => {
+    event.preventDefault();
     const charset = 'abcdefghijklmnopqrstuvwxyz0123456789!§$%&/()=?#,;.:-_';
-    let random = Math.floor(Math.random() * charset.length);
-    let passLength = Number(document.querySelector("#length").value);
+    let passLength = document.querySelector("#length").value;
     let pass = "";
-
     for (let i = 0; i < passLength; i++) {
-        pass += charset.charAt(random);
-        if (!event.target.checked) {
-            pass.charAt(random).toLocaleUpperCase;
-        }
+        let random = Math.floor(Math.random() * charset.length);
+        pass += charset.charAt(random)
     }
-    text.value = pass
+    // if (event.target.checked) {       
+    //     text.style.textTransform = "uppercase";
+    // } else {
+    //     text.style.textTransform = "lowercase";
+    // } 
+
+
+    if (event.target.checked) {
+       // text(i+3).style.textTransform = "uppercase";
+        text.style.textTransform = "uppercase";
+    } else {
+        text.style.textTransform = "lowercase";
+    }
+    text.value = pass;
+
 };
 
-
+// const capitol = () => {   
+//     if (event.target.checked) {
+//         text.style.textTransform = "uppercase";
+//     } else {
+//         text.style.textTransform = "lowercase";
+//     }
+// };
 tick.addEventListener("change", passGen);
-form.addEventListener("submit", passGen);
+// tick.addEventListener("change", capitol);
+button.addEventListener("click", passGen);
+document.addEventListener("DOMContentLoaded", passGen);
+// document.addEventListener("DOMContentLoaded", capitol);
