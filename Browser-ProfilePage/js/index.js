88d06846ab0c20c0d0194354499460bfe22dea3c
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-03-16 15:05:15 +0100

    done

--- Browser-ProfilePage/js/index.js
+++ Browser-ProfilePage/js/index.js
@@ -1,1 +1,23 @@
 // Your JS here!
+
+
+// function resetForm() {
+//     document.querySelector(".form").onclick("click", function(){
+//         document.querySelector(".reset").reset();
+//       });
+//     }
+
+   
+// document.querySelector(".reset").onclick = function() {
+//     document.querySelector(".form").value='',
+//     doccument.querySelector("checkbox").checked = false
+    
+
+//  };
+
+ let resetto = document.querySelector(".reset")
+resetto.addEventListener("click",resetForm)
+
+function resetForm() {
+document.querySelector(".form").reset(); 
+} 
