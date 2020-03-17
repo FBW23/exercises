bc43bef9160f57b0b72ee4250ac1d2a2d588d895
DCI-PC20 <juliarpim@gmail.com>
2020-03-17 12:35:11 +0100

    random colors

--- Browser-RandomColor-Generator/index.js
+++ Browser-RandomColor-Generator/index.js
@@ -1,34 +1,34 @@
 
-const section = document.querySelectorAll('section');
-const color = document.querySelectorAll('#color')
 
-const colorGenerator = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    for(let i=0; i<section.length; i++){
-    section[i].style.backgroundColor = "#" + randomColor;
-    section[i].append( "#" + randomColor);
-}
-  }
-  
-  
-const button = document.querySelector('button')
-button.addEventListener('click', colorGenerator)
+
+
+
+
+
+// mybutton.addEventListener('click', () => {
+//   mydiv.style.backgroundColor=randmColor(); 
+
+//   });
  
+const section=document.querySelectorAll('section'); 
+const button=document.querySelector('button');
+function colorGenerator() {
+    let letters = "0123456789ABCDEF";
+    let randomColor = "#";
+    for (let j = 0; j < 6; j++) {
+        randomColor += letters[Math.floor(Math.random() * 16)];
+    }
+    return randomColor;
+}
 
-// function colorGenerator() {
-//     let letters = "0123456789ABCDEF";
-//     let randomColor = "#";
-//     for (let j = 0; j < 6; j++) {
-//         randomColor += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return randomColor;
-// }
+console.log(section.length);
 
-// console.log(section.length);
 
-// for (let i = 0; i < section.length; i++) {
-//     section[i].style.background = colorGenerator();
-//     section[i].append(colorGenerator());
-
-// }
+button.addEventListener('click' , ()=>{
+    for (let i = 0; i < section.length; i++) {
+        section[i].style.background = colorGenerator();
+        section[i].append(colorGenerator());
+    
+    }
+})
 
