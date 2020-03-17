13f643c211e67c1ee033f4b4a7dab684811428dd
nagster23 <nagsster.s@gmail.com>
2020-03-17 10:16:45 +0100

    done

--- Browser-ProfilePage/js/index.js
+++ Browser-ProfilePage/js/index.js
@@ -16,79 +16,52 @@
 //   };
 
 
-  function myFunction() {
-    document.getElementById("Form").reset();
-  };
-
-console.log(`36_CV`);
-const createObj = document.getElementById(`create`);
-const resetObj = document.getElementById(`reset`);
-createObj.onclick = function(event) {
+// elements
+const nameInput = document.querySelector('#nameInput');
+const birthdayInput = document.querySelector('#birthdayInput');
+const primaryColor = document.querySelector('#primaryColor');
+const secondaryColor = document.querySelector('#secondaryColor');
+const textArea = document.querySelector('textarea');
+const checkboxes = document.querySelectorAll('.skills');
+const form = document.querySelector('form');
+const profileContainer = document.querySelector('.container');
+// Submit
+document.addEventListener('submit', function () {
   event.preventDefault();
-//   createObj.addEventListener("click" event)
-//     event.preventDefault(); 
-  
-  const cvObj = document.getElementById(`cv`);
-  const nameObj = document.getElementById(`name`);
-  const birthdayObj = document.getElementById(`birthday`);
-  const photoObj = document.getElementById(`photo`);
-  const favColor1Obj = document.getElementById(`preferredColor1`);
-  const favColor2Obj = document.getElementById(`preferredColor2`);
-  const descriptionObj = document.getElementById(`description`);
-  const skillsObj = document.getElementsByClassName(`skills`);
-  const newCvObj = document.createElement(`div`);
-  newCvObj.classList.add(`yourCV`);
-  newCvObj.style.background = favColor1Obj.value;
-  newCvObj.style.color = favColor2Obj.value;
-  newCvObj.style.marginBottom = `20px`;
-  newCvObj.style.margin = "10px auto";
-  newCvObj.style.width = "40%";
-  cvObj.append(newCvObj);
-  const removeBtnObj = document.createElement(`button`);
-  removeBtnObj.innerText = "X";
-  removeBtnObj.classList.add(`removeBtn`);
-  newCvObj.appendChild(removeBtnObj);
-  removeBtnObj.onclick = function() {
-    this.parentElement.remove();
-  };
-  const nameH1Obj = document.createElement(`h1`);
-  nameH1Obj.innerText = nameObj.value;
-  nameH1Obj.style.textAlign = "center"
-  newCvObj.appendChild(nameH1Obj);
-  const ageObj = document.createElement(`h2`);
-  let birthdayArr = birthdayObj.value.split(`-`);
-  let yearOfBirth = parseInt(birthdayArr[0]);
-  let today = new Date();
-  let alter = today.getFullYear() - yearOfBirth;
-  ageObj.innerText = `${nameObj.value} is ${alter} years old`;
-  newCvObj.appendChild(ageObj);
-  const newPhotoObj = document.createElement(`div`);
-  newPhotoObj.classList.add(`foto`);
-  newPhotoObj.style.width = "10%"
-  newPhotoObj.style.height = "20%"
-  newPhotoObj.style.backgroundImage = `url(${photoObj.value})`;
-  newCvObj.appendChild(newPhotoObj);
-  const newDescriptionObj = document.createElement(`h2`);
-  newDescriptionObj.innerText = descriptionObj.value;
-  newCvObj.appendChild(newDescriptionObj);
-  const newSkillsObj = document.createElement(`h4`);
-  let skillsStr = ``;
-  for (let i = 0; i < skillsObj.length; i++) {
-    if (skillsObj[i].checked === true) {
-      skillsStr += skillsObj[i].value + `, `;
+  // Colors 
+  const userPrimaryColor = primaryColor.value;
+  const userSecondaryColor = secondaryColor.value;
+  profileContainer.style.backgroundColor = userPrimaryColor;
+  profileContainer.style.color = userSecondaryColor;
+  // Adding the value of the nameInput into the h4 element
+  const userName = document.querySelector('#userName');
+  const name = nameInput.value;
+  userName.innerHTML = name;
+  // Adding the value of birthdayInput to the p element
+  const userAge = document.querySelector('#userAge');
+  const age = birthdayInput.value
+  userAge.innerHTML = ` ${name} is born at ${age}`;
+  // Adding the image 
+  const userImage = document.getElementById('avatar').files[0];
+  const image = document.querySelector('#image');
+  const reader = new FileReader();
+  reader.onload = (function (aImg) {
+    return function (e) {
+      aImg.src = e.target.result;
+    };
+  })(image);
+  reader.readAsDataURL(userImage);
+  // Add paragraph about yourself 
+  const aboutMe = document.querySelector('#aboutMe')
+  const userText = textArea.value;
+  aboutMe.innerHTML = userText;
+  // Adding the Skills
+  const userSkills = document.querySelector('#userSkills');
+  const skillsArr = [];
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked === true) {
+      skillsArr.push(checkboxes[i].value);
     }
+    userSkills.innerHTML = `The programing skills of ${name} are ${[...skillsArr]}`
   }
-  skillsStr = skillsStr.slice(0, skillsStr.length - 2);
-  if (skillsStr.split(`, `)[0] === "") {
-    alert(`You should not apply as a web developer!`);
-  } else if (skillsStr.split(`, `).length === 1) {
-    newSkillsObj.innerText = `The programming skill of ${nameObj.value} is: ${skillsStr}.`;
-    newCvObj.appendChild(newSkillsObj);
-  } else {
-    newSkillsObj.innerText = `The programming skills of ${nameObj.value} are: ${skillsStr}.`;
-    newCvObj.appendChild(newSkillsObj);
-  }
-};
-resetObj.onclick = function() {
-  location.reload();
-};
+});
