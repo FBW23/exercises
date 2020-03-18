3ff585b725659883c8e6440a22c01905d0eca14e
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-18 14:17:14 +0100

    cmmt

--- Browser-SoundOfSilence/assets/index.js
+++ Browser-SoundOfSilence/assets/index.js
@@ -8,10 +8,12 @@ for (let i = 0; i < 5; i++) {
     // console.log('music array object ', music[i]); // we get the five array objects - KEWL
     // console.log('line ', line[i].childNodes);
     // track[i]=line[i].childNodes[2];
-    let currentTrack = line[i].children[1]; // KEWL
+    let currentTrack = line[i].children[1];
+    currentTrack.innerText = music[i]['trackName'];
+    // KEWL
     //                 current line ,  div number 2   
-    console.log('current track', currentTrack)
-        // console.log('track[i] ', track[i])
+    // console.log('current track', currentTrack)
+    // console.log('track[i] ', track[i])
     // console.log('artist name', music[i]['artistName']); // TOP!
     // console.log('artist name', music[i]['collectionName']);
     // let's add the key values to the html
