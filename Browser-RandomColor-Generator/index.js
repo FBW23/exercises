c1a0d2c353a963e06b362656057357d630aff8db
Bianca Richa <biancaricha@gmail.com>
2020-03-18 10:26:57 +0100

    debug needed

--- Browser-RandomColor-Generator/index.js
+++ Browser-RandomColor-Generator/index.js
@@ -1,20 +1,32 @@
 let section = document.querySelectorAll(".box");
-
+let a = document.querySelectorAll("a");
+let button = document.querySelector("button");
 let main = document.querySelector(".main");
+
 main.style.display = "flex";
 main.style.justifyContent = "space-around";
 main.style.flexWrap = "wrap";
 
-
 function colorGenerator() {
-    return '#'+Math.floor(Math.random()*16777215).toString(16);;
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-for (let i = 0; i < section.length; i++) {
-    section[i].style.background = colorGenerator();
-    section[i].style.width = "30%";
-    section[i].style.height = "200px";
-    section[i].style.margin = "20px";
-    section[i].innerText = colorGenerator();
-    section[i].style.textAlign = "center";
+function createSquares() {
+    let inputValue = document.querySelector("#color").value;
+    for (let i = 0; i < i < parseInt(inputValue); i++) {
+        let newSection = document.createElement("section");
+        let backColor = colorGenerator(); 
+        newSection.classList.add("box");
+        newSection.style.background = backColor;
+        newSection.style.width = "30%";
+        newSection.style.height = "200px";
+        newSection.style.margin = "20px";
+        newSection.style.textAlign = "center";
+        newSection.innerText=backColor;
+
+    }
 }
+
+createSquares();
+
+button.addEventListener("click", createSquares);
