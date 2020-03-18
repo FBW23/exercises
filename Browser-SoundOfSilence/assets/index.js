a2e1176bf31f565f6c5c3bdb33ed1054daafc4ee
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-18 14:30:54 +0100

    cmmt

--- Browser-SoundOfSilence/assets/index.js
+++ Browser-SoundOfSilence/assets/index.js
@@ -1,21 +1,24 @@
 // HTML elements saved in Lets
 let line = document.querySelectorAll('.line');
 let track = document.querySelector('.track');
-let artist = document.querySelector('#artist');
-let date = document.querySelector('#date');
+let artist = document.querySelector('.artist');
+let date = document.querySelector('.date');
 // Looping the HTML elements modifications
 for (let i = 0; i < 5; i++) {
     // console.log('music array object ', music[i]); // we get the five array objects - KEWL
-    // console.log('line ', line[i].childNodes);
-    // track[i]=line[i].childNodes[2];
+   
     let currentTrack = line[i].children[1];
     currentTrack.innerText = music[i]['trackName'];
-    // KEWL
-    //                 current line ,  div number 2   
-    // console.log('current track', currentTrack)
-    // console.log('track[i] ', track[i])
-    // console.log('artist name', music[i]['artistName']); // TOP!
-    // console.log('artist name', music[i]['collectionName']);
+    let currentArtist = line[i].children[2];
+    currentArtist.innerText = music[i]['artistName'];
+    let currentCollection = line[i].children[3];
+    currentCollection.innerText = music[i]['collectionName'];
+    // KEW
+    let releaseDate = line[i].children[4];
+    let releaseYear = music[i]['releaseDate'];
+    releaseDate.innerText = releaseYear.substr(0,4);
+   
     // let's add the key values to the html
+    
 
 }
