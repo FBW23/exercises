b635ad40b050ebbbf81c51056b7650a605128652
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-18 14:07:10 +0100

    push

--- Browser-SoundOfSilence/assets/index.js
+++ Browser-SoundOfSilence/assets/index.js
@@ -1,21 +1,19 @@
 // HTML elements saved in Lets
-
 let line = document.querySelectorAll('.line');
-
-let track = document.querySelector('#track');
-
+let track = document.querySelector('.track');
 let artist = document.querySelector('#artist');
-
 let date = document.querySelector('#date');
-
 // Looping the HTML elements modifications
-
 for (let i = 0; i < 5; i++) {
     // console.log('music array object ', music[i]); // we get the five array objects - KEWL
     // console.log('line ', line[i].childNodes);
-    track[i]=line[i].childNodes[i]
-    console.log('track[i] ', track[i])
+    // track[i]=line[i].childNodes[2];
+    let currentTrack = line[i].children[1]; // KEWL
+    //                 current line ,  div number 2   
+    console.log('current track', currentTrack)
+        // console.log('track[i] ', track[i])
     // console.log('artist name', music[i]['artistName']); // TOP!
     // console.log('artist name', music[i]['collectionName']);
     // let's add the key values to the html
+
 }
