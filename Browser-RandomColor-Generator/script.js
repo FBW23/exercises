ae0f4ab9b45dbe4462b281db16f4def394a0cfc6
aghy <zenlogie@gmail.com>
2020-03-18 13:47:33 +0100

    second edition

--- Browser-RandomColor-Generator/script.js
+++ Browser-RandomColor-Generator/script.js
@@ -1,12 +1,11 @@
 const btn = document.querySelector("#btn");
 const num = document.querySelector("#numFields");
-const form = document.querySelector("form");
 const colorPallette = document.querySelector("#colorPallette");
 
 btn.addEventListener("click", createUnit);
 
 function rndClr() {
-    const signs = '0123456789ABCDEF';
+    const signs = "0123456789ABCDEF";
     let clr = "#";
     for (let i = 0; i < 6; i++) {
         clr += signs[Math.floor(Math.random() * 16)];
@@ -16,16 +15,21 @@ function rndClr() {
 
 function createUnit() {
     event.preventDefault();
-    let numFields = num.value;
+    const numFields = num.value;
     for (let index = 0; index < numFields; index++) {
-        let unit = document.createElement("div");
-        unit.addEventListener("dblclick", () => {
+        const closeBtn = document.createElement("span");
+        const colorCode = document.createElement("span");
+        closeBtn.classList.add("delCol");
+        const unit = document.createElement("div");
+        closeBtn.addEventListener("click", () => {
             colorPallette.removeChild(unit);
         });
-        let color = rndClr();
+        const color = rndClr();
         unit.classList.add("pallette");
-        unit.style.backgroundColor = color
-        unit.innerText = color;
+        unit.style.backgroundColor = color;
+        colorCode.innerText = color;
+        unit.appendChild(closeBtn);
+        unit.appendChild(colorCode);
         colorPallette.appendChild(unit);
     }
 }
