2cce9fadc3db9e5fc188db80d329edd53c188f55
Julien Montagnon <jumontagnon@gmail.com>
2020-03-19 09:30:59 +0100

    bla

--- Browser-RandomColor-Generator/color.js
+++ Browser-RandomColor-Generator/color.js
@@ -1,7 +1,5 @@
 // 1. Create a function to generate a random color HEX code
-
 const hexLetter = 'abcdef0123456789';
-
 const randomColor = () => {
     let hex = '#' + '';
     for (let i = 0; i < 6; i++) {
@@ -11,51 +9,30 @@ const randomColor = () => {
     }
     return hex;
 };
-
-
-
 // const colorDiv = document.querySelector('.card');
 // colorDiv.style.backgroundColor = randomColor();
-
 // 2. Create a function to add a new color item to the list, passing the color code as argument
-
+// saving the form
 const form = document.querySelector('.form-inline');
-
+// saving the number input
 const numberInput = document.querySelector('#numberInput');
 const numberDiv = numberInput.value;
+// saving buttons
 const button = document.querySelector('#button');
-const body = document.querySelector('#body');
 const eachDiv = '';
 const newDivCreator = () => {
-    let newDiv = document.createElement('div');
-    newDiv.className = 'card';
-    document.querySelector('.div-container').appendChild(newDiv);
-    newDiv.style.width = '18rem';
-    newDiv.style.height = '18rem'
-    newDiv.style.backgroundColor = randomColor();
-
-    for (let i = 0; i < 3; i++) {
-        let list = document.createElement('li');
-        list.className = 'div-container';
-        document.querySelector('.div-section').appendChild(list);
+    const numberDiv = numberInput.value;
+    for (i = 0; i < numberDiv; i++) {
+        let divContainer = document.createElement('div');
+        divContainer.className = 'div-container';
+        document.querySelector('.section').appendChild(divContainer);
+        let newDiv = document.createElement('div');
+        newDiv.className = 'card';
+        document.querySelector('.div-container').appendChild(newDiv);
+        newDiv.style.width = '21rem';
+        newDiv.style.height = '7rem'
+        newDiv.style.backgroundColor = randomColor();
     }
 };
-
-// const newDivCreator = () => {
-//     for (i = 0; i < 3; i++) {
-//         let list = document.createElement('li');
-//         list.className = 'div-container';
-//         document.querySelector('.div-section').appendChild(list);
-//       for
-//         let newDiv = document.createElement('div');
-//         newDiv.className = 'card';
-//         document.querySelector('.div-container').appendChild(newDiv);
-//         newDiv.style.width = '18rem';
-//         newDiv.style.height = '18rem'
-//         newDiv.style.backgroundColor = randomColor();
-
-//     }
-// };
-
-button.addEventListener('click', newDivCreator());
-
+// numberInput.addEventListener('keydown', newDivCreator);
+button.addEventListener('mousedown', newDivCreator);
