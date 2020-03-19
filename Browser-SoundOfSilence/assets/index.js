d5f5ff1b6165faca6af9b33560e1e0cf80453258
o0pmitev <pmitev89@gmail.com>
2020-03-19 09:02:45 +0100

    FINISHED

--- Browser-SoundOfSilence/assets/index.js
+++ Browser-SoundOfSilence/assets/index.js
@@ -1,68 +1,18 @@
-// HTML elements saved in Lets
-let line = document.querySelectorAll('.line');
-let track = document.querySelector('.track');
-let artist = document.querySelector('.artist');
-let date = document.querySelector('.date');
+let tbody = document.querySelector('tbody')
 
 
-const creteTable = () => {
-    let table = document.querySelector('.table');
-    let tbody = document.createElement('tbody');
-    let tr = document.createElement('tr');
-    let th1 = document.createElement('th');
-    let th2 = document.createElement('th');
-    let th3 = document.createElement('th');
-    let th4 = document.createElement('th');
-    let th5 = document.createElement('th');
-    let img = document.createElement('img');
 
-    tr.classList.add('line');
-
-    img.src = 'assets/img/music.jpg'
-    th1.setAttribute('scope', 'col')
-    th1.classList.add('pic');
-
-    th2.setAttribute('scope', 'col')
-    th2.classList.add('track');
-
-    th3.setAttribute('scope', 'col')
-    th3.classList.add('artist');
-
-    th4.setAttribute('scope', 'col')
-    th4.classList.add('type');
-
-    th5.setAttribute('scope', 'col')
-    th5.classList.add('date');
-
-    tbody.appendChild(tr);
-    tr.appendChild(th1);
-    th1.appendChild(img);
-    tr.appendChild(th2);
-    tr.appendChild(th3);
-    tr.appendChild(th4);
-    tr.appendChild(th5);
-    table.appendChild(tbody);
-}
-
-// Looping the HTML elements modifications
-for (let i = 0; i < music.length; i++) {
-    // console.log('music array object ', music[i]); // we get the five array objects - KEWL
-    // create the html
-    
-    // create the html
-    let currentTrack = line[i].children[1];
-    currentTrack.innerText = music[i]['trackName'];
-    let currentArtist = line[i].children[2];
-    currentArtist.innerText = music[i]['artistName'];
-    let currentCollection = line[i].children[3];
-    currentCollection.innerText = music[i]['collectionName'];
-    // KEWL
-    let releaseDate = line[i].children[4];
-    let releaseYear = music[i]['releaseDate'];
-    releaseDate.innerText = releaseYear.substr(0,4);
-    
-    // let's add the key values to the html
-    
-    creteTable()
-}
-
+let table2 = ''
+music.forEach(element => {
+console.log(element);
+table2 +=  `
+<tr class='line'>
+<th scope="col" class="pic"><img src="assets/img/music.jpg"></th>
+<th scope="col" class="track">${element.trackName}</th>
+<th scope="col" class="artist">${element.artistName}</th>
+<th scope="col" class="type">${element.collectionName}</th>
+<th scope="col" class="date">${element.releaseDate}</th>
+</tr>
+`;
+tbody.innerHTML = table2;
+})
