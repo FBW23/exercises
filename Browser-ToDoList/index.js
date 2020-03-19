47e4481e239006f6636a08e3602647fe94a302d4
Nicolo <54029524+grantortino@users.noreply.github.com>
2020-03-19 14:45:33 +0100

    commit

--- Browser-ToDoList/index.js
+++ Browser-ToDoList/index.js
@@ -1,29 +1,72 @@
+// loadEvents();
+// // load every event in the page
+// function loadEvents(){
+//   document.querySelector('button').addEventListener('click', submit);
+//   document.querySelector('checkbox').addEventListener('click', checked);
+//   // document.getElementById('clear').addEventListener('click',clearList);
+//   // // recently added
+//   // document.querySelector('ul').addEventListener('click',deleteOrTick);
+
+// }
+
 const button = document.querySelector('button');
-
 button.addEventListener('click', submit);
-
 
 function submit(e) {
   e.preventDefault();
   let textArea = document.querySelector('textarea');
-  if(textArea.value != '') {
+  if (textArea.value != '') {
     addTask(textArea.value);
   }
   textArea.value = '';
 }
 
 // add task function
-function addTask(task){
+function addTask(task) {
   let ul = document.querySelector('ul');
   let li = document.createElement('li');
-  li.innerHTML = `<span class="delete"><i class="fas fa-times"></i></span><input type="checkbox"><label>${task}</label>`;
+  li.innerHTML = `<span class="delete"><i class="fas fa-times"></i></span><input id='checkbox' type="checkbox"><label>${task}</label>`;
   ul.appendChild(li);
   document.querySelector('.container-fluid').classList.remove('d-none');
+  // checkbox.addEventListener('click', checked);
+  let checkboxTick = document.querySelector('#checkbox');
+  let label = document.querySelector('label');
+  checkboxTick.addEventListener('change', function () {
+    if (checkboxTick.checked === true) {
+      label.style.textDecoration = "line-through";
+    } else {
+      label.style.textDecoration = "none";
+    }
+  })
+  let icon = document.querySelectorAll('.fas');
+  icon.forEach(element => {
+    element.addEventListener('click', function (e) {
+      console.log(e.target.parentNode.parentNode.style.display = "none");
+    })
+  })
+  // icon.addEventListener('click', function (e) {
+  //   console.log(e.target.parentNode.parentNode.style.display = "none");
+  // })
 }
 
 
+// checked
+// how to add you on zoom :D 
+// I send you a link
+// maybe your on mute?
 
 
+// function checked() {
+//   const taskLabel = document.querySelector('label');
+//   const checkbox = document.getElementById('checkbox');
+//   if (checkbox.checked === true) {
+//     taskLabel.style.textDecoration = "line-through";
+//     taskLabel.style.color = "grey";
+//   } else {
+//     taskLabel.style.textDecoration = "none";
+//   }
+// }
+// checked()
 
 // const textArea = document.querySelector('textarea');
 // const toDoContainer = document.getElementById('to-do-container');
