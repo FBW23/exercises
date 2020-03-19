9493d4e8353fc4c8f86f4201f175d116da116393
o0pmitev <pmitev89@gmail.com>
2020-03-19 09:57:57 +0100

    dassda

--- Browser-SoundOfSilence/assets/index.js
+++ Browser-SoundOfSilence/assets/index.js
@@ -4,13 +4,13 @@ let table = '';
 music.forEach(element => {
     console.log(element);
     table += `
-<tr class='line'>
-<td scope="col" class="pic"><img src="assets/img/music.jpg"></td>
-<td scope="col" class="track">${element.trackName}</td>
-<td scope="col" class="artist">${element.artistName}</td>
-<td scope="col" class="type">${element.collectionName}</td>
-<td scope="col" class="date">${element.releaseDate}</td>
-</tr>
-`;
+        <tr class='line'>
+        <td scope="col" class="pic"><img src="assets/img/music.jpg"></td>
+        <td scope="col" class="track">${element.trackName}</td>
+        <td scope="col" class="artist">${element.artistName}</td>
+        <td scope="col" class="type">${element.collectionName}</td>
+        <td scope="col" class="date">${element.releaseDate}</td>
+        </tr>
+    `;
     tbody.innerHTML = table;
 })
