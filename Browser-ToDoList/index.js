91cb4efc9a15ea3315b08666816c7e197cbf34be
DCI-PC20 <juliarpim@gmail.com>
2020-03-23 10:48:59 +0100

    changes

--- Browser-ToDoList/index.js
+++ Browser-ToDoList/index.js
@@ -2,46 +2,40 @@
 
 
 const button = document.querySelector('button');
-const closeB = document.getElementsByClassName('hide');
+const closeB = document.getElementsByClassName('done');
 const toDoList = document.querySelector('.todolist');
-const crossTask = document.getElementsByClassName('cross');  
+const crossTask = document.getElementsByClassName('cross');
 const inputValue = document.querySelector('#addList');
 
 const addTask = () => {
-  
+
     //create the li
     const newLi = document.createElement('li');
     //adding the value of the input inside the new li
     const liContent = document.createTextNode(inputValue.value);
-   
+
     const liClose = document.createElement('a');
-    liClose.href='';
-    liClose.classList.add('hide');
-    //liContent.classList.add('cross');
+    liClose.href = '';
+
+
     const liCloseContent = document.createTextNode('x');
 
     toDoList.appendChild(newLi);
     newLi.appendChild(liContent);
     newLi.appendChild(liClose);
     liClose.appendChild(liCloseContent);
-
-    for(let i=0; i<closeB.length; i++){
-       
-        function closeTask (){
-            let parent= event.target.parentNode;
-            parent.style.display = 'none';
-        }
-            closeB[i].addEventListener('click', closeTask)
-        
+    // because it's in the function, it only adds it for new items
+    function crossOut() {
+        newLi.classList.toggle("done");
     }
 
-//     for(let j=0; j<liContent.length;j++){
-//         function crossTask (){
-//         let boxCross = event.target.parentNode;
-//         boxCross.style.textDecoration = 'line-through'
-//     }
-//      liContent[j].addEventListener('click', crossTask)
-// }
+    newLi.addEventListener("click", crossOut);
+    //END STRIKETHROUGH
+    function deleteListItem(){
+		newLi.classList.add("delete")
+    }
+    closeB.addEventListener('click',deleteListItem)
+
 }
 
 
