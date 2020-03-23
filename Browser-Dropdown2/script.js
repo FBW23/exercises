6ccc76fb8e6a122fa6bc7c81e8845addd52cbeb1
o0pmitev <pmitev89@gmail.com>
2020-03-23 16:39:17 +0100

    FINISHED

--- Browser-Dropdown2/script.js
+++ Browser-Dropdown2/script.js
@@ -2,15 +2,19 @@ const titles = document.querySelectorAll('p');
 const lists = document.querySelectorAll('.dropdown-menu');
 const showBlock = function () {
     // HIDE ALL 
+
     for(let i=0; i<lists.length; i++) {
         lists[i].style.display = 'none';
     }
-    if (event.target.nextElementSibling.style.display === 'flex') {
-        event.target.nextElementSibling.style.display = 'none';
-    } else {
+
+    if (event.target.nextElementSibling.style.display === 'none') {
         event.target.nextElementSibling.style.display = 'flex';
+    } else {
+        event.target.nextElementSibling.style.display = 'none';
     }
+
 }
+
 titles.forEach(title => {
-    title.addEventListener('click', showBlock);
+    title.addEventListener('mouseover', showBlock);
 })
