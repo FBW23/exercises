ed2d6b8e249709437d55a3c20f6acf00a2600d8e
o0pmitev <pmitev89@gmail.com>
2020-03-24 15:21:02 +0100

    FINISHED

--- Browser-library-jquery/index.js
+++ Browser-library-jquery/index.js
@@ -1,18 +1,80 @@
 // Work on your tasks here
-
 // get html elements 
 const submit = $('.submit');
-console.log(submit);
+const modal = $('.modal');
+const html = $('html');
+const teapot = $('.image');
+const teaDescriptions = $('.tea-desc');
+console.log(teaDescriptions);
+
+
+// Random Phrase
+let randomQuote = () => {
+    // Array of Phrases
+    let phrases = ['Oh a cup of tea is always my cup of tea!', 'Rich in flavours', 'Great tea for a cold night', 'Tooling for a warm tea bag!'];
+
+
+    // Random number 
+    let randomNumber = Math.floor((Math.random() * phrases.length));
+    let target = $(event.target);
+    target.text(phrases[randomNumber]);
+}
+
+
+// close modal
+
+const closeCross = $('.modal-close');
+console.log('close cross ', closeCross);
 
 // Show modal function 
 const showModal = () => {
     event.preventDefault();
-    const modal = $('.modal-content');
-    console.log('click')
-
-// looks like its working
+    modal.show();
+    console.log('click');
 }
 
-// event listener
-submit.click('on', showModal);
+// Hide modal funciton
+const hideModal = () => {
+    event.preventDefault();
+    modal.hide();
+}
 
+
+
+// Rotate the teapot back
+
+const rotateBack = () => {
+
+    teapot.toggleClass('teapot-rotate');
+}
+
+submit.on('click', showModal);
+closeCross.on('click', hideModal);
+teapot.on('click', rotateBack);
+
+teaDescriptions.hover(randomQuote);
+
+
+//        ~=Chat=~
+// *-*-*-* NICOLO *-*-*-*
+/** check
+ */
+
+//======== PLAMEN ========
+
+/*
+okey
+/* 
+==== TASK ====  
+TOOLS :
+
+.hover()
+.text()
+randomQuote() 
+<-
+--------------------
+* If the user hovers over the `.tea-desc` nested in the `selection__option`s, 
+the text should change to a random phrase, e.g. "Great tea for a cold night", 
+"Rich in flavours" etc.
+![alt text](./images/tea-descriptions.png "Modal")
+*/
