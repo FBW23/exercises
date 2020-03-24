aa242fd18b68ec9af1f22bdee1af63b99398da9c
nagster23 <nagsster.s@gmail.com>
2020-03-24 14:27:57 +0100

    Library finished

--- Browser-library-jquery/index.js
+++ Browser-library-jquery/index.js
@@ -1,1 +1,49 @@
 // Work on your tasks here
+
+// let teapot = -10;
+
+
+// $('.image').click(function(){
+//     teapot = teapot + 30;
+//     $(this).css('-webkit-transform','rotate('+teapot+'deg)');
+// });
+
+$(document).ready(function(){
+    $('.submit').on("click",function(){
+      $('.modal').css("display","block")
+    });
+    $('.modal-close').on("click",function(){
+      $('.modal').css("display","none")
+    });
+  });
+  
+
+  // teapot
+  $(document).ready(function() {
+    $(".teapot").on("click", function() {
+      $(".teapot").css("-webkit-transform", "rotate(20deg)");
+    });
+    $(".teapot").on("dblclick", function() {
+      $(".teapot").css("-webkit-transform", "rotate(0)");
+    });
+  });
+  
+  // changing text on hover
+  
+  $(document).ready(function() {
+    $(".tea-desc").on("mouseenter ", function() {
+      let text = "";
+      let phrases = new Array(
+        "Great tea for a cold night",
+        "Rich in flavours",
+        "Best tea from Brasil",
+        "Strong black tea"
+      );
+      let randomPhrase = Math.floor(Math.random() * phrases.length);
+      text += phrases[randomPhrase];
+  
+      $(event.target).text(text);
+    });
+  });
+  
+  
