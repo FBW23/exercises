48535102cf3d236712f03e99755b3aa4bfbb9cca
o0pmitev <pmitev89@gmail.com>
2020-03-24 11:30:36 +0100

    first commit

--- Browser-library-jquery/index.js
+++ Browser-library-jquery/index.js
@@ -1,1 +1,18 @@
 // Work on your tasks here
+
+// get html elements 
+const submit = $('.submit');
+console.log(submit);
+
+// Show modal function 
+const showModal = () => {
+    event.preventDefault();
+    const modal = $('.modal-content');
+    console.log('click')
+
+// looks like its working
+}
+
+// event listener
+submit.click('on', showModal);
+
