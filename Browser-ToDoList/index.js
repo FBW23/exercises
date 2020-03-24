ebd11edc1104afc3a83c8d9184ddaf882eb28808
DCI-PC20 <juliarpim@gmail.com>
2020-03-24 10:36:56 +0100

    changes

--- Browser-ToDoList/index.js
+++ Browser-ToDoList/index.js
@@ -2,39 +2,46 @@
 
 
 const button = document.querySelector('button');
-const closeB = document.getElementsByClassName('done');
+//const closeB = document.getElementsByClassName('done');
 const toDoList = document.querySelector('.todolist');
-const crossTask = document.getElementsByClassName('cross');
+//const crossTask = document.getElementsByClassName('cross');
 const inputValue = document.querySelector('#addList');
 
 const addTask = () => {
+  debugger
 
-    //create the li
-    const newLi = document.createElement('li');
-    //adding the value of the input inside the new li
-    const liContent = document.createTextNode(inputValue.value);
+  //create the li
+  const newLi = document.createElement('li');
+  //adding the value of the input inside the new li
+  const liContent = document.createTextNode(inputValue.value);
 
-    const liClose = document.createElement('a');
-    liClose.href = '';
+  const liClose = document.createElement('a');
+  liClose.href = '';
+
+  const liCloseContent = document.createTextNode('x');
 
 
-    const liCloseContent = document.createTextNode('x');
+  function deleteListItem() {
+    debugger
+    event.preventDefault();
+    event.target.remove();
+  }
 
-    toDoList.appendChild(newLi);
-    newLi.appendChild(liContent);
-    newLi.appendChild(liClose);
-    liClose.appendChild(liCloseContent);
-    // because it's in the function, it only adds it for new items
-    function crossOut() {
-        newLi.classList.toggle("done");
-    }
+  liCloseContent.addEventListener('click', deleteListItem, true);
+  
+  toDoList.appendChild(newLi);
+  newLi.appendChild(liContent);
+  newLi.appendChild(liClose);
+  liClose.appendChild(liCloseContent);
 
-    newLi.addEventListener("click", crossOut);
-    //END STRIKETHROUGH
-    function deleteListItem(){
-		newLi.classList.add("delete")
-    }
-    closeB.addEventListener('click',deleteListItem)
+  // function crossOut() {
+  //   debugger
+  //   newLi.classList.toggle("done");
+  // }
+
+  
+  //newLi.addEventListener("click", crossOut);
+
 
 }
 
