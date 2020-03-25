8d5d34016435e2d4fbec308cf154b033d5c88685
wagowsky <vagobala@gmail.com>
2020-03-25 13:35:46 +0100

    fsdfdsfsd

---
+++ Browser-library-handlebars/band.js
@@ -0,0 +1,15 @@
+const balasz = {
+    scooters: [{
+            track: "Hyper, Hyper",
+            year: "1994",
+        },
+        {
+            track: "Back in the UK",
+            year: "1995",
+        },
+        {
+            track: "Call me Manana",
+            year: "1998",
+        }
+    ]
+};
