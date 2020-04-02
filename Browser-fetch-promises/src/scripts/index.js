8406aed983e3bfaad6743a40caa561d9ef554df3
o0pmitev <pmitev89@gmail.com>
2020-04-02 10:56:30 +0200

    FINISHED

--- Browser-fetch-promises/src/scripts/index.js
+++ Browser-fetch-promises/src/scripts/index.js
@@ -5,3 +5,20 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+const modal = document.querySelector('.modal');
+const x = document.querySelector('.close');
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        modal.style.display ='block';
+    },60000);
+});
+
+promise
+.then(() => console.log('success'))
+.catch(()=> console.log('error'))
+
+// close the modal
+x.addEventListener('click', function() {
+    modal.style.display = 'none'
+});
