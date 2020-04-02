c6aa892a6d89cfc98e4a79bba82421f6f3af6852
Joao de Faria Junior <joaodefariajunior1@gmail.com>
2020-04-02 11:22:06 +0200

    done

--- Browser-ComingSoon/src/scripts/index.js
+++ Browser-ComingSoon/src/scripts/index.js
@@ -5,3 +5,32 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+let countDownDate = new Date("Jun 20, 2020 00:01:25").getTime();
+
+
+let x = setInterval(function () {
+
+
+    let now = new Date().getTime();
+
+
+    let distance = countDownDate - now;
+
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("counter").innerHTML = '</p>' + days + " Days" + '</p>' + '</p>' + hours + " Hours" + '</p>' +
+        '<p>' + minutes + " Minutes " + '</p>' + '<p>' + seconds + " Seconds " + '</p>';
+
+
+}, 1000);
+
+function randomColor() {
+    return '#' + ('00000' + (Math.random() * 16777216 << 0).toString(16)).substr(-6);
+}
+let h1 = document.getElementById('summer');
+h1.style.color = randomColor;
--- Browser-fetch-promises/src/scripts/index.js
+++ Browser-fetch-promises/src/scripts/index.js
@@ -5,3 +5,15 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+
+      timeoutID = new Promise(function(resolve) {
+        
+        window.setTimeout(resolve, 10000);
+    }).then(function () { setTimeout(function(){
+        $.colorbox({
+            html:"<h2>You've been on the site for 10 seconds! Cool!</h2>",
+            className: "cta",
+          
+          }); 
+    });
+})
