14574d640086a266485e61dde7d270e078457ff2
hbendis <bendishoxha1@gmail.com>
2020-04-02 13:58:17 +0200

    done

--- Browser-fetch-promises/src/scripts/index.js
+++ Browser-fetch-promises/src/scripts/index.js
@@ -3,5 +3,21 @@ import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
 
+
 // \/ All of your javascript should go here \/
 
+let closeBTN = document.querySelector('.close')
+let message = document.querySelector('.modal');
+const promise1 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        message.style.display = 'block';
+        resolve();
+    }, 60000);
+});
+
+function closeDIV (){
+    document.querySelector('.modal').style.display='none'
+
+}
+
+closeBTN.addEventListener('click', closeDIV);
