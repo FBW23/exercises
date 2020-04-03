45b542d439f4530cdcc0074f8de351083dee9ae4
wagowsky <vagobala@gmail.com>
2020-04-03 12:39:29 +0200

    cleaned it up a bit

--- Browser-fetch-JSON/src/scripts/index.js
+++ Browser-fetch-JSON/src/scripts/index.js
@@ -10,14 +10,10 @@ import {
 
 
 const recipes = JSON.parse(recipesOBJ);
-// const cakes = document.querySelector("#cakes");
 const sections = document.querySelectorAll(".container section");
-
-
-
-
 const categoryNames = Object.keys(recipes);
 const categoryValues = Object.values(recipes);
+
 let currentCategoryName = ``;
 let currentCategoryValue = ``;
 let currentItem = ``;
@@ -26,11 +22,11 @@ let currentAuthor = ``;
 let currentImg = ``;
 let currentIngredients = ``;
 let template = ``;
-let currentType=``;
+let currentType = ``;
 
 for (let i = 0; i < categoryNames.length; i++) {
-    let currentCategoryName = categoryNames[i];
-    let currentCategoryValue = categoryValues[i];
+    currentCategoryName = categoryNames[i];
+    currentCategoryValue = categoryValues[i];
 
     for (let j = 0; j < currentCategoryValue.length; j++) {
         currentItem = currentCategoryValue[j];
@@ -39,10 +35,10 @@ for (let i = 0; i < categoryNames.length; i++) {
         currentAuthor = currentItem.author;
         currentImg = currentItem.image;
         currentIngredients = currentItem.ingredients;
-        currentType=currentItem.type;
+        currentType = currentItem.type;
 
         template =
-        `
+            `
                 <div class="card" style="width: 18rem;">
                 <img class="card-img-top" src="${currentImg}" alt="Card image cap">
                     <div class="card-body">
@@ -56,49 +52,8 @@ for (let i = 0; i < categoryNames.length; i++) {
                 `
 
 
-    sections[i].innerHTML += template;
+        sections[i].innerHTML += template;
 
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-// const creator = () => {
-//     for (let i = 0; i < sections.length; i++) {
-
-// for (let j = 0; j < recipes.cakes.length; j++) {
-//             let cakeName = recipes.cakes[j].title;
-//             let type =  recipes.cakes[j].type;
-//             let author = recipes.cakes[j].author;
-//             let ingredients = recipes.cakes[j].ingredients;
-//             let image = recipes.cakes[j].image;
-
-//           let template =
-//                 `
-//             <div class="card" style="width: 18rem;">
-//             <img class="card-img-top" src="${image}" alt="Card image cap">
-//                 <div class="card-body">
-//             <h5 class="card-title">${cakeName}</h5>
-//             <p class="card-text">${type}</p>
-//             <p class="card-text">${author}</p>
-//             <hr>
-//             <p class="card-text">Ingredients: ${ingredients}</p>
-//             </div>
-//             </div>
-//             `
-
-//             sections[i].innerHTML += template;
-
-//         }
-//     }
-// }
-
-// creator()
