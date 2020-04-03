9b5074960c786d65f11344a91fb9535ecf939150
o0pmitev <pmitev89@gmail.com>
2020-04-03 13:58:26 +0200

    FINISHED

--- Browser-fetch-JSON/src/scripts/index.js
+++ Browser-fetch-JSON/src/scripts/index.js
@@ -6,17 +6,45 @@ import '../styles/main.scss';
 // \/ All of your javascript should go here \/
 
 // import recipes from 'recipesJSON'
-import { recipesJSON } from './data';
 
-console.log(recipesJSON);
+import { recipesJSON } from './data.js';
 
-const cakesBoilerplate = 
-`
-<img src='${recipesJSON['cakes'][0].image}'>
-<h5>${recipesJSON[0].title}</h5>
-<p>${recipesJSON[0].author}</p>
-<p>${recipesJSON[0].ingredients}</p>
-`;
 
-const cakes = document.querySelector('#cakes');
-cakes.innerHTML += cakesBoilerplate;
+const cakesHtml = document.querySelector('#cakes');
+const biscuitsHtml = document.querySelector('#biscuits');
+const breadHtml = document.querySelector('#bread');
+
+const recipes = JSON.parse(recipesJSON);
+
+
+ Object.values(recipes).forEach((element,index) => { element.forEach((element) => {
+    
+    const cakesBoilerplate = 
+    `
+    <div class="card" style="width: 18rem;">
+    <img class="card-img-top" src='${element['image']}'>
+    <div class="card-body">
+     <h5>${element['title']}</h5>
+    <p>${element['author']}</p>
+    <p>${element['ingredients']}</p>
+    </div>
+    </div>
+    `;
+
+    switch(element['type']) {
+        case 'cakes':
+            cakesHtml.innerHTML += cakesBoilerplate;
+            break;
+        case 'biscuits':
+            biscuitsHtml.innerHTML += cakesBoilerplate;
+            break;
+        case 'bread':
+            breadHtml.innerHTML += cakesBoilerplate;
+            break;
+        default: 
+            console.log('end');
+    }
+
+})});
+
+
