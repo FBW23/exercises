4acb2b5f6b278c5122f86fc7037a362b957ee4b4
hbendis <bendishoxha1@gmail.com>
2020-04-14 15:06:26 +0200

    done

--- Browser-fetch-async/src/scripts/index.js
+++ Browser-fetch-async/src/scripts/index.js
@@ -8,47 +8,47 @@ let promiseOfModal = new Promise(function (resolve) {
     }, (100 * 20));
 });
 
-promiseOfModal.then(function(val) {
+promiseOfModal.then(function (val) {
     console.log("User has been on the page for 60 seconds");
     val.style.display = "block";
 })
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
+    switch (e.target.className) {
         case "close":
         case "modal":
-        modal.style.display = "none"
-        break;
+            modal.style.display = "none"
+            break;
     }
 })
 
 
-promiseOfModal.then(function(val) {
-}); 
+promiseOfModal.then(function (val) {});
 
 const btnMsg = "Continue to subscribe"
 
 btn.addEventListener("mouseout", (e) => {
     function resolveMsg() {
         return new Promise(resolve => {
-          setTimeout(() => {
-            resolve('resolved');
-          }, 200);
+            setTimeout(() => {
+                resolve('resolved');
+            }, 200);
         });
-      }
-      async function asyncCall() {
+    }
+    async function asyncCall() {
         const result = await resolveMsg();
         console.log("Change button text");
         btn.textContent = btnMsg;
-      }
-      asyncCall();
+    }
+    asyncCall();
 })
 
 modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
+    switch (e.target.className) {
         case "close":
         case "modal":
-        modal.style.display = "none"
-        break;
+            modal.style.display = "none"
+            break;
     }
+
 })
