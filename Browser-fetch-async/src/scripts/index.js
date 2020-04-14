379f344ceb766423c6c275a9a877794e321cf04e
o0pmitev <pmitev89@gmail.com>
2020-04-14 14:45:05 +0200

    not finished

--- Browser-fetch-async/src/scripts/index.js
+++ Browser-fetch-async/src/scripts/index.js
@@ -1,30 +1,42 @@
 import "../styles/main.scss";
 import "babel-polyfill"
 
-const modal = document.getElementById("myModal");
 
-async function asyncModalCall() {
-    let promiseOfModal = new Promise(function (resolve) {
-        window.setTimeout(function () {
-            resolve(modal)
-        }, (1000));
-    });
+window.addEventListener('DOMContentLoaded', () => {
+    const modal = document.getElementById("myModal");
 
- await promiseOfModal.then(function(val) {
-        console.log("User has been on the page for 60 seconds");
-        val.style.display = "block";
-    })
-    
-}
+    async function asyncModalCall() {
+        let promiseOfModal = new Promise(function (resolve) {
+            window.setTimeout(function () {
+                resolve(modal)
+            }, (1000 * 60));
+        });
 
-asyncModalCall();
+        await promiseOfModal.then(function (val) {
+            console.log("User has been on the page for 60 seconds");
+            val.style.display = "block";
+        })
 
-modal.addEventListener("click", (e) => {
-    switch(e.target.className) {
-        case "close":
-        case "modal":
-        modal.style.display = "none"
-        break;
     }
-})
 
+    asyncModalCall();
+
+    modal.addEventListener("click", (e) => {
+        switch (e.target.className) {
+            case "close":
+            case "modal":
+                modal.style.display = "none"
+                break;
+        }
+    })
+
+
+const subscribePromise = new Promise((resolve, reject) => {
+    const button = document.querySelector('.btn');
+    button.addEventListener('mouseout', function() {
+        resolve(this.style.background = 'black') 
+        
+    })
+})
+ subscribePromise();
+})
