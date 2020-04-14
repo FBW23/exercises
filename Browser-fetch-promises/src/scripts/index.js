417289746f360b6b1b3c16981189448568fbd72e
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-04-14 10:36:42 +0200

    hellO

--- Browser-fetch-promises/src/scripts/index.js
+++ Browser-fetch-promises/src/scripts/index.js
@@ -5,3 +5,26 @@ import '../styles/main.scss';
 
 // \/ All of your javascript should go here \/
 
+document.addEventListener('DOMContentLoaded', function () {
+    const body = document.querySelector('body');
+    const modal = document.querySelector('.modal');
+    let promiseOfModal = new Promise(function (resolve, reject) {
+        window.setTimeout(function () {
+            resolve(modal, body); // give your references 
+        }, (2*1000)); // 60 seconds
+        // reject(body);
+    });
+    promiseOfModal.then(function() {
+        modal.style.display = "block";
+    });
+    promiseOfModal.then(function() {
+        body.style.background = "black";
+    });
+    promiseOfModal.catch(function(e) {
+        console.error(e);
+    });
+    const close = document.querySelector('.close');
+    close.addEventListener('click', function() {
+        modal.style.display = "none";
+    });
+});
