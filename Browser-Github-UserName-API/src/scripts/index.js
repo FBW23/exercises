95ca607be90c198eda4a2ed3934783a1c7c54961
o0pmitev <pmitev89@gmail.com>
2020-04-16 11:59:18 +0200

    FINISHED

--- Browser-Github-UserName-API/src/scripts/index.js
+++ Browser-Github-UserName-API/src/scripts/index.js
@@ -3,20 +3,27 @@
 
 const form1 = document.querySelector('.form1');
 const githubUserName1 = document.querySelector('#githubUsername1');
+const unorderedList1 = document.querySelector('.list-group');
 
 const submitHandler = () => {
   event.preventDefault();
 
   const username = githubUserName1.value;
-  
+
   async function getGithubRepositories() {
-   const response = await fetch(`https://api.github.com/users/${username}/repos`)
-   const data = await response.json();
-   return data
+    const response = await fetch(`https://api.github.com/users/${username}/repos`)
+    const data = await response.json();
+    return data
   }
   getGithubRepositories()
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+    .then(data => {
+      data.forEach(element => {
+          unorderedList1.innerHTML += `
+      <li class="list-group-item"><a style="text-decoration:none" href='${element.html_url}'>${element.name}</a></li>
+      
+      `
+        })
+        .catch(error => console.log(error));
+    })
 }
-
 form1.addEventListener('submit', submitHandler);
