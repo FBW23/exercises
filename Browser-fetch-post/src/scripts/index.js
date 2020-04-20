300b397470cdd5b84cc04c4b3f5a14e59e44b129
Julien Montagnon <jumontagnon@gmail.com>
2020-04-20 14:20:19 +0200

    working with Nicolo

--- Browser-fetch-post/src/scripts/index.js
+++ Browser-fetch-post/src/scripts/index.js
@@ -5,3 +5,37 @@ import "babel-polyfill";
 
 // \/ All of your javascript should go here \/
 
+const form = document.querySelector('.container');
+const name = document.querySelector('input[type=text]');
+const email = document.querySelector('input[type=email]');
+const password = document.querySelector('input[type=password]');
+const checkbox = document.querySelector('input[type=checkbox]');
+
+
+
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+
+    const myBody = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+        checkbox: checkbox.checked
+    };
+
+    const url = "https://jsonplaceholder.typicode.com/posts"
+
+    fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(myBody)
+        })
+        .then(response => response.json()) // parse JSON
+        .then(json => console.log(json)); // console.log it!
+
+
+}
+form.addEventListener('submit', handleSubmit);
+
+
