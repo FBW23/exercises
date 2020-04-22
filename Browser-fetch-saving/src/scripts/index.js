281038c9b63c0572b7267f07a4414b3a28cabbc8
o0pmitev <pmitev89@gmail.com>
2020-04-22 09:53:09 +0200

    22.04.2020

--- Browser-fetch-saving/src/scripts/index.js
+++ Browser-fetch-saving/src/scripts/index.js
@@ -1,17 +1,33 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
 import 'babel-polyfill';
-import Client from './apiCall';
-import View from './view';
+import Client from './apiCall.js';
+import View from './view.js';
 // Import any additional modules you want to include below \/
 
 
 // \/ All of your javascript should go here \/
-console.log(View);
-console.log(Client)
-localStorage.clear();
+const form = document.querySelector('input');
 
-let newClass = new Client();
 
-newClass.getMovieData()
-.then( response => console.log(Object.keys(response)))
+
+ function submitForm() {
+  console.log(form.value);
+  localStorage.setItem(form.value, form.value);
+  const clientInput =  new Client();
+  const movieData =  clientInput.getMovieData(form.value);
+  const createView =  new View();
+  console.log(createView)
+  const renderMovieBoilerPlate =  createView.moviesSection.insertAdjacentHTML(movies)
+
+  console.log(createView)
+}
+
+
+
+
+
+
+
+
+let save = document.querySelector(".btn-save").addEventListener('click', submitForm)
